eec3ce375cdfd04390795b399f731e194d512114
Jeremy Stretch <jstretch@digitalocean.com>
2016-03-22 14:55:40 -0400

    Removed Device filters

--- netbox/secrets/filters.py
+++ netbox/secrets/filters.py
@@ -1,21 +1,9 @@
 import django_filters
 
-from dcim.models import Device
 from .models import Secret, SecretRole
 
 
 class SecretFilter(django_filters.FilterSet):
-    device_id = django_filters.ModelMultipleChoiceFilter(
-        name='device',
-        queryset=Device.objects.all(),
-        label='Device (ID)',
-    )
-    device = django_filters.ModelMultipleChoiceFilter(
-        name='device',
-        queryset=Device.objects.all(),
-        to_field_name='name',
-        label='Device (name)',
-    )
     role_id = django_filters.ModelMultipleChoiceFilter(
         name='role',
         queryset=SecretRole.objects.all(),
@@ -30,4 +18,4 @@ class SecretFilter(django_filters.FilterSet):
 
     class Meta:
         model = Secret
-        fields = ['name', 'device_id', 'device', 'role_id', 'role']
+        fields = ['name', 'role_id', 'role']
