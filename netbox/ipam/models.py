8cf2ae7851e587666db09450dc1c87367a95b255
Jeremy Stretch <jstretch@digitalocean.com>
2016-08-19 10:31:16 -0400

    Fixes #495: Include tenant in prefix and IP CSV export

--- netbox/ipam/models.py
+++ netbox/ipam/models.py
@@ -275,6 +275,7 @@ class Prefix(CreatedUpdatedModel):
         return ','.join([
             str(self.prefix),
             self.vrf.rd if self.vrf else '',
+            self.tenant.name if self.tenant else '',
             self.site.name if self.site else '',
             self.get_status_display(),
             self.role.name if self.role else '',
@@ -378,6 +379,7 @@ class IPAddress(CreatedUpdatedModel):
         return ','.join([
             str(self.address),
             self.vrf.rd if self.vrf else '',
+            self.tenant.name if self.tenant else '',
             self.device.identifier if self.device else '',
             self.interface.name if self.interface else '',
             'True' if is_primary else '',
