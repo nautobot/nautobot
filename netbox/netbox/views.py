f44a322df5a0e315599371f63bb3717dbdafd1f7
Jeremy Stretch <jstretch@digitalocean.com>
2016-10-24 13:53:58 -0400

    Closes #630: Added a custom 404 page

--- netbox/netbox/views.py
+++ netbox/netbox/views.py
@@ -47,16 +47,20 @@ def home(request):
     })
 
 
-def trigger_500(request):
-    """Hot-wired method of triggering a server error to test reporting."""
-    raise Exception("Congratulations, you've triggered an exception! Go tell all your friends what an exceptional "
-                    "person you are.")
-
-
 def handle_500(request):
-    """Custom server error handler"""
+    """
+    Custom server error handler
+    """
     type_, error, traceback = sys.exc_info()
     return render(request, '500.html', {
         'exception': str(type_),
         'error': error,
     }, status=500)
+
+
+def trigger_500(request):
+    """
+    Hot-wired method of triggering a server error to test reporting
+    """
+    raise Exception("Congratulations, you've triggered an exception! Go tell all your friends what an exceptional "
+                    "person you are.")
