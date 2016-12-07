4a9b4c5387aace8eb72cfbda3aad76e5a3896ef4
Jeremy Stretch <jstretch@digitalocean.com>
2016-12-07 14:00:52 -0500

    Fixes #732: Allow custom select field values to be deselected if the field is not required

--- netbox/extras/forms.py
+++ netbox/extras/forms.py
@@ -49,7 +49,7 @@ def get_custom_fields_for_model(content_type, filterable_only=False, bulk_edit=F
         # Select
         elif cf.type == CF_TYPE_SELECT:
             choices = [(cfc.pk, cfc) for cfc in cf.choices.all()]
-            if bulk_edit or filterable_only:
+            if not cf.required or bulk_edit or filterable_only:
                 choices = [(None, '---------')] + choices
             field = forms.TypedChoiceField(choices=choices, coerce=int, required=cf.required)
 
