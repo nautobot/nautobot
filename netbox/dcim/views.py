466c505bb88fda2e12471343e16fe0250817fe15
Jeremy Stretch <jstretch@digitalocean.com>
2017-01-13 09:30:59 -0500

    Corrected PEP8 errors

--- netbox/dcim/views.py
+++ netbox/dcim/views.py
@@ -601,18 +601,16 @@ def device(request, pk):
     power_outlets = natsorted(
         PowerOutlet.objects.filter(device=device).select_related('connected_port'), key=attrgetter('name')
     )
-    interfaces = Interface.objects.order_naturally(device.device_type.interface_ordering)\
-        .filter(device=device, mgmt_only=False).select_related(
-            'connected_as_a__interface_b__device',
-            'connected_as_b__interface_a__device',
-            'circuit_termination__circuit',
-        )
-    mgmt_interfaces = Interface.objects.order_naturally(device.device_type.interface_ordering)\
-        .filter(device=device, mgmt_only=True).select_related(
-            'connected_as_a__interface_b__device',
-            'connected_as_b__interface_a__device',
-            'circuit_termination__circuit',
-        )
+    interfaces = Interface.objects.filter(device=device, mgmt_only=False).select_related(
+        'connected_as_a__interface_b__device',
+        'connected_as_b__interface_a__device',
+        'circuit_termination__circuit',
+    ).order_naturally(device.device_type.interface_ordering)
+    mgmt_interfaces = Interface.objects.filter(device=device, mgmt_only=True).select_related(
+        'connected_as_a__interface_b__device',
+        'connected_as_b__interface_a__device',
+        'circuit_termination__circuit',
+    ).order_naturally(device.device_type.interface_ordering)
     device_bays = natsorted(
         DeviceBay.objects.filter(device=device).select_related('installed_device__device_type__manufacturer'),
         key=attrgetter('name')
