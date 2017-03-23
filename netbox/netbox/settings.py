a51f5edbc8b18a2d549212c1a3039eb62a3daa55
Jeremy Stretch <jstretch@digitalocean.com>
2017-03-23 16:29:42 -0400

    Post-release version bump

--- netbox/netbox/settings.py
+++ netbox/netbox/settings.py
@@ -12,7 +12,7 @@ except ImportError:
                                "the documentation.")
 
 
-VERSION = '1.9.3'
+VERSION = '1.9.4-dev'
 
 # Import local configuration
 for setting in ['ALLOWED_HOSTS', 'DATABASE', 'SECRET_KEY']:
