69e54ab410b5f412bfe0fc81e2ff888091a38fa3
Jeremy Stretch <jstretch@digitalocean.com>
2017-03-28 12:19:08 -0400

    Token admin form improvements

--- netbox/users/admin.py
+++ netbox/users/admin.py
@@ -1,8 +1,18 @@
+from django import forms
 from django.contrib import admin
 
 from .models import Token
 
 
+class TokenAdminForm(forms.ModelForm):
+    key = forms.CharField(required=False, help_text="If no key is provided, one will be generated automatically.")
+
+    class Meta:
+        fields = ['user', 'key', 'write_enabled', 'expires', 'description']
+        model = Token
+
+
 @admin.register(Token)
 class TokenAdmin(admin.ModelAdmin):
-    list_display = ['user', 'key', 'created', 'expires', 'write_enabled', 'description']
+    form = TokenAdminForm
+    list_display = ['key', 'user', 'created', 'expires', 'write_enabled', 'description']
