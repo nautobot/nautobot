38d826d152b5c5f0e78df1f0da146c7e67d98566
Jeremy Stretch <jstretch@digitalocean.com>
2017-04-21 10:32:10 -0400

    Fixes #1092: Increase randomness in SECRET_KEY generation tool

--- netbox/generate_secret_key.py
+++ netbox/generate_secret_key.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # This script will generate a random 50-character string suitable for use as a SECRET_KEY.
-import os
 import random
 
 charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(-_=+)'
-random.seed = (os.urandom(2048))
-print(''.join(random.choice(charset) for c in range(50)))
+secure_random = random.SystemRandom()
+print(''.join(secure_random.sample(charset, 50)))
