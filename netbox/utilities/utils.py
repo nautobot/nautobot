aa6ca21a34f57b8ce2f06d9b2e5101699ce33623
Jeremy Stretch <jstretch@digitalocean.com>
2017-05-15 13:18:49 -0400

    PEP8 fix

--- netbox/utilities/utils.py
+++ netbox/utilities/utils.py
@@ -31,7 +31,7 @@ def foreground_color(bg_color):
     Return the ideal foreground color (black or white) for a given background color in hexadecimal RGB format.
     """
     bg_color = bg_color.strip('#')
-    r, g, b = [int(bg_color[c:c+2], 16) for c in (0, 2, 4)]
+    r, g, b = [int(bg_color[c:c + 2], 16) for c in (0, 2, 4)]
     if r * 0.299 + g * 0.587 + b * 0.114 > 186:
         return '000000'
     else:
