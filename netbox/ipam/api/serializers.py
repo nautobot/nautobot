f03a378ce0f438b6228d7fc52a5271dd19c6fe27
Jeremy Stretch <jstretch@digitalocean.com>
2017-05-31 11:50:03 -0400

    Fixes #1239: Fix server error when creating VLANGroup via API

--- netbox/ipam/api/serializers.py
+++ netbox/ipam/api/serializers.py
@@ -137,7 +137,7 @@ class WritableVLANGroupSerializer(serializers.ModelSerializer):
         # Validate uniqueness of name and slug if a site has been assigned.
         if data.get('site', None):
             for field in ['name', 'slug']:
-                validator = UniqueTogetherValidator(queryset=VLAN.objects.all(), fields=('site', field))
+                validator = UniqueTogetherValidator(queryset=VLANGroup.objects.all(), fields=('site', field))
                 validator.set_context(self)
                 validator(data)
 
