b8df05cf88b21f1165b43160329a3a617ffb2a11
Ichabond <tom@strickx.com>
2017-11-02 08:51:27 -0700

    Fixes #1655. Removed explicit field references. (#1656)
    * Fixes #1655
    
    Further field name references were found in `consoleport.html`. These have now been removed, so we rely on proper a proper `__str__` implementation of both `ConsolePort` and `ConsoleServerPort`.
    
    * Fixes #1655: Removed explicit field references
    
    Cleaned up all (notable) .name references, and removed them so __str__ can do the lifting. Did not remove the references where it was explicitly referenced to .name (eg. in details). Extended the Secret model to also include the name in __str__, since that was weirdly absent.
    
    * Adapted PR to comply with comments
    
    Re-introduced certain references to make sure explicit references are still used where needed.

--- netbox/secrets/models.py
+++ netbox/secrets/models.py
@@ -286,6 +286,9 @@ class Secret(CreatedUpdatedModel):
         super(Secret, self).__init__(*args, **kwargs)
 
     def __str__(self):
+        if self.role and self.device and self.name:
+            return '{} for {} ({})'.format(self.role, self.device, self.name)
+        # Return role and device if no name is set
         if self.role and self.device:
             return '{} for {}'.format(self.role, self.device)
         return 'Secret'
