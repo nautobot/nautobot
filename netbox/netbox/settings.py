88adc5ca86f6dbc22bad766a812c17cb72c83d2d
Jeremy Stretch <jstretch@digitalocean.com>
2018-03-22 15:06:59 -0400

    Post-release version bump

--- netbox/netbox/settings.py
+++ netbox/netbox/settings.py
@@ -22,7 +22,7 @@ if sys.version_info[0] < 3:
         DeprecationWarning
     )
 
-VERSION = '2.3.2'
+VERSION = '2.3.3-dev'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
