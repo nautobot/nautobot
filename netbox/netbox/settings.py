398041c607daf88c71149a7471360526be840a32
Jeremy Stretch <jstretch@digitalocean.com>
2018-07-02 15:54:09 -0400

    Release v2.3.5

--- netbox/netbox/settings.py
+++ netbox/netbox/settings.py
@@ -22,7 +22,7 @@ if sys.version_info[0] < 3:
         DeprecationWarning
     )
 
-VERSION = '2.3.5-dev'
+VERSION = '2.3.5'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
