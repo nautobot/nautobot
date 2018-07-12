931c58bc9a9f8216887ff76a4e162b9588dc7ab5
Jeremy Stretch <jstretch@digitalocean.com>
2018-07-12 13:16:34 -0400

    Enabled export templates for VRFs

--- netbox/extras/constants.py
+++ netbox/extras/constants.py
@@ -51,7 +51,7 @@ EXPORTTEMPLATE_MODELS = [
     'provider', 'circuit',                                                          # Circuits
     'site', 'region', 'rack', 'rackgroup', 'manufacturer', 'devicetype', 'device',  # DCIM
     'consoleport', 'powerport', 'interfaceconnection',                              # DCIM
-    'aggregate', 'prefix', 'ipaddress', 'vlan',                                     # IPAM
+    'aggregate', 'prefix', 'ipaddress', 'vlan', 'vrf',                              # IPAM
     'tenant',                                                                       # Tenancy
     'cluster', 'virtualmachine',                                                    # Virtualization
 ]
