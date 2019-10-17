adc96702db0ae237177941203df3c0193e58308e
Jeremy Stretch <stretch@packetlife.net>
2019-10-17 15:43:49 -0400

    Deleted errant import of graphviz

--- netbox/extras/models.py
+++ netbox/extras/models.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
 from datetime import date
 
-import graphviz
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
