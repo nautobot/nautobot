770f4c962cf4bfb5d44bf7cc6ac5f1e080e0cd7f
Jeremy Stretch <stretch@packetlife.net>
2020-01-07 10:31:44 -0500

    Fixes #3856: Allow filtering VM interfaces by multiple MAC addresses

--- netbox/virtualization/filters.py
+++ netbox/virtualization/filters.py
@@ -208,24 +208,13 @@ class InterfaceFilter(django_filters.FilterSet):
         to_field_name='name',
         label='Virtual machine',
     )
-    mac_address = django_filters.CharFilter(
-        method='_mac_address',
+    mac_address = MultiValueMACAddressFilter(
         label='MAC address',
     )
 
     class Meta:
         model = Interface
         fields = ['id', 'name', 'enabled', 'mtu']
-
-    def _mac_address(self, queryset, name, value):
-        value = value.strip()
-        if not value:
-            return queryset
-        try:
-            mac = EUI(value.strip())
-            return queryset.filter(mac_address=mac)
-        except AddrFormatError:
-            return queryset.none()
 
     def search(self, queryset, name, value):
         if not value.strip():
