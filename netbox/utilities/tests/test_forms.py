fe89982d4ef663427d91e4efe67b364ccebaa811
Saria Hajjar <shajjar@mc-lon-mb11797.local>
2020-01-10 10:26:46 +0000

    Removed redundant list call

--- netbox/utilities/tests/test_forms.py
+++ netbox/utilities/tests/test_forms.py
@@ -14,7 +14,7 @@ class ExpandIPAddress(TestCase):
             '1.2.3.10/32',
         ])
 
-        self.assertEqual(sorted(list(expand_ipaddress_pattern(input, 4))), output)
+        self.assertEqual(sorted(expand_ipaddress_pattern(input, 4)), output)
 
     def test_ipv4_set(self):
         input = '1.2.3.[4,44]/32'
@@ -23,7 +23,7 @@ class ExpandIPAddress(TestCase):
             '1.2.3.44/32',
         ])
 
-        self.assertEqual(sorted(list(expand_ipaddress_pattern(input, 4))), output)
+        self.assertEqual(sorted(expand_ipaddress_pattern(input, 4)), output)
 
     def test_ipv4_multiple_ranges(self):
         input = '1.[9-10].3.[9-11]/32'
@@ -36,7 +36,7 @@ class ExpandIPAddress(TestCase):
             '1.10.3.11/32',
         ])
 
-        self.assertEqual(sorted(list(expand_ipaddress_pattern(input, 4))), output)
+        self.assertEqual(sorted(expand_ipaddress_pattern(input, 4)), output)
 
     def test_ipv4_multiple_sets(self):
         input = '1.[2,22].3.[4,44]/32'
@@ -47,8 +47,7 @@ class ExpandIPAddress(TestCase):
             '1.22.3.44/32',
         ])
 
-        self.assertEqual(sorted(list(expand_ipaddress_pattern(input, 4))), output)
-
+        self.assertEqual(sorted(expand_ipaddress_pattern(input, 4)), output)
 
     def test_ipv4_set_and_range(self):
         input = '1.[2,22].3.[9-11]/32'
@@ -61,7 +60,7 @@ class ExpandIPAddress(TestCase):
             '1.22.3.11/32',
         ])
 
-        self.assertEqual(sorted(list(expand_ipaddress_pattern(input, 4))), output)
+        self.assertEqual(sorted(expand_ipaddress_pattern(input, 4)), output)
 
     # TODO: IPv6
     # TODO: negative tests
