fcbbb36afc038c29e3cee35dda9c03bca47eee10
Jeremy Stretch <stretch@packetlife.net>
2020-01-23 15:41:09 -0500

    Add tests for home and search views

---
+++ netbox/netbox/tests/test_api.py
@@ -0,0 +1,13 @@
+from django.urls import reverse
+
+from utilities.testing import APITestCase
+
+
+class AppTest(APITestCase):
+
+    def test_root(self):
+
+        url = reverse('api-root')
+        response = self.client.get('{}?format=api'.format(url), **self.header)
+
+        self.assertEqual(response.status_code, 200)
