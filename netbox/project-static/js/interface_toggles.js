5c5b9c95aaf0fc6b003fab7be6746cae408976c6
Saria Hajjar <sariahajjar@gmail.com>
2020-01-27 22:07:42 +0000

    Interface selector restricted to only interface

--- netbox/project-static/js/interface_toggles.js
+++ netbox/project-static/js/interface_toggles.js
@@ -16,21 +16,20 @@ $('input.interface-filter').on('input', function() {
     var filter = new RegExp(this.value);
     var interface;
 
-    for (interface of $('#interfaces_table > tbody > tr')) {
+    for (interface of $('#interfaces_table > tbody > tr.interface')) {
         // Slice off 'interface_' at the start of the ID
         if (filter.test(interface.id.slice(10))) {
             // Match the toggle in case the filter now matches the interface
             $(interface).find('input:checkbox[name=pk]').prop('checked', $('input.toggle').prop('checked'));
             $(interface).show();
+            if ($('button.toggle-ips').attr('selected')) {
+                $(interface).next('tr.ipaddresses').show();
+            }
         } else {
             // Uncheck to prevent actions from including it when it doesn't match
             $(interface).find('input:checkbox[name=pk]').prop('checked', false);
             $(interface).hide();
+            $(interface).next('tr.ipaddresses').hide();
         }
-    }
-
-    // Show the ip addresses table row for the visible (matched) interfaces, if checked
-    if ($('button.toggle-ips').attr('selected')) {
-        $('#interfaces_table > tbody > tr:visible').next('tr.ipaddresses').show();
     }
 });
