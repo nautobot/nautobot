a58bbccfd39268c7c67d8a9ffb16d7153667dc17
Jeremy Stretch <stretch@packetlife.net>
2020-02-05 16:12:48 -0500

    Fixes #4094: Convert unnecessary regular expressions to strings in URL patterns

--- netbox/secrets/api/urls.py
+++ netbox/secrets/api/urls.py
@@ -15,15 +15,15 @@ router = routers.DefaultRouter()
 router.APIRootView = SecretsRootView
 
 # Field choices
-router.register(r'_choices', views.SecretsFieldChoicesViewSet, basename='field-choice')
+router.register('_choices', views.SecretsFieldChoicesViewSet, basename='field-choice')
 
 # Secrets
-router.register(r'secret-roles', views.SecretRoleViewSet)
-router.register(r'secrets', views.SecretViewSet)
+router.register('secret-roles', views.SecretRoleViewSet)
+router.register('secrets', views.SecretViewSet)
 
 # Miscellaneous
-router.register(r'get-session-key', views.GetSessionKeyViewSet, basename='get-session-key')
-router.register(r'generate-rsa-key-pair', views.GenerateRSAKeyPairViewSet, basename='generate-rsa-key-pair')
+router.register('get-session-key', views.GetSessionKeyViewSet, basename='get-session-key')
+router.register('generate-rsa-key-pair', views.GenerateRSAKeyPairViewSet, basename='generate-rsa-key-pair')
 
 app_name = 'secrets-api'
 urlpatterns = router.urls
