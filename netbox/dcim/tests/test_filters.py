047f13ac5d47038568f49ea55d0861222a93e4b0
Jeremy Stretch <stretch@packetlife.net>
2020-02-14 15:07:59 -0500

    Update tests

--- netbox/dcim/tests/test_filters.py
+++ netbox/dcim/tests/test_filters.py
@@ -1144,8 +1144,8 @@ class DeviceTestCase(TestCase):
 
         # Assign primary IPs for filtering
         ipaddresses = (
-            IPAddress(family=4, address='192.0.2.1/24', interface=interfaces[0]),
-            IPAddress(family=4, address='192.0.2.2/24', interface=interfaces[1]),
+            IPAddress(address='192.0.2.1/24', interface=interfaces[0]),
+            IPAddress(address='192.0.2.2/24', interface=interfaces[1]),
         )
         IPAddress.objects.bulk_create(ipaddresses)
         Device.objects.filter(pk=devices[0].pk).update(primary_ip4=ipaddresses[0])
