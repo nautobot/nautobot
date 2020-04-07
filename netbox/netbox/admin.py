902b1b2c3239f4693748243b0c3c52d6599a53a7
Jeremy Stretch <stretch@packetlife.net>
2020-04-07 10:17:34 -0400

    Fixes #4458: Remove custom admin site to avoid conflict with django-rq 2.3.0

--- netbox/netbox/admin.py
+++ netbox/netbox/admin.py
@@ -1,20 +1,13 @@
 from django.conf import settings
-from django.contrib.admin import AdminSite
-from django.contrib.auth.admin import GroupAdmin, UserAdmin
-from django.contrib.auth.models import Group, User
+from django.contrib.admin import site as admin_site
+from taggit.models import Tag
 
 
-class NetBoxAdminSite(AdminSite):
-    """
-    Custom admin site
-    """
-    site_header = 'NetBox Administration'
-    site_title = 'NetBox'
-    site_url = '/{}'.format(settings.BASE_PATH)
+# Override default AdminSite attributes so we can avoid creating and
+# registering our own class
+admin_site.site_header = 'NetBox Administration'
+admin_site.site_title = 'NetBox'
+admin_site.site_url = '/{}'.format(settings.BASE_PATH)
 
-
-admin_site = NetBoxAdminSite(name='admin')
-
-# Register external models
-admin_site.register(Group, GroupAdmin)
-admin_site.register(User, UserAdmin)
+# Unregister the unused stock Tag model provided by django-taggit
+admin_site.unregister(Tag)
