35a280eb31be691523567a77470564f28282960a
Jeremy Stretch <stretch@packetlife.net>
2020-08-22 21:03:51 -0400

    Release v2.9.1

--- docs/release-notes/version-2.9.md
+++ docs/release-notes/version-2.9.md
@@ -1,6 +1,6 @@
 # NetBox v2.9
 
-## v2.9.1 (FUTURE)
+## v2.9.1 (2020-08-22)
 
 ### Enhancements
 
--- netbox/netbox/settings.py
+++ netbox/netbox/settings.py
@@ -16,7 +16,7 @@ from django.core.validators import URLValidator
 # Environment setup
 #
 
-VERSION = '2.9.1-dev'
+VERSION = '2.9.1'
 
 # Hostname
 HOSTNAME = platform.node()
