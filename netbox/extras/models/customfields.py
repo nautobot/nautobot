af3c4905ea1a4ed24ad23b342f489013a18c252d
Jeremy Stretch <stretch@packetlife.net>
2020-12-31 10:21:01 -0500

    Fixes #5558: Fix regex validation support for custom URL fields

--- netbox/extras/models/customfields.py
+++ netbox/extras/models/customfields.py
@@ -196,7 +196,8 @@ class CustomField(models.Model):
             })
 
         # Regex validation can be set only for text fields
-        if self.validation_regex and self.type != CustomFieldTypeChoices.TYPE_TEXT:
+        regex_types = (CustomFieldTypeChoices.TYPE_TEXT, CustomFieldTypeChoices.TYPE_URL)
+        if self.validation_regex and self.type not in regex_types:
             raise ValidationError({
                 'validation_regex': "Regular expression validation is supported only for text and URL fields"
             })
