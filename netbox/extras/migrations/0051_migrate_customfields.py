359ae5d116f983039b87b4b694b822116a32e652
Brian Candler <b.candler@pobox.com>
2021-01-05 15:05:17 +0000

    Raise exceptions for other inconsistencies when migrating custom field data
    Fixes #5573

--- netbox/extras/migrations/0051_migrate_customfields.py
+++ netbox/extras/migrations/0051_migrate_customfields.py
@@ -67,7 +67,7 @@ def migrate_customfieldvalues(apps, schema_editor):
         cf_data = model.objects.filter(pk=cfv.obj_id).values('custom_field_data').first()
         try:
             cf_data['custom_field_data'][cfv.field.name] = deserialize_value(cfv.field, cfv.serialized_value)
-        except ValueError as e:
+        except Exception as e:
             print(f'{cfv.field.name} ({cfv.field.type}): {cfv.serialized_value} ({cfv.pk})')
             raise e
         model.objects.filter(pk=cfv.obj_id).update(**cf_data)
