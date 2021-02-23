92815acf97723952181acbda3df51cb69608651f
jathanism <jathan@gmail.com>
2021-02-23 06:59:17 -0800

    Add `nautobot.setup()` function to bootstrap Nautobot settings
    - Patched `nautobot.core.wsgi` to use `nautobot.setup()` to assert
      consistent file loading across all apps

--- nautobot/core/wsgi.py
+++ nautobot/core/wsgi.py
@@ -2,6 +2,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nautobot.core.settings")
+import nautobot
+
+# This is the Django default left here for visibility on how the Nautobot pattern
+# differs.
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nautobot.core.settings")
+
+# Instead of importing `DJANGO_SETTINGS_MODULE` we're using the custom loader
+# pattern from `nautobot.core.runner` to read environment or config path for us.
+nautobot.setup()
 
 application = get_wsgi_application()
