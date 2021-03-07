dfaf58648bb5d32e48e4b35243dafab2efd1adbf
jathanism <jathan@gmail.com>
2021-03-06 19:05:23 -0800

    Add:
    - remove_stale_contenttypes
    - clearsessions
    - invalidate all

--- nautobot/core/management/commands/upgrade.py
+++ nautobot/core/management/commands/upgrade.py
@@ -12,6 +12,9 @@ This will run the following management commands with default settings, in order:
 - migrate
 - trace_paths
 - collectstatic
+- remove_stale_contenttypes
+- clearsessions
+- invalidate all
 """
 
 
@@ -26,11 +29,25 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
+            "--no-clearsessions",
+            action="store_false",
+            dest="clearsessions",
+            default=True,
+            help="Do not automatically clean out expired sessions.",
+        )
+        parser.add_argument(
             "--no-collectstatic",
             action="store_false",
             dest="collectstatic",
             default=True,
-            help="Do not automatically collect static files into STATIC_ROOT.",
+            help="Do not automatically collect static files into a single location.",
+        )
+        parser.add_argument(
+            "--no-invalidate-all",
+            action="store_false",
+            dest="invalidate_all",
+            default=True,
+            help="Do not invalidate cache for entire application",
         )
         parser.add_argument(
             "--no-migrate",
@@ -38,6 +55,13 @@ class Command(BaseCommand):
             dest="migrate",
             default=True,
             help="Do not automatically perform any database migrations.",
+        )
+        parser.add_argument(
+            "--no-remove-stale-contenttypes",
+            action="store_false",
+            dest="remove_stale_contenttypes",
+            default=True,
+            help="Do not automatically remove stale content types.",
         )
         parser.add_argument(
             "--no-trace-paths",
@@ -69,3 +93,22 @@ class Command(BaseCommand):
         if options.get("collectstatic"):
             print("Collecting static files...")
             call_command("collectstatic", interactive=False)
+            print()
+
+        # Run remove_stale_contenttypes
+        if options.get("remove_stale_contenttypes"):
+            print("Removing stale content types...")
+            call_command("remove_stale_contenttypes", interactive=False)
+            print()
+
+        # Run clearsessions
+        if options.get("clearsessions"):
+            print("Removing expired sessions...")
+            call_command("clearsessions")
+            print()
+
+        # Run invalidate all
+        if options.get("invalidate_all"):
+            print("Invalidating cache ...")
+            call_command("invalidate", "all")
+            print()
