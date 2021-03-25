f62afd4ada53c6ebdbe621cbc5ab28475c31f291
Glenn Matthews <glenn.matthews@networktocode.com>
2021-03-25 11:15:16 -0400

    Remove a couple of leftover references to django.contrib.postgres.fields

--- nautobot/dcim/fields.py
+++ nautobot/dcim/fields.py
@@ -1,4 +1,3 @@
-from django.contrib.postgres.fields import ArrayField
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
