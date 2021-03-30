85a321df05604e9a7fc84d85236a48f3f9103e0a
nniehoff <github@nickniehoff.net>
2021-03-30 10:33:04 -0600

    Removing logging from config template

---
+++ docker/nautobot_config.append.py
@@ -0,0 +1,37 @@
+LOG_LEVEL = "DEBUG" if DEBUG else "INFO"
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "normal": {
+            "format": "%(asctime)s.%(msecs)03d %(levelname)-7s %(name)s :\n  %(message)s",
+            "datefmt": "%H:%M:%S",
+        },
+        "verbose": {
+            "format": "%(asctime)s.%(msecs)03d %(levelname)-7s %(name)-20s %(filename)-15s %(funcName)30s() :\n  %(message)s",
+            "datefmt": "%H:%M:%S",
+        },
+    },
+    "handlers": {
+        "normal_console": {
+            "level": "DEBUG",
+            "class": "rq.utils.ColorizingStreamHandler",
+            "formatter": "normal",
+        },
+    },
+    "loggers": {
+        "django": {"handlers": ["normal_console"], "level": LOG_LEVEL},
+        "nautobot": {
+            "handlers": ["normal_console"],
+            "level": LOG_LEVEL,
+        },
+        "rq.worker": {
+            "handlers": ["normal_console"],
+            "level": LOG_LEVEL,
+        },
+    },
+    "root": {
+        "handlers": ["normal_console"],
+        "level": LOG_LEVEL,
+    },
+}
