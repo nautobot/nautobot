76d6a62c321da764dc5aae7cebd5c6edc07af877
jathanism <jathan@gmail.com>
2021-03-31 21:03:45 +0000

    Added context processors for SSO auth template variables
    - Updated nav to check for `SSO_AUTH_ENABLED`
    - SAML "idp" URL fragment only emitted if SAML is configured.
      - First and only IDP will be returned.

--- nautobot/core/context_processors.py
+++ nautobot/core/context_processors.py
@@ -1,13 +1,53 @@
 from django.conf import settings as django_settings
+from django.utils.functional import lazy
 
+from nautobot.core.settings_funcs import sso_auth_enabled
 from nautobot.extras.registry import registry
+
+
+def get_saml_idp():
+    """
+    Context function to provide a query string for the first IDP configured for SAML.
+
+    If the configured SAML IDP is `google`, this returns `idp=google`.
+
+    If SAML is not configured, this returns `None`.
+    """
+
+    idp_map = getattr(django_settings, 'SOCIAL_AUTH_SAML_ENABLED_IDPS', None)
+
+    # We will only retrieve the first and only IDP defined as we cannot support
+    # more than a single IDP for SAML at this time until we come up with a more
+    # robust login system.
+    value = ''
+    if idp_map is not None:
+        try:
+            idp = list(idp_map.keys())[0]
+        except IndexError:
+            pass
+        else:
+            value = f"idp={idp}"
+
+    return value
 
 
 def settings_and_registry(request):
     """
     Expose Django settings and Nautobot registry stores in the template context. Example: {{ settings.DEBUG }}
     """
+
     return {
         "settings": django_settings,
         "registry": registry,
+    }
+
+
+def sso_auth(request):
+    """
+    Expose SSO-related variables for use in generating login URL fragments for external authentication providers. 
+    """
+
+    return {
+        "SAML_IDP": get_saml_idp,
+        "SSO_AUTH_ENABLED": lazy(lambda: sso_auth_enabled(django_settings.AUTHENTICATION_BACKENDS))
     }
