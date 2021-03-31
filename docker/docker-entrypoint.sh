904d360d740e6a7f5189b5831ee76337a829bae5
nniehoff <github@nickniehoff.net>
2021-03-31 13:33:41 -0600

    Post Upgrade

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,7 +8,7 @@ set -e
 DB_WAIT_TIMEOUT=${DB_WAIT_TIMEOUT-3}
 MAX_DB_WAIT_TIME=${MAX_DB_WAIT_TIME-30}
 CUR_DB_WAIT_TIME=0
-while ! nautobot-server migrate 2>&1 && [ "${CUR_DB_WAIT_TIME}" -lt "${MAX_DB_WAIT_TIME}" ]; do
+while ! nautobot-server post_upgrade 2>&1 && [ "${CUR_DB_WAIT_TIME}" -lt "${MAX_DB_WAIT_TIME}" ]; do
   echo "⏳ Waiting on DB... (${CUR_DB_WAIT_TIME}s / ${MAX_DB_WAIT_TIME}s)"
   sleep "${DB_WAIT_TIMEOUT}"
   CUR_DB_WAIT_TIME=$(( CUR_DB_WAIT_TIME + DB_WAIT_TIMEOUT ))
@@ -17,9 +17,6 @@ if [ "${CUR_DB_WAIT_TIME}" -ge "${MAX_DB_WAIT_TIME}" ]; then
   echo "❌ Waited ${MAX_DB_WAIT_TIME}s or more for the DB to become ready."
   exit 1
 fi
-
-# Collect Static files (still required for uwsgi static files)
-nautobot-server collectstatic --no-input
 
 # Create Superuser if required
 if [ "$NAUTOBOT_CREATE_SUPERUSER" == "true" ]; then
