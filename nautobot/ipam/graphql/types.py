4cd09b7af2e4644f58f577cf4e53cee88e9d5450
Jathan McCollum <jathan@gmail.com>
2021-04-08 07:44:41 -0700

    Fix all the GraphQL search/filtering issues (#246)
    - Add discrete GraphQL types in `nautobot.ipam.graphql.types` to account for the "virtual" (computed) model fields on these objects, specifically: `Aggregate.prefix`, `IPAddress.address`, `Prefix.prefix` (each of which have their corresponding `Type` object)
    - Asserted that these types are registered in `nautobot.core.graphql.schema`
    - Refactored `nautobot.core.graphql.generators.generate_list_search_parameters()` to call `graphene_django.filter.utils.get_filtering_args_from_filterset()` to generate the search filter params instead of doing it ourselves.
      - The `type` field is included now, but renamed to `_type` in search_params overload for `generate_list_search_parameters`
      - It patches the filterset to overload filter fields from `filterset.filters` to `filterset.base_filters` so GraphQL schema generation for list resolver query parameters detection/generation will correctly detect the custom fields and type them in the GQL schema.
    - Refactored `nautobot.core.graphql.generators.generate_list_resolver()` to just pass through kwargs  since the search params are being correctly generated by `get_filtering_args_from_filterset()`.
    - Updated `nautobot.ipam.fields.IPNetworkFormField` to have `from_db_value`, `to_python`, and `get_db_prep_value`
      methods to facilitate bi-directional translation of `IPNetwork` <=> `varbinary` (packed binary int). All translation is now automatically handled by the field itself.
    - Updated IPAM network models to cast objects to `str` vs. `bytes` on repr/deconstruct
    - Updated IPAM network querysets to just pass objects through and not cast to `bytes`
    - Updated `IPAddress` UI view to not cast `host` field to byteas and just pass it through to queryset
    - Also update tests for new schema generation (`String!` is now `[ID]!` in this test case)
    - Added unit tests and docstrings for `VarbinaryIPField`.
    
    Co-authored-by: carbonarok <carbonarok@gmail.com>
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>

--- nautobot/ipam/graphql/types.py
+++ nautobot/ipam/graphql/types.py
@@ -1,13 +1,40 @@
+import graphene
 from graphene_django import DjangoObjectType
+from graphene_django.converter import convert_django_field, convert_field_to_string
 
-from nautobot.ipam.models import IPAddress
-from nautobot.ipam.filters import IPAddressFilterSet
+from nautobot.ipam import models, filters, fields
 from nautobot.extras.graphql.types import TagType  # noqa: F401
+
+
+# Register VarbinaryIPField to be converted to a string type
+convert_django_field.register(fields.VarbinaryIPField)(convert_field_to_string)
+
+
+class AggregateType(DjangoObjectType):
+    """Graphql Type Object for Aggregate model."""
+
+    prefix = graphene.String()
+
+    class Meta:
+        model = models.Aggregate
+        filterset_class = filters.AggregateFilterSet
 
 
 class IPAddressType(DjangoObjectType):
     """Graphql Type Object for IPAddress model."""
 
+    address = graphene.String()
+
     class Meta:
-        model = IPAddress
-        filterset_class = IPAddressFilterSet
+        model = models.IPAddress
+        filterset_class = filters.IPAddressFilterSet
+
+
+class PrefixType(DjangoObjectType):
+    """Graphql Type Object for Prefix model."""
+
+    prefix = graphene.String()
+
+    class Meta:
+        model = models.Prefix
+        filterset_class = filters.PrefixFilterSet
