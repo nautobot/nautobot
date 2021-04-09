5fd5774f2b34e092ed513775d7d5889c115976b9
nniehoff <github@nickniehoff.net>
2021-04-09 09:12:26 -0600

    Adding healthcheck to fail on start if invalid config

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,7 +8,7 @@ set -e
 DB_WAIT_TIMEOUT=${DB_WAIT_TIMEOUT-3}
 MAX_DB_WAIT_TIME=${MAX_DB_WAIT_TIME-30}
 CUR_DB_WAIT_TIME=0
-while ! nautobot-server post_upgrade 2>&1 && [ "${CUR_DB_WAIT_TIME}" -lt "${MAX_DB_WAIT_TIME}" ]; do
+while ! nautobot-server post_upgrade --no-invalidate-all 2>&1 && [ "${CUR_DB_WAIT_TIME}" -lt "${MAX_DB_WAIT_TIME}" ]; do
   echo "⏳ Waiting on DB... (${CUR_DB_WAIT_TIME}s / ${MAX_DB_WAIT_TIME}s)"
   sleep "${DB_WAIT_TIMEOUT}"
   CUR_DB_WAIT_TIME=$(( CUR_DB_WAIT_TIME + DB_WAIT_TIMEOUT ))
@@ -16,6 +16,15 @@ done
 if [ "${CUR_DB_WAIT_TIME}" -ge "${MAX_DB_WAIT_TIME}" ]; then
   echo "❌ Waited ${MAX_DB_WAIT_TIME}s or more for the DB to become ready."
   exit 1
+fi
+
+# Run a quick healthcheck and bail if something fails, --deploy will warn on potential issues for production
+echo "⏳ Running initial systems check..."
+nautobot-server check --deploy
+RC=$?
+if [[ $RC != 0 ]]; then
+	echo -e "❌ Nautobot systems check failed!"
+	exit $RC
 fi
 
 # Create Superuser if required
