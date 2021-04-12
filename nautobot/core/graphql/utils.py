067db08117f5992674769c5f670b4bb23bf0e061
Damien Garros <dgarros@gmail.com>
2021-04-12 10:29:09 -0400

    Feedback from glenn

--- nautobot/core/graphql/utils.py
+++ nautobot/core/graphql/utils.py
@@ -13,49 +13,49 @@ def str_to_var_name(verbose_name):
     return verbose_name.lower().replace(" ", "_").replace("-", "_")
 
 
-def get_filtering_args_from_filterset(filterset):
+def get_filtering_args_from_filterset(filterset_class):
     """Generate a list of filter arguments from a filterset.
 
     The FilterSet class will be instantiated before extracting the list of arguments to
-    account for dynamic filters, inserted when the class is instanciated. (required for Custom Fields filters).
+    account for dynamic filters, inserted when the class is instantiated. (required for Custom Fields filters).
 
-    Filter field that are inheriting from BooleanFilter and NumberFilter will be converted
+    Filter fields that are inheriting from BooleanFilter and NumberFilter will be converted
     to their appropriate type, everything else will be of type String.
-    if the field field is a subclass of MultipleChoiceFilter, the argument will be converted as a list
+    if the filter field is a subclass of MultipleChoiceFilter, the argument will be converted as a list
 
     Args:
-        filterset(FilterSet): FilterSet class used to extract the argument
+        filterset_class(FilterSet): FilterSet class used to extract the argument
 
     Returns:
         dict(graphene.Argument): Filter Arguments organized in a dictionary
     """
 
     args = {}
-    instance = filterset()
+    instance = filterset_class()
 
     for filter_name, filter_field in instance.filters.items():
 
         field_type = graphene.String
         filter_field_class = type(filter_field)
 
-        if issubclass(filter_field_class, BooleanFilter):
-            field_type = graphene.Boolean
-
-        elif issubclass(filter_field_class, NumberFilter):
-            field_type = graphene.Int
-
         if issubclass(filter_field_class, MultiValueNumberFilter):
             field_type = graphene.List(graphene.Int)
+        else:
+            if issubclass(filter_field_class, BooleanFilter):
+                field_type = graphene.Boolean
+            elif issubclass(filter_field_class, NumberFilter):
+                field_type = graphene.Int
+            else:
+                field_type = graphene.String
 
-        elif issubclass(filter_field_class, MultipleChoiceFilter):
-            field_type = graphene.List(field_type)
+            if issubclass(filter_field_class, MultipleChoiceFilter):
+                field_type = graphene.List(field_type)
 
-        if field_type:
-            args[filter_name] = graphene.Argument(
-                field_type,
-                description=filter_field.label,
-                required=False,
-            )
+        args[filter_name] = graphene.Argument(
+            field_type,
+            description=filter_field.label,
+            required=False,
+        )
 
     # Hack to swap `type` fields to `_type` since they will conflict with
     # `graphene.types.fields.Field.type` in Graphene 2.x.
