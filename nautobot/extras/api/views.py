b2dcaba33bb5a2b26d369e0d43d94376ea157f04
carbonarok <carbonarok@gmail.com>
2021-04-13 09:43:23 +0100

    Added testing for sync API endpoint

--- nautobot/extras/api/views.py
+++ nautobot/extras/api/views.py
@@ -140,7 +140,6 @@ class GitRepositoryViewSet(CustomFieldModelViewSet):
     """
     Manage the use of Git repositories as external data sources.
     """
-
     queryset = GitRepository.objects.all()
     serializer_class = serializers.GitRepositorySerializer
     filterset_class = filters.GitRepositoryFilterSet
