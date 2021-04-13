c58536e77c84b35294c06ad679dfd102d9565034
AndyA <andy.wm.arthur@gmail.com>
2021-04-13 16:05:48 -0700

    Update nautobot/ipam/querysets.py
    Co-authored-by: Jathan McCollum <jathan@gmail.com>

--- nautobot/ipam/querysets.py
+++ nautobot/ipam/querysets.py
@@ -251,7 +251,7 @@ class IPAddressQuerySet(RestrictedQuerySet):
         hextets = value.split(":")
 
         # Before we normalize, check that final value is a digit.
-        if hextets[-1].alnum():
+        if hextets[-1].isalnum():
             fill_zeroes = False  # Leave "" in there.
             prefix_len = 128  # Force /128
         else:
