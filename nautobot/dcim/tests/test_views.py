1c18b90cc578dcc6abd6e9cd0ded83706786c107
Josh VanDeraa <jv@networktocode.com>
2021-04-14 11:09:23 -0500

    Updates to use create

--- nautobot/dcim/tests/test_views.py
+++ nautobot/dcim/tests/test_views.py
@@ -1997,8 +1997,7 @@ class PowerFeedTestCase(ViewTestCases.PrimaryObjectViewTestCase):
 
         powerfeed = self.powerfeeds[0]
 
-        cable = Cable(termination_a=powerport, termination_b=powerfeed, status=Status.objects.get(slug="connected"))
-        cable.save()
+        cable = Cable.objects.create(termination_a=powerport, termination_b=powerfeed, status=Status.objects.get(slug="connected"))
 
         url = reverse("dcim:powerfeed", kwargs=dict(pk=powerfeed.pk))
         self.assertHttpStatus(self.client.get(url), 200)
