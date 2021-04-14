4cb67336bb4768a8bf0fac281c38d6afbbb01c98
AndyA <andy.wm.arthur@gmail.com>
2021-04-14 08:16:28 -0700

    Andy/jsonbagg (#217)
    - JSONBAgg for MySQL and Postgres
    - removed dependecy on JSON Aggregation Ordering
    
    Co-authored-by: Jathan McCollum <jathan@gmail.com>

--- nautobot/utilities/query_functions.py
+++ nautobot/utilities/query_functions.py
@@ -1,4 +1,5 @@
-from django.contrib.postgres.aggregates import JSONBAgg
+from django.db.models import Aggregate, JSONField, Value
+
 from django.contrib.postgres.aggregates.mixins import OrderableAggMixin
 from django.db import NotSupportedError
 from django.db.models import Func
@@ -24,15 +25,29 @@ class CollateAsChar(Func):
         return super().as_sql(compiler, connection, function, template, arg_joiner, **extra_context)
 
 
-class OrderableJSONBAgg(OrderableAggMixin, JSONBAgg):
-    """
-    TODO in Django 3.2 ordering is supported natively on JSONBAgg so this is no longer needed.
-    """
+class JSONBAgg(Aggregate):
+    function = None
+    output_field = JSONField()
 
-    template = "%(function)s(%(distinct)s%(expressions)s %(ordering)s)"
+    # Borrowed from `django.contrib.postrgres.aggregates.JSONBagg`.
+    def convert_value(self, value, expression, connection):
+        if not value:
+            return "[]"
+        return value
+
+    def as_sql(self, compiler, connection, **extra_context):
+        engine = connection.settings_dict["ENGINE"]
+        if not JSONBAgg.function:
+            if "postgres" in engine:
+                JSONBAgg.function = "JSONB_AGG"
+            elif "mysql" in engine:
+                JSONBAgg.function = "JSON_ARRAYAGG"
+            else:
+                raise ConnectionError("Only Postgres and MySQL supported")
+        return super().as_sql(compiler, connection, **extra_context)
 
 
-class EmptyGroupByJSONBAgg(OrderableJSONBAgg):
+class EmptyGroupByJSONBAgg(JSONBAgg):
     """
     JSONBAgg is a builtin aggregation function which means it includes the use of a GROUP BY clause.
     When used as an annotation for collecting config context data objects, the GROUP BY is
@@ -42,3 +57,11 @@ class EmptyGroupByJSONBAgg(OrderableJSONBAgg):
     """
 
     contains_aggregate = False
+
+
+class OrderableJSONBAgg(OrderableAggMixin, JSONBAgg):
+    """
+    TODO in Django 3.2 ordering is supported natively on JSONBAgg so this is no longer needed.
+    """
+
+    template = "%(function)s(%(distinct)s%(expressions)s %(ordering)s)"
