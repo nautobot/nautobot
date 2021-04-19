5e1ed8f942cf83da0cbdf4515801a591e835b78e
Jathan McCollum <jathan@gmail.com>
2021-04-19 14:10:18 -0700

    Release notes for 1.0.0b4 (#339)
    * Release notes for 1.0.0b4
    * Revisions from PR feedback
    * Update for PR merges
    * Make linting happy from upstream merge issues

--- nautobot/dcim/tests/test_views.py
+++ nautobot/dcim/tests/test_views.py
@@ -5,8 +5,6 @@ import yaml
 
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
-
-from django.contrib.contenttypes.models import ContentType
 from django.test import override_settings
 from django.urls import reverse
 from netaddr import EUI
