288847024c2caf3186c9782ee3d1eac9785d97bc
Glenn Matthews <glenn.matthews@networktocode.com>
2021-05-20 17:33:37 -0400

    Remove unnecessary max_length validator from VarbinaryIPField (#455)
    * Remove incorrect max_length validator. Fixes #454
    
    * Test create with each entry in self.create_data
    
    * Ensure API rejects invalid IPNetwork strings
    
    * Update release-notes
    
    * Simpler test implementation

--- nautobot/ipam/api/nested_serializers.py
+++ nautobot/ipam/api/nested_serializers.py
@@ -1,3 +1,4 @@
+import netaddr
 from rest_framework import serializers
 
 from nautobot.core.api import WritableNestedSerializer
@@ -20,7 +21,17 @@ __all__ = [
 
 class IPFieldSerializer(serializers.CharField):
     def to_representation(self, value):
+        """Convert internal (IPNetwork) representation to API (string) representation."""
         return str(value)
+
+    def to_internal_value(self, value):
+        """Convert API (string) representation to internal (IPNetwork) representation."""
+        try:
+            return netaddr.IPNetwork(value)
+        except netaddr.AddrFormatError:
+            raise serializers.ValidationError("Invalid IP address format: {}".format(value))
+        except (TypeError, ValueError) as e:
+            raise serializers.ValidationError(e)
 
 
 #
