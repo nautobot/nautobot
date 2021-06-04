1bbe633b91d1a0d35b5df69e79846c94e42cdae1
Stephen Kiely <stephenkiely87@gmail.com>
2021-06-04 17:38:46 -0500

    Give the user some help on empty repos.
    Move the commit.hexsha check as an empty repo won't have a commit.
    This allows the user to fix the repo without needing to delete the git
    repository record.

--- nautobot/utilities/git.py
+++ nautobot/utilities/git.py
@@ -9,6 +9,10 @@ from git import Repo
 logger = logging.getLogger("nautobot.utilities.git")
 
 
+class BranchDoesNotExist(Exception):
+    pass
+
+
 class GitRepo:
     def __init__(self, path, url):
         """
@@ -29,13 +33,12 @@ class GitRepo:
         """
         Check out the given branch, and optionally the specified commit within that branch.
         """
-        # Short-circuit logic - do we already have this commit checked out?
-        if commit_hexsha == self.repo.head.commit.hexsha:
-            logger.debug(f"Commit {commit_hexsha} is already checked out.")
-            return commit_hexsha
-
         self.fetch()
         if commit_hexsha:
+            # Short-circuit logic - do we already have this commit checked out?
+            if commit_hexsha == self.repo.head.commit.hexsha:
+                logger.debug(f"Commit {commit_hexsha} is already checked out.")
+                return commit_hexsha
             # Sanity check - GitPython doesn't provide a handy API for this so we just call a raw Git command:
             # $ git branch <branch> --contains <commit>
             # prints the branch name if it DOES contain the commit, and nothing if it DOES NOT contain the commit.
@@ -48,8 +51,17 @@ class GitRepo:
         if branch in self.repo.heads:
             branch_head = self.repo.heads[branch]
         else:
-            branch_head = self.repo.create_head(branch, self.repo.remotes.origin.refs[branch])
-            branch_head.set_tracking_branch(self.repo.remotes.origin.refs[branch])
+            try:
+                branch_head = self.repo.create_head(branch, self.repo.remotes.origin.refs[branch])
+                branch_head.set_tracking_branch(self.repo.remotes.origin.refs[branch])
+            except IndexError as git_error:
+                logger.error(
+                    "Branch %s does not exist at %s. %s", branch, list(self.repo.remotes.origin.urls)[0], git_error
+                )
+                raise BranchDoesNotExist(
+                    f"Please create branch '{branch}' in upstream and try again."
+                    f" If this is a new repo, please add a commit before syncing. {git_error}"
+                )
 
         logger.info(f"Checking out latest commit on branch `{branch}`...")
         branch_head.checkout()
