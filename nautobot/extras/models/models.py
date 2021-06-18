5a9b33d3b5d93c498ce38b169befae79b4127e3d
John Anderson <lampwins@gmail.com>
2021-06-18 14:52:10 -0400

    add object based schema validation checks

--- nautobot/extras/models/models.py
+++ nautobot/extras/models/models.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core.serializers.json import DjangoJSONEncoder
-from django.core.validators import ValidationError
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.http import HttpResponse
 from django.urls import reverse
@@ -577,6 +577,18 @@ class ConfigContextSchema(OrganizationalModel):
             Draft7Validator.check_schema(self.data_schema)
         except SchemaError as e:
             raise ValidationError({"data_schema": e.message})
+
+        if (
+            type(self.data_schema) is not dict
+            or "properties" not in self.data_schema
+            or self.data_schema.get("type") != "object"
+        ):
+            raise ValidationError(
+                {
+                    "data_schema": "Nautobot only supports context data in form of an object and thus the "
+                    "JSON schema must be of type object and specify a set of properties."
+                }
+            )
 
 
 #
