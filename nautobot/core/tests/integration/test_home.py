817c08d8b03c66fcdc48433efd9df2ba43225204
carbonarok <carbonarok@gmail.com>
2021-07-08 14:27:49 +0100

    Removed bad code.

--- nautobot/core/tests/integration/test_home.py
+++ nautobot/core/tests/integration/test_home.py
@@ -5,48 +5,13 @@ class HomeTestCase(SeleniumTestCase):
     """Integration tests against the home page."""
 
     fixtures = ["user-data.json"]  # bob/bob
-    homepage_layout = [
-        {
-                "Organization": [
-                "Sites",
-                "Tenants"
-            ],
-            "Power": [
-                "Power Feeds",
-                "Power Panels",
-            ]
-        },
-        {
-            "Circuits": [
-                "Providers",
-                "Circuits",
-            ],
-            "Virtualization": [
-                "Clusters",
-                "Virtual Machines",
-            ]
-        }
-    ]
-
-    def setUp(self):
-        super().setUp()
-        self.login(self.user.username, self.password)
-
-    def tearDown(self):
-        self.logout()
-        super().tearDown()
 
     def test_login(self):
         """
         Perform a UI login.
         """
-        self.load_page(self.live_server_url)
+
+        self.login("bob", "bob")
+
         # Wait for the page to render and make sure we got a body.
         self.selenium.wait_for_html("body")
-
-    def test_homepage_panels(self):
-        """
-        Render homepage panels.
-        """
-        self.load_page(self.live_server_url)
-        for column in self.homepage_layout:
