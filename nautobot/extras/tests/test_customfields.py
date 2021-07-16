ccce638ec3106d9456d8cd15f04ba2c18f97f890
Glenn Matthews <glenn.matthews@networktocode.com>
2021-07-16 09:14:11 -0400

    Add rendered value check to test

--- nautobot/extras/tests/test_customfields.py
+++ nautobot/extras/tests/test_customfields.py
@@ -1230,8 +1230,12 @@ class CustomFieldTableTest(TestCase):
             "date_field": "2020-01-02",
             "url_field": '<a href="http://example.com/2">http://example.com/2</a>',
             "choice_field": '<span class="label label-default">Bar</span>',
-            "multi_choice_field": '<span class="label label-default">Bar</span> <span class="label label-default">Baz</span>',
+            "multi_choice_field": (
+                '<span class="label label-default">Bar</span> <span class="label label-default">Baz</span> '
+            ),
         }
+        
+        bound_row = site_table.rows[0]
 
         for col_name, col_expected_value in custom_column_expected.items():
             internal_col_name = "cf_" + col_name
@@ -1239,4 +1243,5 @@ class CustomFieldTableTest(TestCase):
             self.assertIsNotNone(custom_column)
             self.assertIsInstance(custom_column, CustomFieldColumn)
 
-            # TODO: check rendered content
+            rendered_value = bound_row.get_cell(internal_col_name)
+            self.assertEqual(rendered_value, col_expected_value)
