2a717ef47b672486ceacf853821d847ed8861513
Glenn Matthews <glenn.matthews@networktocode.com>
2021-08-11 15:58:15 -0400

    Fix #779. Port of https://github.com/netbox-community/netbox/commit/20a85c1ef264ecfffcbe8602ab103baed5a7cf5b

--- nautobot/tenancy/tables.py
+++ nautobot/tenancy/tables.py
@@ -16,13 +16,32 @@ MPTT_LINK = """
 <a href="{{ record.get_absolute_url }}">{{ record.name }}</a>
 """
 
-COL_TENANT = """
-{% if record.tenant %}
-    <a href="{% url 'tenancy:tenant' slug=record.tenant.slug %}" title="{{ record.tenant.description }}">{{ record.tenant }}</a>
-{% else %}
-    &mdash;
-{% endif %}
-"""
+
+#
+# Table columns
+#
+
+
+class TenantColumn(tables.TemplateColumn):
+    """
+    Column for linking to a record's associated Tenant, or failing that, it's associated VRF's tenant.
+    """
+
+    template_code = """
+    {% if record.tenant %}
+        <a href="{{ record.tenant.get_absolute_url }}" title="{{ record.tenant.description }}">{{ record.tenant }}</a>
+    {% elif record.vrf.tenant %}
+        <a href="{{ record.vrf.tenant.get_absolute_url }}" title="{{ record.vrf.tenant.description }}">{{ record.vrf.tenant }}</a>*
+    {% else %}
+        &mdash;
+    {% endif %}
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(template_code=self.template_code, *args, **kwargs)
+
+    def value(self, value):
+        return str(value)
 
 
 #
