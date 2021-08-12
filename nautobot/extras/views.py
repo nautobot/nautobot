0a9e861be402b9c01d43166d7ec31b9d5620c4cf
Veit Heller <veit@veitheller.de>
2021-08-12 20:24:44 +0200

    Incorporate feedback by @glennmatthews
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>

--- nautobot/extras/views.py
+++ nautobot/extras/views.py
@@ -880,7 +880,7 @@ class JobApprovalRequestView(ContentTypePermissionRequiredMixin, View):
     def get(self, request, scheduled_job):
         """
         Render the job form with data from the scheduled_job instance, but mark all fields as disabled.
-        We don't care to actually get any data back from the form as we will not every change it.
+        We don't care to actually get any data back from the form as we will not ever change it.
         Instead, we offer the user three submit buttons, dry-run, approve, and deny, which we act upon in the post.
         """
         scheduled_job = get_object_or_404(ScheduledJob, pk=scheduled_job)
