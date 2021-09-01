38e33e3bd085fdbdbd1a30ae4ec0ff13c4532f33
hellerve <veit@veitheller.de>
2021-09-01 16:55:35 +0200

    Switch to cron scheduler

--- nautobot/extras/models/models.py
+++ nautobot/extras/models/models.py
@@ -1124,16 +1124,19 @@ class ScheduledJob(BaseModel):
             # This is one-time clocked task
             return clocked(clocked_time=self.start_time)
 
-        # A week is 7 days, otherwise the iteration is set to 1
-        every = 7 if self.interval == JobExecutionType.TYPE_WEEKLY else 1
-        return schedules.schedule(
-            timedelta(**{JobExecutionType.CELERY_INTERVAL_MAP[self.interval]: every}),
-            nowfun=lambda: make_aware(now()),
-        )
+        return self.to_cron()
 
     @staticmethod
     def earliest_possible_time():
         return timezone.now() + timedelta(seconds=15)
+
+    def to_cron(self):
+        if self.interval == TYPE_HOURLY:
+            return schedules.crontab(minute=0)
+        elif self.interval == TYPE_DAILY:
+            return schedules.crontab(minute=0, hour=0)
+        elif self.interval == TYPE_WEEKLY:
+            return schedules.crontab(minute=0, hour=0, day_of_week="mon")
 
 
 signals.pre_delete.connect(ScheduledJobs.changed, sender=ScheduledJob)
