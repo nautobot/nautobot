9f92622d0d2d3fe38752b57803f926adc3bb9a81
carbonarok <carbonarok@gmail.com>
2021-09-30 13:52:22 +0100

    Home renamed. Added styling for popup. Fixed widget for multi object selction.

--- nautobot/core/admin.py
+++ nautobot/core/admin.py
@@ -1,14 +1,14 @@
 from django.contrib.admin import site as admin_site
 from social_django.models import Association, Nonce, UserSocialAuth
 from taggit.models import Tag
-from django.db import models
-from django_celery_beat.admin import ClockedScheduleAdmin as OverrideClockedScheduleAdmin
-from django_celery_beat.models import ClockedSchedule
-from nautobot.utilities.forms.widgets import DateTimePicker
-
-
-class ClockedScheduleAdmin(OverrideClockedScheduleAdmin):
-    formfield_overrides = {models.DateTimeField: {"widget": DateTimePicker}}
+from django_celery_beat import admin
+from django_celery_beat.models import (
+    ClockedSchedule,
+    CrontabSchedule,
+    IntervalSchedule,
+    PeriodicTask,
+    SolarSchedule,
+)
 
 
 # Override default AdminSite attributes so we can avoid creating and
@@ -19,9 +19,12 @@ admin_site.site_title = "Nautobot"
 # Unregister the unused stock Tag model provided by django-taggit
 admin_site.unregister(Tag)
 
-# Re-register ClockedSchedule with custom widget
+# Remove Celery Beat from admin menu
 admin_site.unregister(ClockedSchedule)
-admin_site.register(ClockedSchedule)
+admin_site.unregister(CrontabSchedule)
+admin_site.unregister(IntervalSchedule)
+admin_site.unregister(PeriodicTask)
+admin_site.unregister(SolarSchedule)
 
 # Unregister SocialAuth from Django admin menu
 admin_site.unregister(Association)
