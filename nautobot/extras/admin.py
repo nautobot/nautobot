c5162cafd7862bd9728b4eabb50ad6e69d0666b1
carbonarok <carbonarok@gmail.com>
2021-10-07 13:30:20 +0100

    Removed redefinition of unused import

--- nautobot/extras/admin.py
+++ nautobot/extras/admin.py
@@ -3,10 +3,8 @@ from django import forms
 from django.contrib import admin, messages
 from django.db import transaction
 from django.db.models import ProtectedError
-from django.contrib import admin
 
 from .models import CustomField, CustomFieldChoice, FileProxy, JobResult
-from .models import FileProxy, JobResult
 
 
 def order_content_types(field):
