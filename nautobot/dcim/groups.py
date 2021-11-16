5ef69223f8618a9d3f049c11c959ceab162a5ce0
Damien Garros <dgarros@gmail.com>
2021-11-15 22:27:22 -0500

    Add support for tag, cleanup

--- nautobot/dcim/groups.py
+++ nautobot/dcim/groups.py
@@ -36,34 +36,7 @@ class DeviceDynamicGroupMap(BaseDynamicGroupMap):
     filterset = DeviceFilterSet
     filterform = DeviceFilterForm
 
-    field_order = ["platform", "role", "site"]
-
-    # @classmethod
-    # def get_queryset_filter(cls, obj):
-    #     """Return a queryset filter matching this DynamicGroup parameters."""
-
-    #     queryset_filter = Q()
-
-    #     for field_name in cls.field_order:
-    #         class_name = f"get_queryset_filter_default"
-    #         if hasattr(cls, f"get_queryset_filter_{field_name}"):
-    #             class_name = f"get_queryset_filter_{field_name}"
-
-    #     queryset_filter |= getattr(cls, class_name)(field_name, )
-
-    #     queryset_filter |= Q(filter__role__contains=obj.device_role.slug)
-    #     queryset_filter |= Q(filter__site__contains=obj.site.slug)
-    #     queryset_filter |= Q(filter__platform__contains=obj.platform.slug)
-
-    #     queryset_filter |= Q(filter__region__contains=obj.site.region.slug)
-
-    #     if obj.site.region.parent:
-    #         queryset_filter = Q(filter__region__contains=obj.site.region.parent.slug)
-
-    #     for tag in obj.tags.slugs():
-    #         queryset_filter |= Q(filter__tags__contains=tag)
-
-    #     return queryset_filter
+    field_order = ["platform", "manufacturer", "device_type_id", "role", "site", "tag"]
 
 
 class SiteDynamicGroupMap(BaseDynamicGroupMap):
@@ -72,29 +45,4 @@ class SiteDynamicGroupMap(BaseDynamicGroupMap):
     filterset = SiteFilterSet
     filterform = SiteFilterForm
 
-    field_order = ["region"]
-
-    # region = DynamicModelMultipleChoiceField(
-    #     queryset=Region.objects.all(),
-    #     to_field_name="slug",
-    #     required=False,
-    # )
-
-    # tag = DynamicModelMultipleChoiceField(
-    #     queryset=Tag.objects.all(),
-    #     to_field_name="slug",
-    #     required=False,
-    # )
-
-    # @classmethod
-    # def get_queryset_filter(cls, obj):
-    #     """Return a queryset filter matching this DynamicGroup parameters."""
-    #     queryset_filter = Q(filter__region__contains=obj.region.slug)
-
-    #     if obj.region.parent:
-    #         queryset_filter = Q(filter__region__contains=obj.region.parent.slug)
-
-    #     for tag in obj.tags.slugs():
-    #         queryset_filter |= Q(filter__tag__contains=tag)
-
-    #     return queryset_filter
+    field_order = ["tag"]
