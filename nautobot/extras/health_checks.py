eeaaae0e44f978b2f1928bdb9cb16e4350c0860c
Florian LACOMMARE <florian_lacommare@carrefour.com>
2022-03-22 12:49:23 +0100

    - Remove hardcoded default value

--- nautobot/extras/health_checks.py
+++ nautobot/extras/health_checks.py
@@ -33,29 +33,31 @@ class DatabaseBackend(BaseHealthCheckBackend):
 class RedisBackend(BaseHealthCheckBackend):
     """Health check for Redis."""
 
-    redis_url = getattr(settings, "CACHEOPS_REDIS", "redis://localhost:6379/1")
+    redis_url = getattr(settings, "CACHEOPS_REDIS", None)
     sentinel_url = getattr(settings, "CACHEOPS_SENTINEL", None)
 
     def check_status(self):
         """Check Redis service by pinging the redis instance with a redis connection."""
-        # if CACHEOPS_REDIS is set to False or Sentinel is enabled
-        # We need to check Redis using Sentinel
-        if not self.redis_url or self.sentinel_url is not None:
+        # if Sentinel is enabled we need to check Redis using Sentinel
+        if self.sentinel_url is not None:
             try:
                 sentinel = Sentinel(self.sentinel_url["locations"], socket_timeout=self.sentinel_url["socket_timeout"])
                 with sentinel.master_for(self.sentinel_url["service_name"], db=self.sentinel_url["db"]) as master:
                     master.ping()
             except (ConnectionRefusedError, exceptions.ConnectionError, exceptions.TimeoutError) as e:
                 self.add_error(ServiceUnavailable("Unable to connect to Redis Sentinel: %s" % type(e).__name__), e)
-            except BaseException as e:
+            except Exception as e:
                 self.add_error(ServiceUnavailable("Unknown error"), e)
         # Sentinel is not used, so we check Redis directly
         else:
-            try:
-                # conn is used as a context to release opened resources later
-                with from_url(self.redis_url) as conn:
-                    conn.ping()  # exceptions may be raised upon ping
-            except (ConnectionRefusedError, exceptions.ConnectionError, exceptions.TimeoutError) as e:
-                self.add_error(ServiceUnavailable("Unable to connect to Redis: %s" % type(e).__name__), e)
-            except BaseException as e:
-                self.add_error(ServiceUnavailable("Unknown error"), e)
+            if self.redis_url is None:
+                self.add_error(ServiceUnavailable("CACHEOPS_REDIS is not set"), e)
+            else:
+                try:
+                    # conn is used as a context to release opened resources later
+                    with from_url(self.redis_url) as conn:
+                        conn.ping()  # exceptions may be raised upon ping
+                except (ConnectionRefusedError, exceptions.ConnectionError, exceptions.TimeoutError) as e:
+                    self.add_error(ServiceUnavailable("Unable to connect to Redis: %s" % type(e).__name__), e)
+                except Exception as e:
+                    self.add_error(ServiceUnavailable("Unknown error"), e)
