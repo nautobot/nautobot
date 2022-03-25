8460f791f742299b61bc7ac3e86259c47989d57b
Matt Vitale <matt.vitale@networktocode.com>
2022-03-25 16:17:01 -0400

    DRY

--- nautobot/project-static/js/theme.js
+++ nautobot/project-static/js/theme.js
@@ -1,18 +1,21 @@
 // Capture the current theme from local storage and adjust the page to use the current theme.
 const htmlEl = document.getElementsByTagName('html')[0];
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
 if (currentTheme) {
     htmlEl.dataset.theme = currentTheme;
     
     // Toggle footer theme button to toggle
     if (currentTheme == "dark") {
-        document.getElementById("btn-light-theme").style.display = '';
-        document.getElementById("btn-dark-theme").style.display = 'none';
+        displayLightBtn();
     }
     else {
-        document.getElementById("btn-light-theme").style.display = 'none';
-        document.getElementById("btn-dark-theme").style.display = '';
+        displayDarkBtn();
     }
+}
+else {
+    // Default to light theme
+    displayDarkBtn();
 }
 // When the user changes the theme, we need to save the new value on local storage
 const toggleTheme = (theme) => {
@@ -21,11 +24,18 @@ const toggleTheme = (theme) => {
     
     // Toggle footer theme button to toggle
     if (theme == "dark") {
-        document.getElementById("btn-light-theme").style.display = '';
-        document.getElementById("btn-dark-theme").style.display = 'none';
+        displayLightBtn();
     }
     else {
-        document.getElementById("btn-light-theme").style.display = 'none';
-        document.getElementById("btn-dark-theme").style.display = '';
+        displayDarkBtn();
     }
+}
+
+function displayDarkBtn() {
+    document.getElementById("btn-light-theme").style.display = 'none';
+    document.getElementById("btn-dark-theme").style.display = '';
+}
+function displayLightBtn() {
+    document.getElementById("btn-light-theme").style.display = '';
+    document.getElementById("btn-dark-theme").style.display = 'none';
 }
