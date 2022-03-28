3a1896297bd0cc717fa5c78490f8161c6cfd4dd4
Matt Vitale <matt.vitale@networktocode.com>
2022-03-28 08:35:51 -0400

    Auto-detect system theme settings.  Enable user override

--- nautobot/project-static/js/theme.js
+++ nautobot/project-static/js/theme.js
@@ -2,7 +2,9 @@
 const htmlEl = document.getElementsByTagName('html')[0];
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
+// CurrentTheme overrides auto-detection when specified by manually clicking theme button
 if (currentTheme) {
+    // Set theme setting to HTML dataset element, for CSS rendering
     htmlEl.dataset.theme = currentTheme;
     
     // Toggle footer theme button to toggle
@@ -14,11 +16,18 @@ if (currentTheme) {
     }
 }
 else {
-    // Default to light theme
-    displayDarkBtn();
+    // If user changes system theme, detect and change theme automatically
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) {
+        // Detect and set theme based on what it was just changed to
+        detectThemeSettings()
+    })
+    // Attempt to detect current system theme preferences and set theme to match
+    detectThemeSettings()
 }
-// When the user changes the theme, we need to save the new value on local storage
+
+// When the user manually changes the theme, we need to save the new value on local storage
 const toggleTheme = (theme) => {
+    // Only set in local storage if user manually specifies a theme, enabling system/browser theme override
     htmlEl.dataset.theme = theme;
     localStorage.setItem('theme', theme);
     
@@ -31,6 +40,22 @@ const toggleTheme = (theme) => {
     }
 }
 
+function detectThemeSettings() {
+    // Attempt to detect current system theme preferences
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // Dark mode
+        setDarkTheme();
+    }
+    else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+        // Light mode
+        setLightTheme();
+    }
+    else {
+        // Default to light theme
+        setLightTheme();
+    }
+}
+
 function displayDarkBtn() {
     document.getElementById("btn-light-theme").style.display = 'none';
     document.getElementById("btn-dark-theme").style.display = '';
@@ -38,4 +63,14 @@ function displayDarkBtn() {
 function displayLightBtn() {
     document.getElementById("btn-light-theme").style.display = '';
     document.getElementById("btn-dark-theme").style.display = 'none';
+}
+
+function setDarkTheme() {
+    htmlEl.dataset.theme = "dark";
+    displayLightBtn();
+}
+
+function setLightTheme() {
+    htmlEl.dataset.theme = "light";
+    displayDarkBtn();
 }
