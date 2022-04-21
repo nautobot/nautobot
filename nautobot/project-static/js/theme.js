28abc92f607715af45857c815f0800e246edd8ca
Matt Vitale <matt.vitale@networktocode.com>
2022-04-20 20:52:52 -0400

    Add new theme modal

--- nautobot/project-static/js/theme.js
+++ nautobot/project-static/js/theme.js
@@ -1,18 +1,17 @@
-// Capture the current theme from local storage and adjust the page to use the current theme.
 const htmlEl = document.getElementsByTagName('html')[0];
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 // CurrentTheme overrides auto-detection when specified by manually clicking theme button
-if (currentTheme) {
+if (currentTheme && currentTheme != "system") {
     // Set theme setting to HTML dataset element, for CSS rendering
     htmlEl.dataset.theme = currentTheme;
-    
-    // Toggle footer theme button to toggle
-    if (currentTheme == "dark") {
-        displayLightBtn();
+
+    // Set theme to light or dark if manually specified
+    if (currentTheme == "light") {
+        setLightTheme();
     }
-    else {
-        displayDarkBtn();
+    else if (currentTheme == "dark") {
+        setDarkTheme();
     }
 }
 else {
@@ -31,13 +30,23 @@ const toggleTheme = (theme) => {
     htmlEl.dataset.theme = theme;
     localStorage.setItem('theme', theme);
     
-    // Toggle footer theme button to toggle
-    if (theme == "dark") {
-        displayLightBtn();
+    if (theme == "system") {
+        // If user changes system theme, detect and change theme automatically
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) {
+            // Detect and set theme based on what it was just changed to
+            detectThemeSettings()
+        })
+        // Attempt to detect current system theme preferences and set theme to match
+        detectThemeSettings()
     }
-    else {
-        displayDarkBtn();
+    else if (theme == "dark") {
+        setDarkTheme();
     }
+    else if (theme == "light") {
+        setLightTheme();
+    }
+    // Reload page after setting theme
+    document.location.reload(true);
 }
 
 function detectThemeSettings() {
@@ -56,58 +65,10 @@ function detectThemeSettings() {
     }
 }
 
-function displayDarkBtn() {
-    document.getElementById("btn-light-theme").style.display = 'none';
-    document.getElementById("btn-dark-theme").style.display = '';
-}
-function displayLightBtn() {
-    document.getElementById("btn-light-theme").style.display = '';
-    document.getElementById("btn-dark-theme").style.display = 'none';
-}
-
 function setDarkTheme() {
     htmlEl.dataset.theme = "dark";
-    displayLightBtn();
 }
 
 function setLightTheme() {
     htmlEl.dataset.theme = "light";
-    displayDarkBtn();
 }
-
-// $('#theme_modal').on('show.bs.modal', function(event) {
-//     var button = $(event.relatedTarget) // Button that triggered the modal
-  
-//     var modal = $(this)
-  
-//     modal.find('.modal-title').text('Select Nautobot Theme ')
-//     modal.find('.modal-result').load('modals/modal_theme.html')
-//   })
-
-// $('#theme_modal').on('hidden.bs.modal', function() {
-//     var modal = $(this)
-//     modal.find('.modal-title').text('')
-//     modal.find('.modal-result').text('')
-// })
-
-$('#theme_modal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var obj = button.data('obj');
-    var url = button.data('url');
-    var modal_title = $(this).find('.modal-title');
-    var modal_body = $(this).find('.modal-body');
-    modal_title.text(obj);
-    modal_body.empty();
-    $.ajax({
-        url: url,
-        dataType: 'json',
-        success: function(json) {
-            $.each(json, function(i, segment) {
-                modal_body.append(
-                    'abcd' +
-                    '<hr />'
-                );
-            })
-        }
-    });
-});
