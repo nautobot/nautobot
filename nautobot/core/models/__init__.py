00be17800cd78749c8203186b553b043431c7cfd
Dan Bridson <briddo@gmail.com>
2022-04-27 20:14:02 +0100

    Limit access to change password form (on develop branch) (#1696)
    * Implementation
    
    * Removed unnecessary code as per PR comment suggestions and added final test for successful post response on django auth backend
    
    * black
    
    * Warning copy made clearer
    
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>
    
    * PR comments actioned
    
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>

--- nautobot/core/models/__init__.py
+++ nautobot/core/models/__init__.py
@@ -7,7 +7,7 @@ from nautobot.utilities.querysets import RestrictedQuerySet
 
 class BaseModel(models.Model):
     """
-    Base model class that all models should inhert from.
+    Base model class that all models should inherit from.
 
     This abstract base provides globally common fields and functionality.
 
