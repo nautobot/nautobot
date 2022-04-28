d0f868c9f436caed9a12bdf41205d7f613ecbc3a
Jathan McCollum <jathan@gmail.com>
2022-04-28 13:11:19 -0700

    Optimize repeated lookups of `JobResult` objects and related fields (#1699)
    * Optimize repeated lookups of `JobResult` objects and related fields
    
    - Added `prefetch_related` to the UI `JobResultListView.queryset` and API `JobResultViewSet.queryset` to optimize caching of related fields
    - Added `get_job_content_type()` utility function to return a cached ContentType for `extras.Job` so that it can be used by callers to reduce spurious singular queries for this object
    
    * Collapse "job_model", "obj_type", and "related_object" columns in to new, more efficient "linked_record" column
    
    * Fix issues w/ caching `ContentType` for `extras.Job` and test suite
    
    * Add name to default_columns so tests will pass
    
    * Update helper method
    
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>

--- nautobot/extras/models/jobs.py
+++ nautobot/extras/models/jobs.py
@@ -37,7 +37,7 @@ from nautobot.extras.constants import (
 )
 from nautobot.extras.plugins.utils import import_object
 from nautobot.extras.querysets import JobQuerySet, ScheduledJobExtendedQuerySet
-from nautobot.extras.utils import extras_features, FeatureQuery, jobs_in_directory
+from nautobot.extras.utils import get_job_content_type, extras_features, FeatureQuery, jobs_in_directory
 
 from .customfields import CustomFieldModel
 
@@ -452,10 +452,13 @@ class JobResult(BaseModel, CustomFieldModel):
         instance whose PK corresponds to the `job_id`. This behavior is currently unused in the Nautobot core,
         but may be of use to plugin developers wishing to create JobResults that have a one-to-one relationship
         to plugin model instances.
+
+        This method is potentially rather slow as get_job() may need to actually load the Job class from disk;
+        consider carefully whether you actually need to use it.
         """
         from nautobot.extras.jobs import get_job  # needed here to avoid a circular import issue
 
-        if self.obj_type == ContentType.objects.get(app_label="extras", model="job"):
+        if self.obj_type == get_job_content_type():
             # Related object is an extras.Job subclass, our `name` matches its `class_path`
             return get_job(self.name)
 
@@ -481,6 +484,8 @@ class JobResult(BaseModel, CustomFieldModel):
     def related_name(self):
         """
         Similar to self.name, but if there's an appropriate `related_object`, use its name instead.
+
+        Since this calls related_object, the same potential performance concerns exist. Use with caution.
         """
         related_object = self.related_object
         if not related_object:
@@ -488,6 +493,27 @@ class JobResult(BaseModel, CustomFieldModel):
         if hasattr(related_object, "name"):
             return related_object.name
         return str(related_object)
+
+    @property
+    def linked_record(self):
+        """
+        A newer alternative to self.related_object that looks up an extras.models.Job instead of an extras.jobs.Job.
+        """
+        if self.job_model is not None:
+            return self.job_model
+        model_class = self.obj_type.model_class()
+        if model_class is not None:
+            if hasattr(model_class, "name"):
+                try:
+                    return model_class.objects.get(name=self.name)
+                except model_class.DoesNotExist:
+                    pass
+            if hasattr(model_class, "class_path"):
+                try:
+                    return model_class.objects.get(class_path=self.name)
+                except model_class.DoesNotExist:
+                    pass
+        return None
 
     def get_absolute_url(self):
         return reverse("extras:jobresult", kwargs={"pk": self.pk})
