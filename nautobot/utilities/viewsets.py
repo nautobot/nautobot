47a2e9d6ca44e40ce70388a2613d0238d73188d3
Hanlin Miao <hanlinmiao@Hanlins-MBP-2.home>
2022-06-14 16:37:11 -0400

    overrode retrieve() and list() from MVS to replace generic.objectview and genericlistview

---
+++ nautobot/utilities/viewsets.py
@@ -0,0 +1,240 @@
+from django.views.generic import View
+from django.conf import settings
+from django.utils.http import is_safe_url
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import NoReverseMatch, reverse
+from django.contrib.contenttypes.models import ContentType
+from django.contrib import messages
+from django.http import HttpResponse
+from django_tables2 import RequestConfig
+from rest_framework import viewsets
+
+from .permissions import resolve_permission
+from nautobot.utilities.permissions import get_permission_for_model
+from nautobot.utilities.views import ObjectPermissionRequiredMixin
+from nautobot.utilities.templatetags.helpers import validated_viewname
+from nautobot.extras.models import CustomField, ExportTemplate
+from nautobot.utilities.paginator import EnhancedPaginator, get_paginate_count
+from nautobot.utilities.forms import TableConfigForm
+from nautobot.utilities.utils import csv_format
+
+class NautobotViewSet(viewsets.ModelViewSet, ObjectPermissionRequiredMixin, View):
+    queryset = None
+    serializer_class = None
+    template_name = None
+    filterset = None
+    filterset_form = None
+    table = None
+    action_buttons = None
+
+
+    def get_required_permission(self):
+        return get_permission_for_model(self.queryset.model, "view")
+
+    def get_template_name(self):
+        """
+        Return self.template_name if set. Otherwise, resolve the template path by model app_label and name.
+        """
+        if self.template_name is not None:
+            return self.template_name
+        model_opts = self.queryset.model._meta
+        return f"{model_opts.app_label}/{model_opts.model_name}.html"
+    
+    def get_extra_context(self, request, instance):
+        """
+        Return any additional context data for the template.
+
+        Args:
+            request: The current request
+            instance: The object being viewed
+
+        Returns:
+            dict
+        """
+        return {}
+
+    def get_changelog_url(self, instance):
+        """Return the changelog URL for a given instance."""
+        meta = self.queryset.model._meta
+
+        # Don't try to generate a changelog_url for an ObjectChange.
+        if meta.model_name == "objectchange":
+            return None
+
+        route = f"{meta.app_label}:{meta.model_name}_changelog"
+        if meta.app_label in settings.PLUGINS:
+            route = f"plugins:{route}"
+
+        # Iterate the pk-like fields and try to get a URL, or return None.
+        fields = ["pk", "slug"]
+        for field in fields:
+            if not hasattr(instance, field):
+                continue
+
+            try:
+                return reverse(route, kwargs={field: getattr(instance, field)})
+            except NoReverseMatch:
+                continue
+
+        # This object likely doesn't have a changelog route defined.
+        return None
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = get_object_or_404(self.queryset, **kwargs)
+        return render(
+            request,
+            self.get_template_name(),
+            {
+                "object": instance,
+                "verbose_name": self.queryset.model._meta.verbose_name,
+                "verbose_name_plural": self.queryset.model._meta.verbose_name_plural,
+                "changelog_url": self.get_changelog_url(instance),
+                **self.get_extra_context(request, instance),
+            },
+        )
+
+    def queryset_to_yaml(self):
+        """
+        Export the queryset of objects as concatenated YAML documents.
+        """
+        yaml_data = [obj.to_yaml() for obj in self.queryset]
+
+        return "---\n".join(yaml_data)
+
+    def queryset_to_csv(self):
+        """
+        Export the queryset of objects as comma-separated value (CSV), using the model's to_csv() method.
+        """
+        csv_data = []
+        custom_fields = []
+
+        # Start with the column headers
+        headers = self.queryset.model.csv_headers.copy()
+
+        # Add custom field headers, if any
+        if hasattr(self.queryset.model, "_custom_field_data"):
+            for custom_field in CustomField.objects.get_for_model(self.queryset.model):
+                headers.append(custom_field.name)
+                custom_fields.append(custom_field.name)
+
+        csv_data.append(",".join(headers))
+
+        # Iterate through the queryset appending each object
+        for obj in self.queryset:
+            data = obj.to_csv()
+
+            for custom_field in custom_fields:
+                data += (obj.cf.get(custom_field, ""),)
+
+            csv_data.append(csv_format(data))
+
+        return "\n".join(csv_data)
+    
+    def validate_action_buttons(self, request):
+        """Verify actions in self.action_buttons are valid view actions."""
+
+        always_valid_actions = ("export",)
+        valid_actions = []
+        invalid_actions = []
+
+        for action in self.action_buttons:
+            if action in always_valid_actions or validated_viewname(self.queryset.model, action) is not None:
+                valid_actions.append(action)
+            else:
+                invalid_actions.append(action)
+        if invalid_actions:
+            messages.error(request, f"Missing views for action(s) {', '.join(invalid_actions)}")
+        return valid_actions
+    
+    def list(self, request, *args, **kwargs):
+        self.template_name = "generic/object_list.html"
+        self.action_buttons = ("add", "import", "export")
+        model = self.queryset.model
+        content_type = ContentType.objects.get_for_model(model)
+
+        if self.filterset:
+            self.queryset = self.filterset(request.GET, self.queryset).qs
+
+        # Check for export template rendering
+        if request.GET.get("export"):
+            et = get_object_or_404(
+                ExportTemplate,
+                content_type=content_type,
+                name=request.GET.get("export"),
+            )
+            try:
+                return et.render_to_response(self.queryset)
+            except Exception as e:
+                messages.error(
+                    request,
+                    "There was an error rendering the selected export template ({}): {}".format(et.name, e),
+                )
+
+        # Check for YAML export support
+        elif "export" in request.GET and hasattr(model, "to_yaml"):
+            response = HttpResponse(self.queryset_to_yaml(), content_type="text/yaml")
+            filename = "{}{}.yaml".format(
+                settings.BRANDING_PREPENDED_FILENAME, self.queryset.model._meta.verbose_name_plural
+            )
+            response["Content-Disposition"] = 'attachment; filename="{}"'.format(filename)
+            return response
+
+        # Fall back to built-in CSV formatting if export requested but no template specified
+        elif "export" in request.GET and hasattr(model, "to_csv"):
+            response = HttpResponse(self.queryset_to_csv(), content_type="text/csv")
+            filename = "{}{}.csv".format(
+                settings.BRANDING_PREPENDED_FILENAME, self.queryset.model._meta.verbose_name_plural
+            )
+            response["Content-Disposition"] = 'attachment; filename="{}"'.format(filename)
+            return response
+
+        # Provide a hook to tweak the queryset based on the request immediately prior to rendering the object list
+        self.queryset = self.alter_queryset(request)
+
+        # Compile a dictionary indicating which permissions are available to the current user for this model
+        permissions = {}
+        for action in ("add", "change", "delete", "view"):
+            perm_name = get_permission_for_model(model, action)
+            permissions[action] = request.user.has_perm(perm_name)
+
+        # Construct the objects table
+        table = self.table(self.queryset, user=request.user)
+        if "pk" in table.base_columns and (permissions["change"] or permissions["delete"]):
+            table.columns.show("pk")
+
+        # Apply the request context
+        paginate = {
+            "paginator_class": EnhancedPaginator,
+            "per_page": get_paginate_count(request),
+        }
+        RequestConfig(request, paginate).configure(table)
+
+        filter_form = None
+        if self.filterset_form:
+            if request.GET:
+                # Bind form to the values specified in request.GET
+                filter_form = self.filterset_form(request.GET, label_suffix="")
+            else:
+                # Use unbound form with default (initial) values
+                filter_form = self.filterset_form(label_suffix="")
+
+        valid_actions = self.validate_action_buttons(request)
+
+        context = {
+            "content_type": content_type,
+            "table": table,
+            "permissions": permissions,
+            "action_buttons": valid_actions,
+            "table_config_form": TableConfigForm(table=table),
+            "filter_form": filter_form,
+        }
+        context.update(self.extra_context())
+
+        return render(request, self.template_name, context)
+
+    def alter_queryset(self, request):
+        # .all() is necessary to avoid caching queries
+        return self.queryset.all()
+
+    def extra_context(self):
+        return {}
