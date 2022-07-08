92862ce2281ec25456486c406ee423768769d323
Dan Bridson <655255+briddo@users.noreply.github.com>
2022-07-08 22:38:24 +0100

    PR suggestions

--- examples/example_plugin_with_view_override/example_plugin_with_view_override/__init__.py
+++ examples/example_plugin_with_view_override/example_plugin_with_view_override/__init__.py
@@ -9,16 +9,16 @@ __version__ = metadata.version(__name__)
 from nautobot.extras.plugins import PluginConfig
 
 
-class ExamplePluginWithOverridesConfig(PluginConfig):
+class ExamplePluginWithOverrideConfig(PluginConfig):
     name = "example_plugin_with_view_override"
     verbose_name = "Example Plugin With View Override"
     author = "Nautobot development team"
     author_email = "nautobot@example.com"
     version = __version__
     description = "For testing purposes only"
-    base_url = "example-plugin-with-view-overrides"
+    base_url = "example-plugin-with-view-override"
 
 
-config = PluginWithOverridesConfig
+config = ExamplePluginWithOverrideConfig
 
 default_app_config = "nautobot.core.apps.CoreConfig"
