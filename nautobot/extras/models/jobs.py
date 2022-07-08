f374953a2e3d08d04c37342cbbecc8944a52566b
Glenn Matthews <glenn.matthews@networktocode.com>
2022-07-08 09:40:55 -0400

    Fix #2002 - remove RQ worker container from docker-compose default environment

--- nautobot/extras/models/jobs.py
+++ nautobot/extras/models/jobs.py
@@ -251,7 +251,7 @@ class Job(PrimaryModel):
                     logger.warning("Job %s %s has no associated Git repository", self.module_name, self.job_class_name)
                     return None
                 try:
-                    # In the case where we have multiple Nautobot instances, or multiple RQ worker instances,
+                    # In the case where we have multiple Nautobot instances, or multiple worker instances,
                     # they are not required to share a common filesystem; therefore, we may need to refresh our local
                     # clone of the Git repository to ensure that it is in sync with the latest repository clone
                     # from any instance.
