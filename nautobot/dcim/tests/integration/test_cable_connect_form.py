9939d59702c0c480242ad067be2a3ebbf000d9c5
Dan Bridson <655255+briddo@users.noreply.github.com>
2022-07-11 20:05:02 +0100

    flake8

--- nautobot/dcim/tests/integration/test_cable_connect_form.py
+++ nautobot/dcim/tests/integration/test_cable_connect_form.py
@@ -22,7 +22,7 @@ class CableConnectFormTestCase(SeleniumTestCase):
         self.user.save()
         self.login(self.user.username, self.password)
         device1 = create_test_device("Device 1")
-        device2 = create_test_device("Device 2")
+        create_test_device("Device 2")
         interface1 = Interface.objects.create(device=device1, name="Interface 1")
         Interface.objects.create(device=device1, name="Interface 2")
         Interface.objects.create(device=device1, name="Interface 3")
