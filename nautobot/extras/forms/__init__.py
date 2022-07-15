9179f935c52622de488a50134a2f2ca7e6df4d47
Hanlin Miao <hanlinmiao@Hanlins-MBP-2.home>
2022-07-15 11:47:54 -0400

    modified code to pass CI tests

--- nautobot/extras/forms/__init__.py
+++ nautobot/extras/forms/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
 from .base_classes import *
 from .forms import *
-from .forms import CustomFieldChoiceFormSet, SecretsGroupAssociationFormSet
+from .forms import BaseDynamicGroupMembershipFormSet, CustomFieldChoiceFormSet, DynamicGroupMembershipFormSet, SecretsGroupAssociationFormSet
 from .mixins import *
