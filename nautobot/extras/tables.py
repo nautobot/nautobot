b353a66a1022402df521f0c1578f970b7b423ee5
jathanism <jathan@gmail.com>
2022-07-23 10:51:13 -0700

    Fix display bug for descendants/ancestors memberships
    - Memberships query was not correctly topologically sorted based on incoming ordering of flattened ancestors tree.

--- nautobot/extras/tables.py
+++ nautobot/extras/tables.py
@@ -335,9 +335,9 @@ class DynamicGroupMembershipTable(DynamicGroupTable):
 
 DESCENDANTS_LINK = """
 {% load helpers %}
-{% for node, depth in descendants_map.items %}
-    {% if node == record.name %}
-        {% for i in depth|as_range %}
+{% for node in descendants_tree %}
+    {% if node.name == record.name %}
+        {% for i in node.depth|as_range %}
             {% if not forloop.first %}
             <i class="mdi mdi-circle-small"></i>
             {% endif %}
@@ -361,9 +361,9 @@ class NestedDynamicGroupDescendantsTable(DynamicGroupMembershipTable):
 
 ANCESTORS_LINK = """
 {% load helpers %}
-{% for node, depth in ancestors_map.items %}
-    {% if node == record.name %}
-        {% for i in depth|as_range %}
+{% for node in ancestors_tree %}
+    {% if node.name == record.name %}
+        {% for i in node.depth|as_range %}
             {% if not forloop.first %}
             <i class="mdi mdi-circle-small"></i>
             {% endif %}
@@ -382,8 +382,8 @@ class NestedDynamicGroupAncestorsTable(DynamicGroupTable):
     name = tables.TemplateColumn(template_code=ANCESTORS_LINK)
 
     class Meta(DynamicGroupTable.Meta):
-        fields = ["name", "members", "description"]
-        exclude = ["actions", "content_type"]
+        fields = ["name", "members", "description", "actions"]
+        exclude = ["content_type"]
 
 
 class ExportTemplateTable(BaseTable):
