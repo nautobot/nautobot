f65983c56baf8d62ce20556ea3b35a050857d9d1
Marek Zbroch <marek.zbroch@networktocode.com>
2022-08-05 16:07:21 +0200

    Cable Endpoints - DCIM URLs

--- nautobot/dcim/urls.py
+++ nautobot/dcim/urls.py
@@ -870,12 +870,6 @@ urlpatterns = [
         kwargs={"model": ConsolePort},
     ),
     path(
-        "console-ports/<uuid:termination_a_id>/connect/<str:termination_b_type>/",
-        views.CableCreateView.as_view(),
-        name="consoleport_connect",
-        kwargs={"termination_a_type": ConsolePort},
-    ),
-    path(
         "devices/console-ports/add/",
         views.DeviceBulkAddConsolePortView.as_view(),
         name="device_bulk_add_consoleport",
@@ -948,12 +942,6 @@ urlpatterns = [
         views.PathTraceView.as_view(),
         name="consoleserverport_trace",
         kwargs={"model": ConsoleServerPort},
-    ),
-    path(
-        "console-server-ports/<uuid:termination_a_id>/connect/<str:termination_b_type>/",
-        views.CableCreateView.as_view(),
-        name="consoleserverport_connect",
-        kwargs={"termination_a_type": ConsoleServerPort},
     ),
     path(
         "devices/console-server-ports/add/",
@@ -1158,12 +1146,6 @@ urlpatterns = [
         kwargs={"model": Interface},
     ),
     path(
-        "interfaces/<uuid:termination_a_id>/connect/<str:termination_b_type>/",
-        views.CableCreateView.as_view(),
-        name="interface_connect",
-        kwargs={"termination_a_type": Interface},
-    ),
-    path(
         "devices/interfaces/add/",
         views.DeviceBulkAddInterfaceView.as_view(),
         name="device_bulk_add_interface",
@@ -1225,12 +1207,6 @@ urlpatterns = [
         name="frontport_trace",
         kwargs={"model": FrontPort},
     ),
-    path(
-        "front-ports/<uuid:termination_a_id>/connect/<str:termination_b_type>/",
-        views.CableCreateView.as_view(),
-        name="frontport_connect",
-        kwargs={"termination_a_type": FrontPort},
-    ),
     # path('devices/front-ports/add/', views.DeviceBulkAddFrontPortView.as_view(), name='device_bulk_add_frontport'),
     # Rear ports
     path("rear-ports/", views.RearPortListView.as_view(), name="rearport_list"),
@@ -1288,12 +1264,6 @@ urlpatterns = [
         views.PathTraceView.as_view(),
         name="rearport_trace",
         kwargs={"model": RearPort},
-    ),
-    path(
-        "rear-ports/<uuid:termination_a_id>/connect/<str:termination_b_type>/",
-        views.CableCreateView.as_view(),
-        name="rearport_connect",
-        kwargs={"termination_a_type": RearPort},
     ),
     path(
         "devices/rear-ports/add/",
@@ -1426,6 +1396,7 @@ urlpatterns = [
     ),
     # Cables
     path("cables/", views.CableListView.as_view(), name="cable_list"),
+    path('cables/add/', views.CableEditView.as_view(), name='cable_add'),
     path("cables/import/", views.CableBulkImportView.as_view(), name="cable_import"),
     path("cables/edit/", views.CableBulkEditView.as_view(), name="cable_bulk_edit"),
     path("cables/delete/", views.CableBulkDeleteView.as_view(), name="cable_bulk_delete"),
