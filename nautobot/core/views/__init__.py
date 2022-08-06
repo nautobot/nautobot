738af49ec68d769d4349e27b2cba598812bf391f
Hanlin Miao <hanlinmiao@Hanlins-MBP-2.home>
2022-08-05 20:14:40 -0400

    more tests

--- nautobot/core/views/__init__.py
+++ nautobot/core/views/__init__.py
@@ -3,7 +3,6 @@ import platform
 import sys
 
 from django.conf import settings
-from django.contrib.auth.mixins import AccessMixin
 from django.http import HttpResponseServerError, JsonResponse
 from django.shortcuts import render
 from django.template import loader, RequestContext, Template
