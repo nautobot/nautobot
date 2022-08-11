b50c5bce4ea085c14577aae5b10db2ec03042231
itdependsnetworks <ken@celenza.org>
2022-08-11 17:35:12 -0400

    Add tags to location form

--- nautobot/dcim/forms.py
+++ nautobot/dcim/forms.py
@@ -473,7 +473,7 @@ class LocationForm(NautobotModelForm, TenancyForm):
 
     class Meta:
         model = Location
-        fields = ["location_type", "parent", "site", "name", "slug", "status", "tenant_group", "tenant", "description"]
+        fields = ["location_type", "parent", "site", "name", "slug", "status", "tenant_group", "tenant", "description", "tags"]
 
 
 class LocationBulkEditForm(TagsBulkEditFormMixin, StatusModelBulkEditFormMixin, NautobotBulkEditForm):
