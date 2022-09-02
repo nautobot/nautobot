cab0f67f7521d8d1f8ab64911c57e415dd1e0d00
Hanlin Miao <hanlinmiao@Hanlins-MBP-2.home>
2022-09-02 01:21:51 -0400

    Revert "commit to save changes"
    This reverts commit ed2455acf012ee3afa25ad40c487bb376f767906.

--- nautobot/extras/tests/test_jobs.py
+++ nautobot/extras/tests/test_jobs.py
@@ -1,15 +1,14 @@
 import datetime
 import json
-import logging
 import re
 import uuid
 from io import StringIO
 from unittest import mock
 
-from celery.contrib.testing.worker import start_worker  # noqa F401
+from celery.contrib.testing.worker import start_worker
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.management import call_command
 from django.core.management.base import CommandError
@@ -17,6 +16,7 @@ from django.test import override_settings
 from django.test.client import RequestFactory
 from django.utils import timezone
 
+from nautobot.core.celery import app
 from nautobot.dcim.models import DeviceRole, Site
 from nautobot.extras.choices import (
     JobExecutionType,
@@ -129,11 +129,10 @@ class JobTest(TransactionTestCase):
         """
         module = "test_fail"
         name = "TestFail"
-        # Silence the error messages when intended exceptions are raised and job_result is logging it during unittests
-        # Re-enable logging after
-        logging.disable(logging.ERROR)
-        job_result = create_job_result_and_run_job(module, name, commit=False)
-        logging.disable(logging.NOTSET)
+        try:
+            job_result = create_job_result_and_run_job(module, name, commit=False)
+        except Exception as e:
+            self.assert_equal(str(e), "Test failure")
         self.assertEqual(job_result.status, JobResultStatusChoices.STATUS_ERRORED)
 
     def test_field_default(self):
@@ -205,11 +204,10 @@ class JobTest(TransactionTestCase):
         """
         module = "test_read_only_fail"
         name = "TestReadOnlyFail"
-        # Silence the error messages when intended exceptions are raised and job_result is logging it during unittests
-        # Re-enable logging after
-        logging.disable(logging.ERROR)
-        job_result = create_job_result_and_run_job(module, name, commit=False)
-        logging.disable(logging.NOTSET)
+        try:
+            job_result = create_job_result_and_run_job(module, name, commit=False)
+        except Exception as e:
+            self.assertEqual(str(e), "Test failure")
         self.assertEqual(job_result.status, JobResultStatusChoices.STATUS_ERRORED)
         self.assertEqual(Site.objects.count(), 0)  # Ensure DB transaction was aborted
         # Also ensure the standard log message about aborting the transaction is *not* present
@@ -346,11 +344,8 @@ class JobTest(TransactionTestCase):
         module = "test_object_var_required"
         name = "TestRequiredObjectVar"
         data = {"region": None}
-        # Silence the error messages when intended exceptions are raised and job_result is logging it during unittests
-        # Re-enable logging after
-        logging.disable(logging.ERROR)
         job_result = create_job_result_and_run_job(module, name, data=data, commit=False)
-        logging.disable(logging.NOTSET)
+        self.assertRaises(ValidationError, create_job_result_and_run_job(module, name, data=data, commit=False))
 
         # Assert stuff
         self.assertEqual(job_result.status, JobResultStatusChoices.STATUS_ERRORED)
@@ -366,18 +361,17 @@ class JobTest(TransactionTestCase):
         module = "test_object_vars"
         name = "TestObjectVars"
         data = "BAD DATA STRING"
-        # Silence the error messages when intended exceptions are raised and job_result is logging it during unittests
-        # Re-enable it after
-        logging.disable(logging.ERROR)
-        job_result = create_job_result_and_run_job(module, name, data=data, commit=False)
-        logging.disable(logging.NOTSET)
+        try:
+            job_result = create_job_result_and_run_job(module, name, data=data, commit=False)
+        except Exception as e:
+            self.assertEqual(e, TypeError)
 
-        # Assert stuff
-        self.assertEqual(job_result.status, JobResultStatusChoices.STATUS_ERRORED)
-        log_failure = JobLogEntry.objects.filter(
-            grouping="initialization", log_level=LogLevelChoices.LOG_FAILURE
-        ).first()
-        self.assertIn("Data should be a dictionary", log_failure.message)
+            # Assert stuff
+            self.assertEqual(job_result.status, JobResultStatusChoices.STATUS_ERRORED)
+            log_failure = JobLogEntry.objects.filter(
+                grouping="initialization", log_level=LogLevelChoices.LOG_FAILURE
+            ).first()
+            self.assertIn("Data should be a dictionary", log_failure.message)
 
     def test_job_latest_result_property(self):
         """
@@ -461,29 +455,28 @@ class JobFileUploadTest(TransactionTestCase):
         self.assertEqual(FileProxy.objects.count(), 1)
 
         # Run the job
-        # Silence the error messages when intended exceptions are raised and job_result is logging it during unittests
-        # Re-enable logging after
-        logging.disable(logging.ERROR)
-        job_result = create_job_result_and_run_job(module, name, data=serialized_data, commit=False)
-        logging.disable(logging.NOTSET)
+        try:
+            job_result = create_job_result_and_run_job(module, name, data=serialized_data, commit=False)
+        except Exception as e:
+            self.assertEqual(str(e), "Test failure")
 
-        # Assert that file contents were correctly read
-        self.assertEqual(
-            JobLogEntry.objects.filter(job_result=job_result, log_level=LogLevelChoices.LOG_WARNING, grouping="run")
-            .first()
-            .message,
-            f"File contents: {self.file_contents}",
-        )
-        # Also ensure the standard log message about aborting the transaction is present
-        self.assertEqual(
-            JobLogEntry.objects.filter(job_result=job_result, log_level=LogLevelChoices.LOG_INFO, grouping="run")
-            .first()
-            .message,
-            "Database changes have been reverted due to error.",
-        )
+            # Assert that file contents were correctly read
+            self.assertEqual(
+                JobLogEntry.objects.filter(job_result=job_result, log_level=LogLevelChoices.LOG_WARNING, grouping="run")
+                .first()
+                .message,
+                f"File contents: {self.file_contents}",
+            )
+            # Also ensure the standard log message about aborting the transaction is present
+            self.assertEqual(
+                JobLogEntry.objects.filter(job_result=job_result, log_level=LogLevelChoices.LOG_INFO, grouping="run")
+                .first()
+                .message,
+                "Database changes have been reverted due to error.",
+            )
 
-        # Assert that FileProxy was cleaned up
-        self.assertEqual(FileProxy.objects.count(), 0)
+            # Assert that FileProxy was cleaned up
+            self.assertEqual(FileProxy.objects.count(), 0)
 
 
 class RunJobManagementCommandTest(CeleryTestCase):
@@ -678,12 +671,11 @@ class JobHookReceiverTest(TransactionTestCase):
     def test_missing_receive_job_hook_method(self):
         module = "test_job_hook_receiver"
         name = "TestJobHookReceiverFail"
-        # Silence the error messages when intended exceptions are raised and job_result is logging it during unittests
-        # Re-enable logging after
-        logging.disable(logging.ERROR)
-        job_result = create_job_result_and_run_job(module, name, data=self.data, commit=False)
-        logging.disable(logging.NOTSET)
-        self.assertEqual(job_result.status, JobResultStatusChoices.STATUS_ERRORED)
+        try:
+            job_result = create_job_result_and_run_job(module, name, data=self.data, commit=False)
+        except Exception as e:
+            self.assertEqual(e, NotImplementedError)
+            self.assertEqual(job_result.status, JobResultStatusChoices.STATUS_ERRORED)
 
 
 class JobHookTest(CeleryTestCase):
