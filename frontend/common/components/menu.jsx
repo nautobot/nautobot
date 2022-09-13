aade0fa349f69164321d32f452c75928e7bf16e7
Timizuo <timizuoebideri@gmail.com>
2022-09-13 19:24:28 +0100

    new changes

--- frontend/common/components/menu.jsx
+++ frontend/common/components/menu.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import {useEffect, useState} from "react"
 import { Container } from "react-bootstrap";
 import Nav from 'react-bootstrap/Nav';
@@ -7,9 +6,10 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Menu() {
     const [NavMenu, setNavMenu] = useState([])
-    useEffect(async () => {
-        const nav_api = await (await axios.get(process.env.NEXT_PUBLIC_NAUTOBOT_BASE_URL + "/api/get-menu/")).data
+    useEffect(() => {
+        const nav_api = require("../utils/nav_menu_api.json")
         setNavMenu(nav_api)
+        
     }, [NavMenu])
 
     return (
@@ -24,66 +24,19 @@ export default function Menu() {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <ul class="nav navbar-nav">
-                    <li class="dropdown">
-                        <a 
-                            href="#"
-                            class="dropdown-toggle"
-                            data-tab-weight="100"
-                            data-toggle="dropdown"
-                            role="button"
-                            aria-haspopup="true"
-                            aria-expanded="false"
-                        >
-                            Organization <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li class="dropdown-header" data-group-weight="100">Sites</li>
-                            <li>
-                                <div class="buttons pull-right">
-                                    <a href="/dcim/sites/add/" data-button-weight="100" class="btn btn-xs btn-success" title="Add">
-                                        <i class="mdi mdi-plus-thick"></i>
-                                    </a>
-                                    <a href="/dcim/sites/import/" data-button-weight="200" class="btn btn-xs btn-primary"
-                                        title="Import">
-                                        <i class="mdi mdi-database-import-outline"></i>
-                                    </a>
-                                </div>
-                                <a href="/dcim/sites/" data-item-weight="100">
-                                    Sites
-                                </a>
-                            </li>
-                            <li>
-                                <div class="buttons pull-right">
-                                    <a href="/dcim/regions/add/" data-button-weight="100" class="btn btn-xs btn-success" title="Add">
-                                        <i class="mdi mdi-plus-thick"></i>
-                                    </a>
-                                    <a href="/dcim/regions/import/" data-button-weight="200" class="btn btn-xs btn-primary"
-                                        title="Import">
-                                        <i class="mdi mdi-database-import-outline"></i>
-                                    </a>
-                                </div>
-                                <a href="/dcim/regions/" data-item-weight="200">
-                                    Regions
-                                </a>
-                            </li>
-                            <li class="divider"></li>
-                        </ul>
-                    </li>
-                </ul>
-                    {/* <Nav className="me-auto">
+                    <Nav className="me-auto">
                         {
                             NavMenu.map((item, idx) => (
                                 <NavDropdown key={idx} title={item.name} id="basic-nav-dropdown">
                                     {
-                                        Object.entries(item.properties.groups).map((child, child_idx) => (
-                                            <NavDropdown.Item key={child_idx} href={child.link}>{child[0]}</NavDropdown.Item>
+                                        item.children.map((child, child_idx) => (
+                                            <NavDropdown.Item key={child_idx} href={child.link}>{child.name}</NavDropdown.Item>
                                         ))
                                     }
                                 </NavDropdown>
                             ))
                         }
-                    </Nav> */}
+                    </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
