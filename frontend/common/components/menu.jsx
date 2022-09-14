2ae170623cede51fe5df8e157189a3799b79248e
Timizuo <timizuoebideri@gmail.com>
2022-09-14 23:16:41 +0100

    fix menu

--- frontend/common/components/menu.jsx
+++ frontend/common/components/menu.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import {useEffect, useState} from "react"
 import { Container } from "react-bootstrap";
 import Nav from 'react-bootstrap/Nav';
@@ -6,20 +7,19 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Menu() {
     const [NavMenu, setNavMenu] = useState([])
-    useEffect(() => {
-        const nav_api = require("../utils/nav_menu_api.json")
-        setNavMenu(nav_api)
-        
+    useEffect(async () => {
+        const nav_api = await axios.get(process.env["NEXT_PUBLIC_NAUTOBOT_BASE_URL"] + "/api/get-menu/")
+        setNavMenu(nav_api.data)
     }, [NavMenu])
 
     return (
         <Navbar bg="light" expand="lg" fixed="top">
-            <Container>
+            <Container fluid>
                 <Navbar.Brand href="/">
                     <img 
                         src={process.env.NEXT_PUBLIC_NAUTOBOT_STATIC_ROOT + "/img/nautobot_logo.svg"} 
                         alt="nautobot-logo" 
-                        width={150}
+                        width={100}
                     />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -27,10 +27,20 @@ export default function Menu() {
                     <Nav className="me-auto">
                         {
                             NavMenu.map((item, idx) => (
-                                <NavDropdown key={idx} title={item.name} id="basic-nav-dropdown">
+                                <NavDropdown key={idx} title={item.name} id="basic-nav-dropdown" style={{"fontSize": "14px"}}>
                                     {
-                                        item.children.map((child, child_idx) => (
-                                            <NavDropdown.Item key={child_idx} href={child.link}>{child.name}</NavDropdown.Item>
+                                        Object.entries(item.properties.groups).map((group, group_idx) => (
+                                            <div key={group_idx}>
+                                                {
+                                                    Object.entries(group[1].items).map((menu, menu_idx) => (
+                                                        <NavDropdown.Item style={{"fontSize": "13px"}} key={menu_idx} href={menu[0]}>
+                                                            {menu[1].name}
+                                                        </NavDropdown.Item>
+                                                    ))
+                                                }
+                                                <NavDropdown.Divider />
+                                            </div>
+                                            
                                         ))
                                     }
                                 </NavDropdown>
