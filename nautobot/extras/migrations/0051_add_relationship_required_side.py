55f488e99b2dd1dd82a1cc0fc5ff4a6d50fbe5a4
Dan Bridson <briddo@gmail.com>
2022-09-27 13:38:45 +0100

    fixed conflicting migration files

--- nautobot/extras/migrations/0051_add_relationship_required_side.py
+++ nautobot/extras/migrations/0051_add_relationship_required_side.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("extras", "0049_customfield_grouping"),
+        ("extras", "0050_add_job_task_queues"),
     ]
 
     operations = [
