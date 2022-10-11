a4288e3c86359844c5f57c6e6fc1008a0c09686a
jathanism <jathan@gmail.com>
2022-10-11 14:29:26 -0700

    Refactor the factories to have more human-readable names
    - `Manufacturer` will be named from a predefined list of known network vendors
    - `Platform` will only set `napalm_driver` if a `manufacturer` is set
    - `Platform` will be named based on the `manufacturer.name`
    - `Platform.napalm_driver` will be selected from a mapping of known driver names
    - `DeviceType` will be named based on the `manufacturer.name`

--- nautobot/dcim/factory.py
+++ nautobot/dcim/factory.py
@@ -1,7 +1,8 @@
 import logging
+import random
 
 import factory
-import faker
+import factory.fuzzy
 
 from nautobot.core.factory import OrganizationalModelFactory, PrimaryModelFactory
 from nautobot.dcim.models import DeviceType, Manufacturer, Platform
@@ -12,41 +13,68 @@ from nautobot.utilities.factory import random_instance, UniqueFaker
 logger = logging.getLogger(__name__)
 
 
+# For a randomly deterministic set of vendor names. Must be a tuple.
+MANUFACTURER_NAMES = (
+    "A10",
+    "Arista",
+    "Aruba",
+    "Brocade",
+    "Cisco",
+    "Citrix",
+    "Dell",
+    "F5",
+    "Force10",
+    "Fortinet",
+    "Huawei",
+    "Juniper",
+    "HP",
+    "Palo Alto",
+)
+
+# For `Platform.napalm_driver`, either randomly choose based on Manufacturer slug.
+NAPALM_DRIVERS = {
+    "arista": ["eos"],
+    "cisco": ["ios", "iosxr", "iosxr_netconf", "nxos", "nxos_ssh"],
+    "juniper": ["junos"],
+    "palo-alto": ["panos"],
+}
+
+
 class ManufacturerFactory(OrganizationalModelFactory):
     class Meta:
         model = Manufacturer
         exclude = ("has_description",)
 
-    class Params:
-        unique_name = UniqueFaker("word")
-
     # Slug isn't defined here since it will always inherit from name.
-    name = factory.LazyAttribute(lambda o: o.unique_name.title())
+    name = UniqueFaker("word", ext_word_list=MANUFACTURER_NAMES)
 
     has_description = factory.Faker("pybool")
     description = factory.Maybe("has_description", factory.Faker("text", max_nb_chars=200), "")
 
 
-class ManufacturerGetOrCreateFactory(ManufacturerFactory):
-    class Meta:
-        django_get_or_create = ("name",)
-
-
 class PlatformFactory(OrganizationalModelFactory):
     class Meta:
         model = Platform
-        exclude = ("has_manufacturer", "has_description", "has_napalm_driver", "has_napalm_args")
+        exclude = ("has_manufacturer", "manufacturer_slug", "has_description", "has_napalm_args")
+
+    # This dictates `name` and `napalm_driver`.
+    has_manufacturer = factory.Faker("pybool")
 
     # Slug isn't defined here since it will always inherit from name.
-    name = factory.LazyFunction(
-        lambda: faker.Faker().word(part_of_speech="adjective").title() + " " + faker.Faker().word().title()
+    name = factory.Maybe(
+        "has_manufacturer",
+        factory.LazyAttributeSequence(lambda o, n: "%s Platform %d" % (o.manufacturer.name, n + 1)),
+        factory.Sequence(lambda n: "Platform %d" % n),
     )
 
-    has_manufacturer = factory.Faker("pybool")
     manufacturer = factory.Maybe("has_manufacturer", random_instance(Manufacturer), None)
 
-    has_napalm_driver = factory.Faker("pybool")
-    napalm_driver = factory.Maybe("has_napalm_driver", factory.Faker("locale"), "")
+    # If it has a manufacturer, it *might* have a naplam_driver.
+    napalm_driver = factory.Maybe(
+        "has_manufacturer",
+        factory.LazyAttribute(lambda o: random.choice(NAPALM_DRIVERS.get(o.manufacturer.slug, [""]))),
+        "",
+    )
 
     has_napalm_args = factory.Faker("pybool")
     napalm_args = factory.Maybe(
@@ -68,17 +96,15 @@ class DeviceTypeFactory(PrimaryModelFactory):
 
     manufacturer = random_instance(Manufacturer)
 
-    model = factory.LazyFunction(
-        lambda: faker.Faker().word(part_of_speech="adjective").title() + " " + faker.Faker().word().title()
-    )
+    # Slug isn't defined here since it will always inherit from model.
+    model = factory.LazyAttributeSequence(lambda o, n: "%s DeviceType %d" % (o.manufacturer.name, n + 1))
 
-    slug = UniqueFaker("word")
     has_part_number = factory.Faker("pybool")
     part_number = factory.Maybe("has_part_number", factory.Faker("ean", length=8), "")
 
     # If randomly a subdevice, set u_height to 0.
-    is_subdevice_child = factory.Faker("pybool")
-    u_height = factory.Maybe("is_subdevice_child", 0, factory.Faker("random_int", min=1, max=4))
+    is_subdevice_child = factory.Faker("boolean", chance_of_getting_true=33)
+    u_height = factory.Maybe("is_subdevice_child", 0, factory.fuzzy.FuzzyInteger(1, 4))
 
     is_full_depth = factory.Faker("pybool")
 
