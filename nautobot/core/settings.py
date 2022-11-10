673f0124faf65ae5532b07da8142f96275913b0e
Bryan Culver <bryan.culver@networktocode.com>
2022-11-10 11:39:57 -0500

    Undo lock style changes, restore assertion.

--- nautobot/core/settings.py
+++ nautobot/core/settings.py
@@ -171,6 +171,7 @@ REST_FRAMEWORK_VERSION = VERSION.rsplit(".", 1)[0]  # Use major.minor as API ver
 current_major, current_minor = REST_FRAMEWORK_VERSION.split(".")
 # We support all major.minor API versions from 1.2 to the present latest version.
 # Similar logic exists in tasks.py, please keep them in sync!
+assert current_major == "2", f"REST_FRAMEWORK_ALLOWED_VERSIONS needs to be updated to handle version {current_major}"
 REST_FRAMEWORK_ALLOWED_VERSIONS = ["1.2", "1.3", "1.4", "1.5"] + [
     f"{current_major}.{minor}" for minor in range(0, int(current_minor) + 1)
 ]
