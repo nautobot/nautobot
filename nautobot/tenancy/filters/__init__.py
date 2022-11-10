f14f3e4d653e20fc96bb7f26bd6f70e509db3e35
Glenn Matthews <glenn.matthews@networktocode.com>
2022-11-10 13:09:56 -0500

    Create nautobot.tenancy.filters.mixins submodule

--- nautobot/tenancy/filters/__init__.py
+++ nautobot/tenancy/filters/__init__.py
@@ -2,16 +2,16 @@ import django_filters
 
 from nautobot.dcim.models import Location
 from nautobot.extras.filters import NautobotFilterSet
+from nautobot.tenancy.filters.mixins import TenancyModelFilterSetMixin
+from nautobot.tenancy.models import Tenant, TenantGroup
 from nautobot.utilities.deprecation import class_deprecated_in_favor_of
 from nautobot.utilities.filters import (
     NameSlugSearchFilterSet,
-    NaturalKeyOrPKMultipleChoiceFilter,
     RelatedMembershipBooleanFilter,
     SearchFilter,
     TagFilter,
     TreeNodeMultipleChoiceFilter,
 )
-from .models import Tenant, TenantGroup
 
 
 __all__ = (
@@ -72,32 +72,6 @@ class TenantFilterSet(NautobotFilterSet):
     class Meta:
         model = Tenant
         fields = ["id", "name", "slug"]
-
-
-class TenancyModelFilterSetMixin(django_filters.FilterSet):
-    """
-    An inheritable FilterSet for models which support Tenant assignment.
-    """
-
-    tenant_group_id = TreeNodeMultipleChoiceFilter(
-        queryset=TenantGroup.objects.all(),
-        field_name="tenant__group",
-        label="Tenant Group (ID)",
-    )
-    tenant_group = TreeNodeMultipleChoiceFilter(
-        queryset=TenantGroup.objects.all(),
-        field_name="tenant__group",
-        to_field_name="slug",
-        label="Tenant Group (slug)",
-    )
-    tenant_id = django_filters.ModelMultipleChoiceFilter(
-        queryset=Tenant.objects.all(),
-        label='Tenant (ID) (deprecated, use "tenant" filter instead)',
-    )
-    tenant = NaturalKeyOrPKMultipleChoiceFilter(
-        queryset=Tenant.objects.all(),
-        label="Tenant (slug or ID)",
-    )
 
 
 # TODO: remove in 2.2
