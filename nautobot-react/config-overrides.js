fa1da51849d05a77e87f55eccfcfd2e3cd02db48
Gary Snider <gary.snider@networktocode.com>
2022-12-01 08:40:40 -0600

    add postbuild script to move files to nautobot

--- nautobot-react/config-overrides.js
+++ nautobot-react/config-overrides.js
@@ -16,7 +16,7 @@ module.exports = function override(config, env) {
 		new ModuleFederationPlugin({
 			name: 'nautobot',
 			library: { type: 'var', name: 'nautobot' },
-			filename: 'static/reactjs/nautobotRemoteEntry.js',
+			filename: 'static/reactjs/core/nautobotRemoteEntry.js',
 			exposes: {
 				NautobotCheck: './src/common/shared/NautobotCheck',
 				NautobotInput: './src/common/shared/NautobotInput',
@@ -38,8 +38,8 @@ module.exports = function override(config, env) {
 
 	]
 	const miniCssOptions = {
-		filename: 'static/reactcss/[name].[contenthash:8].css',
-		chunkFilename: 'static/reactcss/[name].[contenthash:8].chunk.css',
+		filename: 'static/reactcss/core/[name].[contenthash:8].css',
+		chunkFilename: 'static/reactcss/core/[name].[contenthash:8].chunk.css',
 	}
 	if (isEnvProduction) {
 		config.plugins.forEach((p, i) => {
@@ -52,8 +52,8 @@ module.exports = function override(config, env) {
 	// Extend existing plugins array with ours.
 	config.plugins.push(...plugins)
 
-	config.output.filename = isEnvProduction ? 'static/reactjs/[name].[contenthash:8].js' : isEnvDevelopment && 'static/reactjs/bundle.js';
-	config.output.chunkFilename = isEnvProduction ? 'static/reactjs/[name].[contenthash:8].chunk.js' : isEnvDevelopment && 'static/reactjs/[name].chunk.js';
+	config.output.filename = isEnvProduction ? 'static/reactjs/core/[name].[contenthash:8].js' : isEnvDevelopment && 'static/reactjs/core/bundle.js';
+	config.output.chunkFilename = isEnvProduction ? 'static/reactjs/core/[name].[contenthash:8].chunk.js' : isEnvDevelopment && 'static/reactjs/core/[name].chunk.js';
 
 	return config;
 }
