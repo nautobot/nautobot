fcfc3405c333ee6fd006c5d68338e05863f63129
Timizuo <timizuoebideri@gmail.com>
2022-12-06 23:08:10 +0100

    update listview

--- nautobot_ui/src/views/ListView.jsx
+++ nautobot_ui/src/views/ListView.jsx
@@ -1,19 +1,11 @@
-import NautobotTable from "@core/Table"
-import BaseLayout from "@layouts/BaseLayout"
 import ListTemplate from "@layouts/ListTemplate"
-import { naxios } from "@utils/axios"
-import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 
 export default function ListView({ match, location }) {
-    const { app_name, model_name } = useParams()
+    const { model_name } = useParams()
 
     return (
-        <ListTemplate 
-            page_title={model_name.capitalize()}
-            table_url=""
-            table_data_url=""
-        ></ListTemplate>
+        <ListTemplate  page_title={model_name.capitalize()} />
     )
 }
