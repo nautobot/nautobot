95e8581940724679b4f81bc197f6e97924eeceee
Bryan Culver <31187+bryanculver@users.noreply.github.com>
2022-12-22 13:53:37 -0500

    Batch Update Next Dependenices (#3027)
    * --no-update lockfile
    
    * Within requirements poetry update
    
    * New dev dependency group format
    
    * Update mkdocstrings-python
    
    * Update pylint
    
    * Update mkdocstrings
    
    * Update black
    
    * Update django-debug-toolbar
    
    * Update drf-spectacular
    
    * Update watchdog
    
    * Update selenium
    
    * Update prometheus-client
    
    * Update django-storages
    
    * Update requests
    
    * 📍 Milemarker: Non-major upgrades at this point.
    
    * 📍 Milemarker: poetry update after making non-major updates
    
    * Pylint
    
    * Update test
    
    * Changelog and lint
    
    * Update changelog fragment. Add TODO for pylint issue.

--- nautobot/extras/tests/test_filters.py
+++ nautobot/extras/tests/test_filters.py
@@ -1416,7 +1416,10 @@ class StatusTestCase(FilterTestCases.NameSlugFilterTestCase):
 
     def test_search(self):
         params = {"q": "active"}
+        # TODO: Remove pylint disable after issue is resolved (see: https://github.com/PyCQA/pylint/issues/7381)
+        # pylint: disable=unsupported-binary-operation
         q = Q(id__iexact="active") | Q(name__icontains="active") | Q(slug__icontains="active")
+        # pylint: enable=unsupported-binary-operation
         q |= Q(content_types__model__icontains="active")
         self.assertQuerysetEqualAndNotEmpty(
             self.filterset(params, self.queryset).qs,
