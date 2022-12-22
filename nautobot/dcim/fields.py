7e3cc8a39acb0741e1f7eba7cacf83331abc5b44
Glenn Matthews <glenn.matthews@networktocode.com>
2022-12-22 17:42:57 -0500

    Fix #2163: remove 'null=True' from CharFields that don't need it, etc.

--- nautobot/dcim/fields.py
+++ nautobot/dcim/fields.py
@@ -4,7 +4,6 @@ from django.db import models
 from netaddr import AddrFormatError, EUI, mac_unix_expanded
 
 from nautobot.ipam.constants import BGP_ASN_MAX, BGP_ASN_MIN
-from nautobot.utilities.deprecation import class_deprecated_in_favor_of
 from nautobot.utilities.fields import JSONArrayField
 from .lookups import PathContains
 
@@ -56,6 +55,8 @@ class MACAddressCharField(models.CharField):
             return value
         if isinstance(value, str):
             value = value.strip()
+        if value == "":
+            return None
         try:
             return EUI(value, version=48, dialect=mac_unix_expanded_uppercase)
         except AddrFormatError:
@@ -63,35 +64,7 @@ class MACAddressCharField(models.CharField):
 
     def get_prep_value(self, value):
         if not value:
-            return None
-        return str(self.to_python(value))
-
-
-# 2.0 TODO: remove this class, it's unused in core and plugins shouldn't be using it in their models
-@class_deprecated_in_favor_of(MACAddressCharField)
-class MACAddressField(models.Field):
-    description = "PostgreSQL-only MAC Address field"
-
-    def python_type(self):
-        return EUI
-
-    def from_db_value(self, value, expression, connection):
-        return self.to_python(value)
-
-    def to_python(self, value):
-        if value is None:
-            return value
-        try:
-            return EUI(value, version=48, dialect=mac_unix_expanded_uppercase)
-        except AddrFormatError:
-            raise ValidationError(f"Invalid MAC address format: {value}")
-
-    def db_type(self, connection):
-        return "macaddr"
-
-    def get_prep_value(self, value):
-        if not value:
-            return None
+            return ""
         return str(self.to_python(value))
 
 
