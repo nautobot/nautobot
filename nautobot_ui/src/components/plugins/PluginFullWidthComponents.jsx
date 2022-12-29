ab5fc8f12f54383c79f1c58d85e0c8ca3ddef085
Gary Snider <gary.snider@networktocode.com>
2022-12-29 17:32:06 -0600

    remove debug logging

--- nautobot_ui/src/components/plugins/PluginFullWidthComponents.jsx
+++ nautobot_ui/src/components/plugins/PluginFullWidthComponents.jsx
@@ -3,8 +3,6 @@ import PluginComponents from "@components/core/Plugins"
 
 
 export default function PluginFullWidthComponentsWithProps(route, props) {
-    console.log(PluginComponents.FullWidthComponents)
-    console.log(`My route is ${route}`)
     if (!PluginComponents.FullWidthComponents?.[route]) return <></>
     return PluginComponents['FullWidthComponents'][route].map((FullWidthComponent, idx) =>
         <FullWidthComponent {...props} key={idx} />
