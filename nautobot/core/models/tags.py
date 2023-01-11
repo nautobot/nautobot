c0c06e6aaee9622a127daf2ff9bfb7acf80d1768
Timizuo <timizuoebideri@gmail.com>
2023-01-11 23:51:50 +0100

    Port Tag from extras over to Core

---
+++ nautobot/core/models/tags.py
@@ -0,0 +1,96 @@
+import logging
+
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.urls import reverse
+from taggit.models import GenericUUIDTaggedItemBase, TagBase
+
+from nautobot.core.choices import ColorChoices
+from nautobot.core.fields import ColorField
+from nautobot.core.models import (
+    BaseModel,
+    ChangeLoggedModel,
+    CustomFieldModel,
+    NotesMixin,
+    RelationshipModel,
+)
+
+from nautobot.core.querysets import RestrictedQuerySet
+from nautobot.extras.utils import TaggableClassesQuery, extras_features
+
+logger = logging.getLogger(__name__)
+
+
+class TagQuerySet(RestrictedQuerySet):
+    """Queryset for `Tags` objects."""
+
+    def get_for_model(self, model):
+        """
+        Return all `Tags` assigned to the given model.
+        """
+        content_type = ContentType.objects.get_for_model(model._meta.concrete_model)
+        return self.filter(content_types=content_type)
+
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
+
+# core_tag, core_taggeditem
+
+
+@extras_features(
+    "custom_fields",
+    "custom_validators",
+    "relationships",
+)
+class Tag(TagBase, BaseModel, ChangeLoggedModel, CustomFieldModel, RelationshipModel, NotesMixin):
+    content_types = models.ManyToManyField(
+        to=ContentType,
+        related_name="tags",
+        limit_choices_to=TaggableClassesQuery(),
+    )
+    color = ColorField(default=ColorChoices.COLOR_GREY)
+    description = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+
+    csv_headers = ["name", "slug", "color", "description"]
+
+    objects = TagQuerySet.as_manager()
+
+    class Meta:
+        ordering = ["name"]
+
+    def natural_key(self):
+        return (self.name,)
+
+    def get_absolute_url(self):
+        return reverse("extras:tag", args=[self.slug])
+
+    def to_csv(self):
+        return (self.name, self.slug, self.color, self.description)
+
+    def validate_content_types_removal(self, content_types_id):
+        """Validate content_types to be removed are not tagged to a model"""
+        errors = {}
+
+        removed_content_types = self.content_types.exclude(id__in=content_types_id)
+
+        # check if tag is assigned to any of the removed content_types
+        for content_type in removed_content_types:
+            model = content_type.model_class()
+            if model.objects.filter(tags=self).exists():
+                errors.setdefault("content_types", []).append(
+                    f"Unable to remove {model._meta.label_lower}. Dependent objects were found."
+                )
+
+        return errors
+
+
+class TaggedItem(BaseModel, GenericUUIDTaggedItemBase):
+    tag = models.ForeignKey(to=Tag, related_name="%(app_label)s_%(class)s_items", on_delete=models.CASCADE)
+    object_id = models.UUIDField()
+
+    class Meta:
+        index_together = ("content_type", "object_id")
