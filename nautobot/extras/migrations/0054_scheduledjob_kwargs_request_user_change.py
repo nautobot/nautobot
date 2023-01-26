511498e2c472d503795990c73dfeb9c4b962ad1f
Gary Snider <gary.snider@networktocode.com>
2023-01-26 13:25:12 -0600

    update migration and add docstring

--- nautobot/extras/migrations/0054_scheduledjob_kwargs_request_user_change.py
+++ nautobot/extras/migrations/0054_scheduledjob_kwargs_request_user_change.py
@@ -2,18 +2,23 @@ from django.db import migrations
 
 
 def rename_scheduled_job_kwargs_request_user(apps, schema_editor):
+    """
+    Rename ScheduledJob.kwargs["request"]["user"] to ScheduledJob.kwargs["request"]["_user_pk"]
+    to support https://github.com/nautobot/nautobot/pull/3105 and prevent an AttributeError exception
+    when the scheduled job runs.
+    """
     ScheduledJob = apps.get_model("extras", "ScheduledJob")
     for sj in ScheduledJob.objects.all():
-        if "request" in sj.kwargs and sj.kwargs["request"].get("user", None) is not None:
-            sj.kwargs["request"].setdefault("_user_pk", sj.kwargs["request"].pop("user"))
+        if "request" in sj.kwargs:
+            sj.kwargs["request"].setdefault("_user_pk", sj.kwargs["request"].pop("user", None))
             sj.save()
 
 
 def reverse_rename_scheduled_job_kwargs_request_user(apps, schema_editor):
     ScheduledJob = apps.get_model("extras", "ScheduledJob")
     for sj in ScheduledJob.objects.all():
-        if "request" in sj.kwargs and sj.kwargs["request"].get("_user_pk", None) is not None:
-            sj.kwargs["request"].setdefault("user", sj.kwargs["request"].pop("_user_pk"))
+        if "request" in sj.kwargs:
+            sj.kwargs["request"].setdefault("user", sj.kwargs["request"].pop("_user_pk", None))
             sj.save()
 
 
