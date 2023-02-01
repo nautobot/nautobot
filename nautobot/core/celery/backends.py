75483b104382804664f14752f77cacaad83b6dbc
jathanism <jathan@gmail.com>
2023-01-31 17:22:23 -0800

    Fixed issue with content-encoding and everything is all JSON.

--- nautobot/core/celery/backends.py
+++ nautobot/core/celery/backends.py
@@ -1,7 +1,5 @@
 from django_celery_results.backends import DatabaseBackend
 
-# from kombu.serialization import dumps, loads
-
 from nautobot.extras.models import JobResult
 
 
@@ -12,13 +10,51 @@ class NautobotDatabaseBackend(DatabaseBackend):
 
     TaskModel = JobResult
 
-    # TODO(jathan): Overloading these prevents the double encoding/decoding but
-    # it's still not working to allow us to just have JSON fields on the
-    # JobResult.
     def encode_content(self, data):
-        # return dumps(data, "nautobot_json")
+        """Pass through encoding since we're storing as JSON explicitly."""
         return "application/x-nautobot-json", "utf-8", data
 
     def decode_content(self, obj, content):
-        # return loads(content, obj.content_type, obj.content_encoding)
+        """Pass through decoding since we're storing as JSON explicitly."""
         return content
+
+    def _get_extended_properties(self, request, traceback):
+        """
+        Overload default so that `argsrepr` and `kwargsrepr` aren't used to construct `args` and
+        `kwargs`. Our need to properly (de)serialize `NautobotFakeRequest` facilitates this.
+        """
+        extended_props = {
+            "periodic_task_name": None,
+            "task_args": None,
+            "task_kwargs": None,
+            "task_name": None,
+            "traceback": None,
+            "worker": None,
+        }
+        if request and self.app.conf.find_value_for_key("extended", "result"):
+
+            # Let's not dump/encode args/kwargs at all in fact.
+            task_args = getattr(request, "args", None)
+            task_kwargs = getattr(request, "kwargs", None)
+
+            # Encode input arguments
+            if task_args is not None:
+                _, _, task_args = self.encode_content(task_args)
+
+            if task_kwargs is not None:
+                _, _, task_kwargs = self.encode_content(task_kwargs)
+
+            properties = getattr(request, "properties", {}) or {}
+            periodic_task_name = properties.get("periodic_task_name", None)
+            extended_props.update(
+                {
+                    "periodic_task_name": periodic_task_name,
+                    "task_args": task_args,
+                    "task_kwargs": task_kwargs,
+                    "task_name": getattr(request, "task", None),
+                    "traceback": traceback,
+                    "worker": getattr(request, "hostname", None),
+                }
+            )
+
+        return extended_props
