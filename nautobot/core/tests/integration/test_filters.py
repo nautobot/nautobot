f5bf4eb9e6b3b5e1589b7869b9d4d8faf5be327d
Hanlin Miao <hanlinmiao@Hanlins-MBP-2.home>
2023-02-08 00:33:30 -0500

    maybe it does not need LocationFactory?

--- nautobot/core/tests/integration/test_filters.py
+++ nautobot/core/tests/integration/test_filters.py
@@ -13,7 +13,6 @@ class ListViewFilterTestCase(SeleniumTestCase):
     def setUp(self):
         super().setUp()
         self.login(self.user.username, self.password)
-        factory.LocationFactory.create_batch(20, has_tenant=False)
 
     def tearDown(self):
         self.logout()
