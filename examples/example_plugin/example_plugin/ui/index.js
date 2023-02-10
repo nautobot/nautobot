91d1f69fb9f1f79537b547016c453045f0f95740
Glenn Matthews <glenn.matthews@networktocode.com>
2023-02-10 14:38:19 -0500

    Fixes to example_plugin and example_plugin_with_view_override

--- examples/example_plugin/example_plugin/ui/index.js
+++ examples/example_plugin/example_plugin/ui/index.js
@@ -1,19 +1,11 @@
 export { default as ExamplePluginFullWidthPageComponent } from "./FullWidthPage"
-export { default as ExamplePluginRetrieveViewOverride } from "./CustomView"
 
 const plugin_config = {
     detail_tabs: {},
     full_width_components: {
-        // TODO: do we really need to use the same component on both?
-        "dcim:sites": ["ExamplePluginFullWidthPageComponent"],
-        "dcim:devices": ["ExamplePluginFullWidthPageComponent"]
+        "dcim:sites": ["ExamplePluginFullWidthPageComponent"]
     },
-    // TODO: this should be moved to example_plugin_with_view_override so that it doesn't impact testing
-    view_overrides: {
-        "ipam:ip-addresses": {
-            "retrieve": "ExamplePluginRetrieveViewOverride"
-        }
-    }
+    view_overrides: {}
 }
 
 export default plugin_config
--- examples/example_plugin_with_view_override/example_plugin_with_view_override/ui/CustomView.jsx
+++ examples/example_plugin_with_view_override/example_plugin_with_view_override/ui/CustomView.jsx
@@ -1,5 +1,3 @@
-// TODO: this should be moved to example_plugin_with_view_override, as it is being used to replace the default view for
-// IPAddress objects, which is not something example_plugin should be doing.
 import Table from 'react-bootstrap/Table';
 
 export function CustomTableView(props) {
