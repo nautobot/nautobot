0591dcfb74cf99522082aa308c79afda62144325
Joe Wesch <joe.wesch@networktocode.com>
2023-02-24 09:58:37 -0600

    Fixes black

--- nautobot/extras/migrations/0055_jobbutton.py
+++ nautobot/extras/migrations/0055_jobbutton.py
@@ -10,28 +10,40 @@ import uuid
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
-        ('extras', '0054_scheduledjob_kwargs_request_user_change'),
+        ("contenttypes", "0002_remove_content_type_name"),
+        ("extras", "0054_scheduledjob_kwargs_request_user_change"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='JobButton',
+            name="JobButton",
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True)),
-                ('created', models.DateField(auto_now_add=True, null=True)),
-                ('last_updated', models.DateTimeField(auto_now=True, null=True)),
-                ('name', models.CharField(max_length=100, unique=True)),
-                ('text', models.CharField(max_length=500)),
-                ('weight', models.PositiveSmallIntegerField(default=100)),
-                ('group_name', models.CharField(blank=True, max_length=50)),
-                ('button_class', models.CharField(default='default', max_length=30)),
-                ('confirmation', models.BooleanField(default=True)),
-                ('content_type', models.ForeignKey(limit_choices_to=nautobot.extras.utils.FeatureQuery('job_buttons'), on_delete=django.db.models.deletion.CASCADE, to='contenttypes.contenttype')),
-                ('job', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='extras.job')),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True
+                    ),
+                ),
+                ("created", models.DateField(auto_now_add=True, null=True)),
+                ("last_updated", models.DateTimeField(auto_now=True, null=True)),
+                ("name", models.CharField(max_length=100, unique=True)),
+                ("text", models.CharField(max_length=500)),
+                ("weight", models.PositiveSmallIntegerField(default=100)),
+                ("group_name", models.CharField(blank=True, max_length=50)),
+                ("button_class", models.CharField(default="default", max_length=30)),
+                ("confirmation", models.BooleanField(default=True)),
+                (
+                    "content_type",
+                    models.ForeignKey(
+                        limit_choices_to=nautobot.extras.utils.FeatureQuery("job_buttons"),
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="contenttypes.contenttype",
+                    ),
+                ),
+                ("job", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="extras.job")),
             ],
             options={
-                'ordering': ['group_name', 'weight', 'name'],
+                "ordering": ["group_name", "weight", "name"],
             },
             bases=(models.Model, nautobot.extras.models.mixins.NotesMixin),
         ),
