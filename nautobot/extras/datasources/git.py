e0a831c0322048dfdf751a718bc824161aa20da7
Bryan Culver <bryan.culver@networktocode.com>
2023-03-03 16:05:28 -0500

    Update from feedback.
    Co-authored-by: Hanlin Miao <46973263+HanlinMiao@users.noreply.github.com>

--- nautobot/extras/datasources/git.py
+++ nautobot/extras/datasources/git.py
@@ -26,13 +26,13 @@ from nautobot.extras.choices import (
 from nautobot.extras.models import (
     ConfigContext,
     ConfigContextSchema,
+    DynamicGroup,
     ExportTemplate,
     GitRepository,
     Job,
     JobLogEntry,
     JobResult,
     Tag,
-    DynamicGroup,
 )
 from nautobot.extras.registry import DatasourceContent, register_datasource_contents
 from nautobot.extras.utils import jobs_in_directory, refresh_job_model_from_job_class
