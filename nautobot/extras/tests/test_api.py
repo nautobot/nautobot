070029991a77e36ea21981ebab5fb2a2c5e1c57e
Bryan Culver <bryan.culver@networktocode.com>
2023-04-20 15:44:14 -0400

    Add message check

--- nautobot/extras/tests/test_api.py
+++ nautobot/extras/tests/test_api.py
@@ -3358,6 +3358,7 @@ class SecretTest(APIViewTestCases.APIViewTestCase):
             response = self.client.get(reverse("extras-api:secret-check", kwargs={"pk": test_secret.pk}), **self.header)
             self.assertHttpStatus(response, status.HTTP_200_OK)
             self.assertEqual(response.data["result"], False)
+            self.assertContains(response.data["message"], "SecretValueNotFoundError")
 
 
 class SecretsGroupTest(APIViewTestCases.APIViewTestCase):
