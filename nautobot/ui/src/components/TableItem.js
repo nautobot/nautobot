bf429a18927c186ac466705cf5ab86ef1703308a
Gary Snider <gary.snider@networktocode.com>
2023-04-26 09:47:03 -0500

    typo and formatting fixes

--- nautobot/ui/src/components/TableItem.js
+++ nautobot/ui/src/components/TableItem.js
@@ -4,61 +4,52 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import { Button } from "@nautobot/nautobot-ui";
 
-
-const TextOrButton = ({obj}) => {
+const TextOrButton = ({ obj }) => {
     if (typeof obj === "object") {
-        const display = obj.display || obj.label
+        const display = obj.display || obj.label;
         if (!obj.color) {
-            return display
+            return display;
         }
         return (
-            <Button 
-                size="xs"
-                className={"ntc-btn-" + obj.color}
-            >
+            <Button size="xs" className={"ntc-btn-" + obj.color}>
                 {display}
             </Button>
-        )
+        );
     }
-    return obj
+    return obj;
+};
 
-    
-}
-
-const TableColumDisplay = ({obj}) => {
+const TableColumnDisplay = ({ obj }) => {
     if (!obj) {
-        return (<FontAwesomeIcon icon={faMinus} />)
-    }
-
-    else if (typeof obj === "object"  && !Array.isArray(obj)) {
-        return <TextOrButton obj={obj} />
-    }
-
-    else if (Array.isArray(obj)) {
+        return <FontAwesomeIcon icon={faMinus} />;
+    } else if (typeof obj === "object" && !Array.isArray(obj)) {
+        return <TextOrButton obj={obj} />;
+    } else if (Array.isArray(obj)) {
         if (typeof obj[0] == "object") {
             return (
                 <>
-                    {
-                        obj.map((item, idx) => <TextOrButton obj={item} key={idx} />)
-                    }
+                    {obj.map((item, idx) => (
+                        <TextOrButton obj={item} key={idx} />
+                    ))}
                 </>
-            )
+            );
         } else {
             return obj.join(", ");
         }
+    } else {
+        return obj;
     }
-
-    else {
-        return obj
-    }
-}
-
+};
 
 export default function TableItem({ name, obj, url }) {
     if (url) {
         // #TODO(timizuo): Use the secondary color here instead of hardcoding it
-        return <Link style={{color: "#007dff"}} to={url}><TableColumDisplay obj={obj} /></Link>
+        return (
+            <Link style={{ color: "#007dff" }} to={url}>
+                <TableColumnDisplay obj={obj} />
+            </Link>
+        );
     } else {
-        return <TableColumDisplay obj={obj} />;
+        return <TableColumnDisplay obj={obj} />;
     }
 }
