c08cf358afb675316e8dc12c3af5b28c0f671a10
Hanlin Miao <hanlinmiao@Hanlins-MBP-2.home>
2023-05-17 11:38:26 -0400

    revert more changes in example_plugin_with_view_override

---
+++ examples/example_plugin_with_view_override/example_plugin_with_view_override/ui/CustomView.js
@@ -0,0 +1,30 @@
+import Table from 'react-bootstrap/Table';
+
+export function CustomTableView(props) {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Key</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>ID</td>
+          <td>{props.id}</td>
+        </tr>
+        <tr>
+          <td>Display</td>
+          <td>{props.display}</td>
+        </tr>
+        <tr>
+          <td>URL</td>
+          <td>{props.url}</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default CustomTableView;
