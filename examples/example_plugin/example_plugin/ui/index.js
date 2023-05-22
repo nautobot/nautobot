af0cc5f752aeb13d3eefc5e40d8b4b4d8b1ed863
Timizuo <94907097+timizuoebideri1@users.noreply.github.com>
2023-05-22 02:48:19 +0200

    Re-introduce app provided model view override (#3721)
    * Reintroduce Plugin Override
    
    * prettier
    
    * Stage One: Add custom plugin route to nautobot ui route
    
    * Add example plugin override
    
    * update view
    
    * Non Model Component Views
    
    * Remove example app with override
    
    * non model view complete
    
    * rename CustomView to ModelView and move it to example_plugin
    
    * disable example_plugin_with_view_override
    
    * Override AnotherExampleModel retrieve view and override LocationRetrieveView from example_plugin_with_view_override/ui/index.js/
    
    * address PR feedback
    
    * revert some of the changes in example_plugin_with_view_override
    
    * revert more changes in example_plugin_with_view_override
    
    * revert FullWidthPage.js
    
    * revert FullWidthPage.js
    
    * check if my changes solid
    
    * export DetailFieldValue
    
    * Documentation on app provided model view override
    
    ---------
    
    Co-authored-by: Hanlin Miao <hanlinmiao@Hanlins-MBP-2.home>

--- examples/example_plugin/example_plugin/ui/index.js
+++ examples/example_plugin/example_plugin/ui/index.js
@@ -1,11 +1,16 @@
 export { default as ExampleAppFullWidthPageComponent } from "./FullWidthPage"
+export { default as ExampleAppOverrideModelView } from "./ModelView"
 
 const app_config = {
     detail_tabs: {},
     full_width_components: {
-        "dcim:sites": ["ExampleAppFullWidthPageComponent"]
+        "dcim:devices": ["ExampleAppFullWidthPageComponent"]
     },
-    view_overrides: {}
+    view_overrides: {
+        "example-plugin:other-models": {
+            "retrieve": "ExampleAppOverrideModelView"
+        },
+    }
 }
 
 export default app_config
--- nautobot/ui/src/views/generic/ObjectRetrieve.js
+++ nautobot/ui/src/views/generic/ObjectRetrieve.js
@@ -75,7 +75,7 @@ function render_header(value) {
     return value;
 }
 
-function DetailFieldValue(value) {
+export function DetailFieldValue(value) {
     const ref = useRef();
     if (value === undefined) {
         return <>&mdash;</>;
@@ -204,7 +204,6 @@ export default function ObjectRetrieve({ api_url }) {
         app_name: "extras",
         model_name: "object-changes",
         schema: true,
-        plugin: isPluginView,
     });
     // Note Data
     const notes_url = `/api/${pluginPrefix}${app_name}/${model_name}/${object_id}/notes/`;
@@ -222,7 +221,6 @@ export default function ObjectRetrieve({ api_url }) {
         app_name: "extras",
         model_name: "notes",
         schema: true,
-        plugin: isPluginView,
     });
 
     if (error || note_error || changelog_error) {
