a53c9a97383534bfbf768efa44891a26f173c8b6
DistantVoyager <111259311+DistantVoyager@users.noreply.github.com>
2023-05-23 14:26:52 -0400

    Updated LoadingWidget to correctly center it in UI

--- nautobot/ui/src/components/LoadingWidget.js
+++ nautobot/ui/src/components/LoadingWidget.js
@@ -1,24 +1,13 @@
 import { Text } from "@nautobot/nautobot-ui";
-import spinningNautobot from "../assets/spinning-nautobot.gif";
 
 // A helpful, consistent loading widget
 export function LoadingWidget({ name = "" }) {
     const display_name = name.length > 0 ? " " + name : "";
     return (
-        <div
-            style={{
-                textAlign: "center",
-                width: "100%",
-                position: "relative",
-                top: "40%",
-            }}
-        >
-            <img
-                src={spinningNautobot}
-                alt="spinning-nautobot"
-                style={{ marginLeft: "auto", marginRight: "auto" }}
-            />
-            <Text color="gray.500">Loading{display_name}...</Text>
+        <div style={{ textAlign: "center", position: "relative", top: "40%", gridColumn: "2 / span 2" }}>
+            <StatusIndicator variant="action" breathe={true} />
+            <Spinner size="lg" color="blue.500" />
+            <Text ml={1} color="gray-3">Loading{display_name}...</Text>
         </div>
     );
 }
