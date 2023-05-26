714fc778e169aecc8a49e66e07ed1c48d960a269
Bryan Culver <bryan.culver@networktocode.com>
2023-05-26 14:09:58 -0400

    Address feedback

--- nautobot/extras/migrations/0087_joblogentry__log_level_data_migration.py
+++ nautobot/extras/migrations/0087_joblogentry__log_level_data_migration.py
@@ -2,7 +2,7 @@ from django.db import migrations
 
 
 LOG_LEVEL_MAPPING = {
-    "default": "info",
+    "default": "debug",
     "success": "info",
     "failure": "error",
 }
@@ -11,7 +11,7 @@ LOG_LEVEL_MAPPING = {
 def migrate_job_log_entry_log_levels(apps, schema_editor):
     """
     Migrate job log levels from 1.0 values to 2.0 values.
-    default => info
+    default => debug
     success => info
     failure => error
     """
