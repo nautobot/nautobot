75bf8cc1fdfbfc6e4b2832c5a6ef5c76befb6a6e
Jathan McCollum <jathan@gmail.com>
2023-06-14 15:08:22 -0700

    Restore some more code that didn't make the merge.

--- nautobot/extras/views.py
+++ nautobot/extras/views.py
@@ -1463,7 +1463,7 @@ class JobResultListView(generic.ObjectListView):
     List JobResults
     """
 
-    queryset = JobResult.objects.select_related("job_model", "user").prefetch_related("logs")
+    queryset = JobResult.objects.defer("result").select_related("job_model", "user").prefetch_related("logs")
     filterset = filters.JobResultFilterSet
     filterset_form = forms.JobResultFilterForm
     table = tables.JobResultTable
@@ -1475,7 +1475,7 @@ class JobResultDeleteView(generic.ObjectDeleteView):
 
 
 class JobResultBulkDeleteView(generic.BulkDeleteView):
-    queryset = JobResult.objects.defer("data").all()
+    queryset = JobResult.objects.defer("result").all()
     table = tables.JobResultTable
 
 
