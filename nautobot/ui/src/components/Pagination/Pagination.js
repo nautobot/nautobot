58fd11809f9f86be8376b5764920c042cba182c6
DistantVoyager <111259311+DistantVoyager@users.noreply.github.com>
2023-07-05 16:00:37 -0400

    Change Pagination component from class to function

--- nautobot/ui/src/components/Pagination/Pagination.js
+++ nautobot/ui/src/components/Pagination/Pagination.js
@@ -1,68 +1,49 @@
 import { Box, Flex } from "@nautobot/nautobot-ui";
 import { Spacer } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 
 import { PageSizeForm, PageNumberForm } from "@components/Pagination";
 
-class Pagination extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
+function Pagination(props) {
+    const [page_size] = useState(props.page_size);
+
+    let data_count = props.data_count;
+    let scroll_ref = props.scroll_ref;
+    let active_page = props.active_page;
+    if (!active_page) {
+        active_page = 0;
+    }
+    let num_pages;
+
+    // trueCurrentPage increments active_page (which starts at 0) by 1 to get the accurate human-form page number
+    let trueCurrentPage = ~~active_page + 1;
+
+    if (data_count % page_size === 0) {
+        num_pages = data_count / page_size;
+    } else {
+        num_pages = data_count / page_size + 1;
     }
 
-    render() {
-        let data_count;
-        let scroll_ref;
-        let active_page;
-        let page_size;
-        let num_pages;
+    // convert float to int
+    num_pages = ~~num_pages;
 
-        data_count = this.props.data_count;
-        scroll_ref = this.props.scroll_ref;
-        active_page = this.props.active_page;
-        if (!active_page) {
-            active_page = 0;
-        }
-
-        // trueCurrentPage increments active_page (which starts at 0) by 1 to get the accurate human-form page number
-        let trueCurrentPage = ~~active_page + 1;
-
-        if (Object.keys(this.state).length === 0) {
-            page_size = this.props.page_size;
-        } else {
-            page_size = this.state.page_size;
-        }
-
-        if (data_count % page_size === 0) {
-            num_pages = data_count / page_size;
-        } else {
-            num_pages = data_count / page_size + 1;
-        }
-        // convert float to int
-        num_pages = ~~num_pages;
-
-        // const pages = [];
-
-        // let list_url = url.split("?")[0]; // strip the query parameters to retain the original list url
-
-        return (
-            <Flex paddingTop={10}>
-                <Box>
-                    <PageNumberForm
-                        firstPage={1}
-                        lastPage={num_pages}
-                        pageSize={page_size}
-                        scroll_ref={scroll_ref}
-                        totalDataCount={data_count}
-                        trueCurrentPage={trueCurrentPage}
-                    ></PageNumberForm>
-                </Box>
-                <Spacer />
-                <Box>
-                    <PageSizeForm scroll_ref={scroll_ref}></PageSizeForm>
-                </Box>
-            </Flex>
-        );
-    }
+    return (
+        <Flex paddingTop={10}>
+            <Box>
+                <PageNumberForm
+                    firstPage={1}
+                    lastPage={num_pages}
+                    pageSize={page_size}
+                    scroll_ref={scroll_ref}
+                    totalDataCount={data_count}
+                    trueCurrentPage={trueCurrentPage}
+                ></PageNumberForm>
+            </Box>
+            <Spacer />
+            <Box>
+                <PageSizeForm scroll_ref={scroll_ref}></PageSizeForm>
+            </Box>
+        </Flex>
+    );
 }
 export default Pagination;
