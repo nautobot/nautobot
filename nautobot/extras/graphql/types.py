adc8bd6285f9400350f08973ababdfa705eb9871
James Harr <jharr@internet2.edu>
2023-07-25 18:14:41 +0000

    feat(graphql): Create base graphql object that can resolve a URL for all otehr objects

--- nautobot/extras/graphql/types.py
+++ nautobot/extras/graphql/types.py
@@ -1,7 +1,29 @@
+import graphene
+from graphene_django import DjangoObjectType
 import graphene_django_optimizer as gql_optimizer
 
 from nautobot.extras.models import Status, Tag, DynamicGroup
 from nautobot.extras.filters import StatusFilterSet, TagFilterSet, DynamicGroupFilterSet
+
+
+class NautobotObjectType(DjangoObjectType):
+    url = graphene.String()
+
+    def resolve_url(self, args):
+        return self.get_absolute_url(api=True)
+
+    class Meta:
+        abstract = True
+
+
+class OptimizedNautobotObjectType(gql_optimizer.OptimizedDjangoObjectType):
+    url = graphene.String()
+
+    def resolve_url(self, args):
+        return self.get_absolute_url(api=True)
+
+    class Meta:
+        abstract = True
 
 
 class TagType(gql_optimizer.OptimizedDjangoObjectType):
