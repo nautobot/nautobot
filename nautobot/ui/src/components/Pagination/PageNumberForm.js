d7f5af7310c87df3ddac8a9d86cf269ae12882c0
Steven W <111259311+DistantVoyager@users.noreply.github.com>
2023-07-27 13:52:07 -0400

    Pagination upgrades (#4155)
    * Fixes the last page number dynamically updating
    
    * Page number now updates if user hits Enter key
    
    * Change fragments
    
    * Add double arrows to pagination for 1st/last page
    
    * Updated change fragment
    
    * Prettier formatting
    
    * Placeholder number disappears when input focused
    
    * Prettier formatting
    
    * Revert "Prettier formatting"
    
    This reverts commit 91289daea00f1f9be632b2ec2694973718a9f670.
    
    * Minor fix for prettier

--- nautobot/ui/src/components/Pagination/PageNumberForm.js
+++ nautobot/ui/src/components/Pagination/PageNumberForm.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Box, Flex, FormControl, Input, Text } from "@nautobot/nautobot-ui";
 import { IconButton } from "@chakra-ui/react";
-import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import {
+    ArrowLeftIcon,
+    ArrowRightIcon,
+    ChevronLeftIcon,
+    ChevronRightIcon,
+} from "@chakra-ui/icons";
 
 export default function PageNumberForm({
     firstPage,
@@ -18,11 +23,18 @@ export default function PageNumberForm({
     const [pageNumber, setPageNumber] = useState(trueCurrentPage);
     // State to track the page number form control input validity
     const [isInputInvalid, setIsInputInvalid] = useState(false);
+    // State to track if the input box is focused
+    const [isInputFocused, setIsInputFocused] = useState(false);
 
     // Keeps the pageNumber var updated to trueCurrentPage (i.e. if trueCurrentPage changes via arrow click)
     useEffect(() => {
         setPageNumber(trueCurrentPage);
     }, [trueCurrentPage]);
+
+    // Sets isInputFocused to true
+    function handleOnFocus() {
+        setIsInputFocused(true);
+    }
 
     // Changes the page number based on the input component after a blur event (i.e. when user clicks out of input box and element loses focus)
     function handleOnBlur(event) {
@@ -51,6 +63,41 @@ export default function PageNumberForm({
         // Otherwise, mark input as invalid
         else {
             setIsInputInvalid(true);
+        }
+        setIsInputFocused(false);
+    }
+
+    function handleOnKeyDown(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+
+            const enteredValue = parseInt(event.target.value);
+            // Checks that the entered value is a number and within the valid page range
+            if (
+                !isNaN(enteredValue) &&
+                enteredValue >= firstPage &&
+                enteredValue <= lastPage
+            ) {
+                setPageNumber(enteredValue);
+                setIsInputInvalid(false);
+
+                // Checks that the entered value is not already the current page
+                if (enteredValue !== trueCurrentPage) {
+                    onPageNumberChange(enteredValue);
+                }
+
+                // Reset input value
+                event.target.value = "";
+            }
+            // Checks if nothing is entered into input and, if so, do nothing
+            else if (event.target.value === "") {
+                setIsInputInvalid(false);
+            }
+            // Otherwise, mark input as invalid
+            else {
+                setIsInputInvalid(true);
+            }
+            setIsInputFocused(false);
         }
     }
 
@@ -98,17 +145,30 @@ export default function PageNumberForm({
                 You are on page
             </Text>
             {trueCurrentPage > firstPage ? (
-                <IconButton
-                    onClick={() => onPageNumberChange("<")}
-                    icon={<ChevronLeftIcon />}
-                    color="gray-4"
-                    variant="ghost"
-                    pl="xs"
-                    _hover={{
-                        transform: "scale(1.2)",
-                        color: "#007DFF",
-                    }}
-                />
+                <Box>
+                    <IconButton
+                        onClick={() => onPageNumberChange(firstPage)}
+                        icon={<ArrowLeftIcon boxSize={3} />}
+                        color="gray-4"
+                        variant="ghost"
+                        pl="sm"
+                        _hover={{
+                            transform: "scale(1.2)",
+                            color: "#007DFF",
+                        }}
+                    />
+                    <IconButton
+                        onClick={() => onPageNumberChange("<")}
+                        icon={<ChevronLeftIcon />}
+                        color="gray-4"
+                        variant="ghost"
+                        pl="xs"
+                        _hover={{
+                            transform: "scale(1.2)",
+                            color: "#007DFF",
+                        }}
+                    />
+                </Box>
             ) : (
                 <Box pl="xs" />
             )}
@@ -120,24 +180,39 @@ export default function PageNumberForm({
             >
                 <Input
                     type="number"
-                    placeholder={pageNumber}
+                    placeholder={isInputFocused ? "" : pageNumber}
                     textAlign="center"
                     width="50px"
+                    onFocus={handleOnFocus}
                     onBlur={handleOnBlur}
+                    onKeyDown={handleOnKeyDown}
                 />
             </FormControl>
             {trueCurrentPage < lastPage ? (
-                <IconButton
-                    onClick={() => onPageNumberChange(">")}
-                    icon={<ChevronRightIcon />}
-                    color="gray-4"
-                    variant="ghost"
-                    pr="xs"
-                    _hover={{
-                        transform: "scale(1.2)",
-                        color: "#007DFF",
-                    }}
-                />
+                <Box>
+                    <IconButton
+                        onClick={() => onPageNumberChange(">")}
+                        icon={<ChevronRightIcon />}
+                        color="gray-4"
+                        variant="ghost"
+                        pr="xs"
+                        _hover={{
+                            transform: "scale(1.2)",
+                            color: "#007DFF",
+                        }}
+                    />
+                    <IconButton
+                        onClick={() => onPageNumberChange(lastPage)}
+                        icon={<ArrowRightIcon boxSize={3} />}
+                        color="gray-4"
+                        variant="ghost"
+                        pr="sm"
+                        _hover={{
+                            transform: "scale(1.2)",
+                            color: "#007DFF",
+                        }}
+                    />
+                </Box>
             ) : (
                 <Box pr="xs" />
             )}
