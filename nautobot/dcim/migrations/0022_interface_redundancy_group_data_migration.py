3db49e1559e848ca45fb7750b13b9972f904bb69
Greg Mueller <greg@grelleum.com>
2023-07-27 12:58:38 -0400

    s/device/interface/g

--- nautobot/dcim/migrations/0022_interface_redundancy_group_data_migration.py
+++ nautobot/dcim/migrations/0022_interface_redundancy_group_data_migration.py
@@ -2,12 +2,12 @@ from django.db import migrations
 from nautobot.extras.management import clear_status_choices, populate_status_choices
 
 
-def populate_device_redundancy_group_status(apps, schema_editor):
+def populate_interface_redundancy_group_status(apps, schema_editor):
     """Create/link default Status records for the InterfaceRedundancyGroup content-type."""
     populate_status_choices(apps, schema_editor, models=["dcim.InterfaceRedundancyGroup"])
 
 
-def clear_device_redundancy_group_status(apps, schema_editor):
+def clear_interface_redundancy_group_status(apps, schema_editor):
     """De-link/delete all Status records from the InterfaceRedundancyGroup content-type."""
     clear_status_choices(apps, schema_editor, models=["dcim.InterfaceRedundancyGroup"])
 
@@ -19,5 +19,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(populate_device_redundancy_group_status, clear_device_redundancy_group_status),
+        migrations.RunPython(populate_interface_redundancy_group_status, clear_interface_redundancy_group_status),
     ]
