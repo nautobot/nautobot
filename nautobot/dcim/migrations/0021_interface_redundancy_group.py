cf1428a45d62bb00f26b9b6be7dbd59d1ba34a79
Greg Mueller <greg@grelleum.com>
2023-07-27 12:54:46 -0400

    Formatted with black.

--- nautobot/dcim/migrations/0021_interface_redundancy_group.py
+++ nautobot/dcim/migrations/0021_interface_redundancy_group.py
@@ -15,84 +15,148 @@ import uuid
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
-        ('extras', '0058_jobresult_add_time_status_idxs'),
-        ('ipam', '0009_alter_vlan_name'),
-        ('dcim', '0020_increase_device_asset_tag_size_limit'),
+        ("contenttypes", "0002_remove_content_type_name"),
+        ("extras", "0058_jobresult_add_time_status_idxs"),
+        ("ipam", "0009_alter_vlan_name"),
+        ("dcim", "0020_increase_device_asset_tag_size_limit"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='InterfaceRedundancyGroup',
+            name="InterfaceRedundancyGroup",
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True)),
-                ('created', models.DateField(auto_now_add=True, null=True)),
-                ('last_updated', models.DateTimeField(auto_now=True, null=True)),
-                ('_custom_field_data', models.JSONField(blank=True, default=dict, encoder=django.core.serializers.json.DjangoJSONEncoder)),
-                ('local_context_data', models.JSONField(blank=True, encoder=django.core.serializers.json.DjangoJSONEncoder, null=True)),
-                ('local_context_data_owner_object_id', models.UUIDField(blank=True, default=None, null=True)),
-                ('name', models.CharField(max_length=100, unique=True)),
-                ('slug', nautobot.core.fields.AutoSlugField(blank=True, max_length=100, populate_from='name', unique=True)),
-                ('description', models.CharField(blank=True, max_length=200)),
-                ('protocol', models.CharField(blank=True, max_length=50)),
-                ('group_id', models.CharField(blank=True, max_length=50)),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True
+                    ),
+                ),
+                ("created", models.DateField(auto_now_add=True, null=True)),
+                ("last_updated", models.DateTimeField(auto_now=True, null=True)),
+                (
+                    "_custom_field_data",
+                    models.JSONField(blank=True, default=dict, encoder=django.core.serializers.json.DjangoJSONEncoder),
+                ),
+                (
+                    "local_context_data",
+                    models.JSONField(blank=True, encoder=django.core.serializers.json.DjangoJSONEncoder, null=True),
+                ),
+                ("local_context_data_owner_object_id", models.UUIDField(blank=True, default=None, null=True)),
+                ("name", models.CharField(max_length=100, unique=True)),
+                (
+                    "slug",
+                    nautobot.core.fields.AutoSlugField(blank=True, max_length=100, populate_from="name", unique=True),
+                ),
+                ("description", models.CharField(blank=True, max_length=200)),
+                ("protocol", models.CharField(blank=True, max_length=50)),
+                ("group_id", models.CharField(blank=True, max_length=50)),
             ],
             options={
-                'ordering': ['name'],
+                "ordering": ["name"],
             },
-            bases=(models.Model, nautobot.extras.models.mixins.DynamicGroupMixin, nautobot.extras.models.mixins.NotesMixin, nautobot.extras.models.models.ConfigContextSchemaValidationMixin),
+            bases=(
+                models.Model,
+                nautobot.extras.models.mixins.DynamicGroupMixin,
+                nautobot.extras.models.mixins.NotesMixin,
+                nautobot.extras.models.models.ConfigContextSchemaValidationMixin,
+            ),
         ),
         migrations.CreateModel(
-            name='InterfaceRedundancyGroupAssociation',
+            name="InterfaceRedundancyGroupAssociation",
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True)),
-                ('created', models.DateField(auto_now_add=True, null=True)),
-                ('last_updated', models.DateTimeField(auto_now=True, null=True)),
-                ('_custom_field_data', models.JSONField(blank=True, default=dict, encoder=django.core.serializers.json.DjangoJSONEncoder)),
-                ('priority', models.PositiveSmallIntegerField()),
-                ('group', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='dcim.interfaceredundancygroup')),
-                ('interface', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='dcim.interface')),
-                ('tags', taggit.managers.TaggableManager(through='extras.TaggedItem', to='extras.Tag')),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True
+                    ),
+                ),
+                ("created", models.DateField(auto_now_add=True, null=True)),
+                ("last_updated", models.DateTimeField(auto_now=True, null=True)),
+                (
+                    "_custom_field_data",
+                    models.JSONField(blank=True, default=dict, encoder=django.core.serializers.json.DjangoJSONEncoder),
+                ),
+                ("priority", models.PositiveSmallIntegerField()),
+                (
+                    "group",
+                    models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="dcim.interfaceredundancygroup"),
+                ),
+                ("interface", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="dcim.interface")),
+                ("tags", taggit.managers.TaggableManager(through="extras.TaggedItem", to="extras.Tag")),
             ],
             options={
-                'ordering': ('group', '-priority'),
-                'unique_together': {('group', 'interface')},
+                "ordering": ("group", "-priority"),
+                "unique_together": {("group", "interface")},
             },
-            bases=(models.Model, nautobot.extras.models.mixins.DynamicGroupMixin, nautobot.extras.models.mixins.NotesMixin),
+            bases=(
+                models.Model,
+                nautobot.extras.models.mixins.DynamicGroupMixin,
+                nautobot.extras.models.mixins.NotesMixin,
+            ),
         ),
         migrations.AddField(
-            model_name='interfaceredundancygroup',
-            name='interfaces',
-            field=models.ManyToManyField(blank=True, related_name='redundancy_groups', through='dcim.InterfaceRedundancyGroupAssociation', to='dcim.Interface'),
+            model_name="interfaceredundancygroup",
+            name="interfaces",
+            field=models.ManyToManyField(
+                blank=True,
+                related_name="redundancy_groups",
+                through="dcim.InterfaceRedundancyGroupAssociation",
+                to="dcim.Interface",
+            ),
         ),
         migrations.AddField(
-            model_name='interfaceredundancygroup',
-            name='local_context_data_owner_content_type',
-            field=models.ForeignKey(blank=True, default=None, limit_choices_to=nautobot.extras.utils.FeatureQuery('config_context_owners'), null=True, on_delete=django.db.models.deletion.CASCADE, to='contenttypes.contenttype'),
+            model_name="interfaceredundancygroup",
+            name="local_context_data_owner_content_type",
+            field=models.ForeignKey(
+                blank=True,
+                default=None,
+                limit_choices_to=nautobot.extras.utils.FeatureQuery("config_context_owners"),
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                to="contenttypes.contenttype",
+            ),
         ),
         migrations.AddField(
-            model_name='interfaceredundancygroup',
-            name='local_context_schema',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='extras.configcontextschema'),
+            model_name="interfaceredundancygroup",
+            name="local_context_schema",
+            field=models.ForeignKey(
+                blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to="extras.configcontextschema"
+            ),
         ),
         migrations.AddField(
-            model_name='interfaceredundancygroup',
-            name='secrets_group',
-            field=models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.SET_NULL, to='extras.secretsgroup'),
+            model_name="interfaceredundancygroup",
+            name="secrets_group",
+            field=models.ForeignKey(
+                blank=True,
+                default=None,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to="extras.secretsgroup",
+            ),
         ),
         migrations.AddField(
-            model_name='interfaceredundancygroup',
-            name='status',
-            field=nautobot.extras.models.statuses.StatusField(null=True, on_delete=django.db.models.deletion.PROTECT, related_name='dcim_interfaceredundancygroup_related', to='extras.status'),
+            model_name="interfaceredundancygroup",
+            name="status",
+            field=nautobot.extras.models.statuses.StatusField(
+                null=True,
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name="dcim_interfaceredundancygroup_related",
+                to="extras.status",
+            ),
         ),
         migrations.AddField(
-            model_name='interfaceredundancygroup',
-            name='tags',
-            field=taggit.managers.TaggableManager(through='extras.TaggedItem', to='extras.Tag'),
+            model_name="interfaceredundancygroup",
+            name="tags",
+            field=taggit.managers.TaggableManager(through="extras.TaggedItem", to="extras.Tag"),
         ),
         migrations.AddField(
-            model_name='interfaceredundancygroup',
-            name='virtual_ip',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='interface_redundancy_virtual_ip', to='ipam.ipaddress'),
+            model_name="interfaceredundancygroup",
+            name="virtual_ip",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="interface_redundancy_virtual_ip",
+                to="ipam.ipaddress",
+            ),
         ),
     ]
