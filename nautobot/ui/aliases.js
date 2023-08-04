5e721b0251c89f92eb285dba5c85af572c2a685a
Timizuo <ebideritimizuo@gmail.com>
2023-08-04 12:18:09 +0100

    Fix pritier

--- nautobot/ui/aliases.js
+++ nautobot/ui/aliases.js
@@ -1,43 +1,48 @@
-const path = require('path');
-const fs = require('fs');
-const jsconfig = require('./jsconfig.json');
+const path = require("path");
+const fs = require("fs");
+const jsconfig = require("./jsconfig.json");
 
 const appDirectory = fs.realpathSync(process.cwd());
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
 const jsconfigPaths = resolveApp(jsconfig.extends);
-const extendedJsConfig = require(jsconfigPaths)
-const compiledJsconfig = {...jsconfig, ...extendedJsConfig}
+const extendedJsConfig = require(jsconfigPaths);
+const compiledJsconfig = { ...jsconfig, ...extendedJsConfig };
 
-const createWebpackAliasesFromJsconfig = (jsconfig)  => {
-  const webpackAliases = {};
+const createWebpackAliasesFromJsconfig = (jsconfig) => {
+    const webpackAliases = {};
 
-  for (let key in jsconfig.compilerOptions.paths) {
-    // Remove the /* from the key
-    const aliasKey = key.replace('/*', '');
-    // Remove the /* from the value and resolve the path
-    const aliasValue = jsconfig.compilerOptions.paths[key][0].replace('/*', '');
-    webpackAliases[aliasKey] = resolveApp(aliasValue);
-  }
-  return webpackAliases;
-}
+    for (let key in jsconfig.compilerOptions.paths) {
+        // Remove the /* from the key
+        const aliasKey = key.replace("/*", "");
+        // Remove the /* from the value and resolve the path
+        const aliasValue = jsconfig.compilerOptions.paths[key][0].replace(
+            "/*",
+            ""
+        );
+        webpackAliases[aliasKey] = resolveApp(aliasValue);
+    }
+    return webpackAliases;
+};
 
-const createJestAliasesFromJsconfig = (jsconfig)  => {
-  const webpackAliases = {};
-  // ^@generated/(.*)$
+const createJestAliasesFromJsconfig = (jsconfig) => {
+    const webpackAliases = {};
+    // ^@generated/(.*)$
 
-  for (let key in jsconfig.compilerOptions.paths) {
-    // Remove the /* from the key
-    const aliasKey = key.replace('/*', '');
-    // Remove the /* from the value and resolve the path
-    const aliasValue = jsconfig.compilerOptions.paths[key][0].replace('/*', '');
-    webpackAliases[`^${aliasKey}/(.*)$`] = `${resolveApp(aliasValue)}/$1`;
-  }
-  return webpackAliases;
-}
-
+    for (let key in jsconfig.compilerOptions.paths) {
+        // Remove the /* from the key
+        const aliasKey = key.replace("/*", "");
+        // Remove the /* from the value and resolve the path
+        const aliasValue = jsconfig.compilerOptions.paths[key][0].replace(
+            "/*",
+            ""
+        );
+        webpackAliases[`^${aliasKey}/(.*)$`] = `${resolveApp(aliasValue)}/$1`;
+    }
+    return webpackAliases;
+};
 
 module.exports = {
-  "webpack": createWebpackAliasesFromJsconfig(compiledJsconfig),
-  "jest": createJestAliasesFromJsconfig(compiledJsconfig),
-}
+    webpack: createWebpackAliasesFromJsconfig(compiledJsconfig),
+    jest: createJestAliasesFromJsconfig(compiledJsconfig),
+};
--- nautobot/ui/config/jest/babelTransform.js
+++ nautobot/ui/config/jest/babelTransform.js
@@ -1,29 +1,29 @@
-'use strict';
+"use strict";
 
-const babelJest = require('babel-jest').default;
+const babelJest = require("babel-jest").default;
 
 const hasJsxRuntime = (() => {
-  if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
-    return false;
-  }
+    if (process.env.DISABLE_NEW_JSX_TRANSFORM === "true") {
+        return false;
+    }
 
-  try {
-    require.resolve('react/jsx-runtime');
-    return true;
-  } catch (e) {
-    return false;
-  }
+    try {
+        require.resolve("react/jsx-runtime");
+        return true;
+    } catch (e) {
+        return false;
+    }
 })();
 
 module.exports = babelJest.createTransformer({
-  presets: [
-    [
-      require.resolve('babel-preset-react-app'),
-      {
-        runtime: hasJsxRuntime ? 'automatic' : 'classic',
-      },
+    presets: [
+        [
+            require.resolve("babel-preset-react-app"),
+            {
+                runtime: hasJsxRuntime ? "automatic" : "classic",
+            },
+        ],
     ],
-  ],
-  babelrc: false,
-  configFile: false,
+    babelrc: false,
+    configFile: false,
 });
