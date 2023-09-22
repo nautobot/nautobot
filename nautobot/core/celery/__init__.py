0a9e7826c83508b508aa76de5828668eaf875d6f
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2023-09-22 19:08:02 +0000

    update doc

--- nautobot/core/celery/__init__.py
+++ nautobot/core/celery/__init__.py
@@ -229,6 +229,7 @@ def register_job(job_class):
     app.register_task(job_class)
 
 
+# TODO: remove this function in Nautobot 3.0.0
 def register_jobs(*jobs):
-    """Deprecated manual job registration function. Left in for backwards compatibility to Nautobot 2.0.0-alpha"""
+    """Deprecated manual job registration function. Left here for backwards compatibility to Nautobot 2.0.0-beta.1"""
     pass
