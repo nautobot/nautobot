9d2edcc3351b67f6c2901bc66cc656acffda7b66
Bryan Culver <bryan.culver@networktocode.com>
2023-09-25 14:46:20 -0400

    Remove UI and docs references to composite_key.

--- nautobot/core/views/utils.py
+++ nautobot/core/views/utils.py
@@ -146,13 +146,13 @@ def get_csv_form_fields_from_serializer_class(serializer_class):
                 field_info["format"] = mark_safe('<code>"app_label.model,app_label.model"</code>')
             else:
                 field_info["format"] = mark_safe(
-                    '<code>"composite_key,composite_key"</code> or <code>"UUID,UUID"</code>'
+                    '<code>"UUID,UUID"</code>'
                 )
         elif isinstance(field, serializers.RelatedField):
             if isinstance(field, ContentTypeField):
                 field_info["format"] = mark_safe("<code>app_label.model</code>")
             else:
-                field_info["format"] = mark_safe("<code>composite_key</code> or <code>UUID</code>")
+                field_info["format"] = mark_safe("<code>UUID</code>")
         elif isinstance(field, (serializers.ListField, serializers.MultipleChoiceField)):
             field_info["format"] = mark_safe('<code>"value,value"</code>')
         elif isinstance(field, (serializers.DictField, serializers.JSONField)):
