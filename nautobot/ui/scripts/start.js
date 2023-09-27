8a661057d1d070ba1f7eb7b5fc34d5e9c9c481f7
Glenn Matthews <glenn.matthews@networktocode.com>
2023-09-27 11:36:08 -0400

    More eslint

--- nautobot/ui/scripts/start.js
+++ nautobot/ui/scripts/start.js
@@ -28,7 +28,6 @@ const openBrowser = require("react-dev-utils/openBrowser");
 const paths = require("../config/paths");
 const configFactory = require("../config/webpack.config");
 const createDevServerConfig = require("../config/webpackDevServer.config");
-const getClientEnvironment = require("../config/env");
 
 const useYarn = fs.existsSync(paths.yarnLockFile);
 const isInteractive = process.stdout.isTTY;
