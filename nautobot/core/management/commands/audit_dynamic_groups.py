a9d81a2dfe9c85135e109425a3a01bb6b8e57749
Glenn Matthews <glenn.matthews@networktocode.com>
2023-09-28 17:53:06 -0400

    audit_dynamic_groups should run clean_filters() as well

--- nautobot/core/management/commands/audit_dynamic_groups.py
+++ nautobot/core/management/commands/audit_dynamic_groups.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
 
 from nautobot.core.utils.lookup import get_filterset_for_model
@@ -14,20 +15,26 @@ class Command(BaseCommand):
         dynamic_groups = DynamicGroup.objects.all().order_by("name")
         is_valid = True
         for dynamic_group in dynamic_groups:
+            try:
+                dynamic_group.clean_filter()
+            except ValidationError as err:
+                self.stderr.write(f'    DynamicGroup "{dynamic_group}" has an invalid filter: {err}')
+                is_valid = False
+                continue
             dynamic_group_model = dynamic_group.content_type.model_class()
             dynamic_group_model_filter = get_filterset_for_model(dynamic_group_model)
             valid_filterset_fields = dynamic_group_model_filter().filters
             filter_data = dynamic_group.filter
             for filter_field in filter_data.keys():
                 if filter_field not in valid_filterset_fields:
-                    self.stdout.write(
-                        f"    DynamicGroup instance with name `{dynamic_group.name}` and content type `{dynamic_group.content_type}` has an invalid filter `{filter_field}`"
+                    self.stderr.write(
+                        f'    DynamicGroup "{dynamic_group}" has an invalid filter field "{filter_field}"'
                     )
                     is_valid = False
         if is_valid:
             self.stdout.write("\n>>> All DynamicGroup filters are validated successfully!")
         else:
-            self.stdout.write(
+            self.stderr.write(
                 "\n>>> Please fix the broken filters stated above according to the documentation available at:\n"
                 "<nautobot-home>/static/docs/installation/upgrading-from-nautobot-v1.html#ui-graphql-and-rest-api-filter-changes\n"
             )
