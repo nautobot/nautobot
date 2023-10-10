e42b6d13f65105177c9ff53192bdb666f1811218
Hanlin Miao <hanlin980505@gmail.com>
2023-10-10 19:51:57 -0400

    address feedback

--- nautobot/core/tests/test_views_utils.py
+++ nautobot/core/tests/test_views_utils.py
@@ -4,7 +4,8 @@ from nautobot.core.models.querysets import count_related
 from nautobot.core.views.utils import check_filter_for_display
 
 from nautobot.dcim.filters import DeviceFilterSet
-from nautobot.dcim.models import DeviceRedundancyGroup, InventoryItem, Manufacturer
+from nautobot.dcim.models import Device, DeviceRedundancyGroup, DeviceType, InventoryItem, Location, Manufacturer
+from nautobot.extras.models import Role, Status
 
 
 class CheckFilterForDisplayTest(TestCase):
@@ -103,5 +104,39 @@ class CheckFilterForDisplayTest(TestCase):
 
 class CheckCountRelatedSubquery(TestCase):
     def test_count_related(self):
+        """Assert that InventoryItems with the same Manufacuturers do not cause issues in ManufacturerListView."""
+        location = Location.objects.filter(parent__isnull=False).first()
+        manufacturer = Manufacturer.objects.create(name="New Manufacturer")
+        devicetype = DeviceType.objects.create(manufacturer=manufacturer, model="Device Type 1")
+        devicerole = Role.objects.get_for_model(Device).first()
+        device_status = Status.objects.get_for_model(Device).first()
+        device1 = Device.objects.create(
+            device_type=devicetype,
+            role=devicerole,
+            name="TestDevice1",
+            status=device_status,
+            location=location,
+        )
+        self.manufacturer_1 = Manufacturer.objects.create(name="Manufacturer 1")
+        self.manufacturer_2 = Manufacturer.objects.create(name="Manufacturer 2")
+        self.manufacturer_3 = Manufacturer.objects.create(name="Manufacturer 3")
+        self.parent_inventory_item_1 = InventoryItem.objects.create(
+            device=device1, manufacturer=self.manufacturer_1, name="Parent Inv 1"
+        )
+        self.child_inventory_item_1 = InventoryItem.objects.create(
+            device=device1,
+            manufacturer=self.manufacturer_1,
+            name="Parent Inv 1",
+            parent=self.parent_inventory_item_1,
+        )
+        self.inventory_item_2 = InventoryItem.objects.create(
+            device=device1, manufacturer=self.manufacturer_2, name="Inv 2"
+        )
+        self.inventory_item_3 = InventoryItem.objects.create(
+            device=device1, manufacturer=self.manufacturer_3, name="Inv 3"
+        )
+        self.inventory_item_4 = InventoryItem.objects.create(
+            device=device1, manufacturer=self.manufacturer_3, name="Inv 4"
+        )
         queryset = Manufacturer.objects.annotate(inventory_item_count=count_related(InventoryItem, "manufacturer"))
         self.assertIsNotNone(queryset.count)
