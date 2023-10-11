78fab233bd196e7f6e47fc7a0ea0491fb38bd210
Hanlin Miao <hanlin980505@gmail.com>
2023-10-11 12:10:17 -0400

    audit_graphql_queries command

--- nautobot/core/management/commands/audit_graphql_queries.py
+++ nautobot/core/management/commands/audit_graphql_queries.py
@@ -10,23 +10,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         from nautobot.extras.models import GraphQLQuery
 
-        self.stdout.write("\n>>> Auditing existing GraphQLQuery data for invalid filters ...\n")
+        self.stdout.write("\n>>> Auditing existing GraphQLQuery data for invalid queries ...\n")
 
         graph_ql_querys = GraphQLQuery.objects.all().order_by("name")
-        user = User.objects.create(username="test_user_1")
+        user = User.objects.create(username="GraphQL Test User")
         is_valid = True
         for graph_ql_query in graph_ql_querys:
             result = execute_query(graph_ql_query.query, user=user).to_dict()
             if result.get("errors"):
                 self.stderr.write(
-                    f'    GraphQLQuery {graph_ql_query.name} has an invalid filter: {result.get("errors")}'
+                    f'    GraphQLQuery {graph_ql_query.name} has an invalid query: {result.get("errors")}'
                 )
                 is_valid = False
         if is_valid:
-            self.stdout.write("\n>>> All GraphQLQuery filters are validated successfully!")
+            self.stdout.write("\n>>> All GraphQLQuery queries are validated successfully!")
         else:
             self.stderr.write(
-                "\n>>> Please fix the broken filters stated above according to the documentation available at:\n"
+                "\n>>> Please fix the outdated queries stated above according to the documentation available at:\n"
                 "<nautobot-home>/static/docs/installation/upgrading-from-nautobot-v1.html#ui-graphql-and-rest-api-filter-changes\n"
             )
-        User.objects.get(username="test_user_1").delete()
+        User.objects.get(username="GraphQL Test User").delete()
