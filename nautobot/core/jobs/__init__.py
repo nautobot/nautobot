d60351b5bfd2c855cc89122e0618ff80ac696a05
Glenn Matthews <glenn.matthews@networktocode.com>
2023-11-01 11:25:39 -0400

    Create ExportObjectList system Job

--- nautobot/core/jobs/__init__.py
+++ nautobot/core/jobs/__init__.py
@@ -1,7 +1,15 @@
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.http import QueryDict
+
+from nautobot.core.api.renderers import NautobotCSVRenderer
+from nautobot.core.api.utils import get_serializer_for_model
 from nautobot.core.celery import app, register_jobs
+from nautobot.core.utils.lookup import get_filterset_for_model
+from nautobot.core.utils.requests import get_filterable_params_from_filter_params
 from nautobot.extras.datasources import ensure_git_repository, git_repository_dry_run, refresh_datasource_content
-from nautobot.extras.jobs import Job, ObjectVar
-from nautobot.extras.models import GitRepository
+from nautobot.extras.jobs import Job, ObjectVar, StringVar
+from nautobot.extras.models import ExportTemplate, GitRepository
 
 name = "System Jobs"
 
@@ -59,5 +67,108 @@ class GitRepositoryDryRun(Job):
             self.logger.info(f"Repository dry run completed in {job_result.duration}")
 
 
-jobs = [GitRepositorySync, GitRepositoryDryRun]
+class ExportObjectList(Job):
+    """System Job to export a list of objects via CSV or ExportTemplate."""
+
+    content_type = ObjectVar(
+        model=ContentType,
+        description="Type of objects to export",
+        label="Content Type",
+    )
+    query_string = StringVar(
+        description='Filterset parameters to apply, in URL query parameter format e.g. "name=test&status=Active"',
+        label="Filterset Parameters",
+        default="",
+        required=False,
+    )
+    export_template = ObjectVar(
+        model=ExportTemplate,
+        query_params={"content_type": "$content_type"},
+        display_field="name",
+        description="Export Template to use (if unspecified, will export to generic CSV instead)",
+        label="Export Template",
+        default=None,
+        required=False,
+    )
+
+    class Meta:
+        name = "Export Object List"
+        has_sensitive_variables = False
+
+    def run(self, *, content_type, query_string="", export_template=None):
+        if not self.user.has_perm(f"{content_type.app_label}.view_{content_type.model}"):
+            self.logger.error('User "%s" does not have permission to view %s objects', self.user, content_type.model)
+            raise Exception("User does not have view permissions on the requested content-type")
+
+        model = content_type.model_class()
+
+        # Start with all objects of the requested type
+        queryset = model.objects.all()
+        # Enforce user permissions
+        queryset = queryset.restrict(self.user, "view")
+
+        # Filter the queryset based on the provided query_string
+        filterset_class = get_filterset_for_model(model)
+        self.logger.debug("Found filterset class: `%s`", filterset_class.__name__)
+        # TODO: ideally the ObjectListView should strip its non_filter_params (which may vary by view!)
+        #       such that they never are even seen here.
+        query_params = QueryDict(query_string)
+        self.logger.debug("Parsed query_params: `%s`", query_params)
+        default_non_filter_params = ("export", "page", "per_page", "sort")
+        filter_params = get_filterable_params_from_filter_params(
+            query_params, default_non_filter_params, filterset_class()
+        )
+        self.logger.debug("Filter params: `%s`", filter_params)
+        filterset = filterset_class(filter_params, queryset)
+        if not filterset.is_valid():
+            self.logger.error("Invalid filters were specified: %s", filterset.errors)
+            raise Exception("Invalid query_string value for this content_type")
+        queryset = filterset.qs
+
+        filename = f"{settings.BRANDING_PREPENDED_FILENAME}{model._meta.verbose_name_plural.lower().replace(' ', '_')}"
+
+        # If query_string == "export", then query_params["export"] will be [""], rather than [] or "" as might expect
+        if export_template is None and query_params.get("export", [""]) != [""]:
+            try:
+                export_template = ExportTemplate.objects.get(content_type=content_type, name=query_params.get("export"))
+            except ExportTemplate.DoesNotExist as err:
+                self.logger.error(
+                    "ExportTemplate %s not found for content-type %s", query_params.get("export"), content_type
+                )
+                raise Exception("Requested export-template not found") from err
+
+        if export_template is not None:
+            # Export templates
+            if export_template.content_type != content_type:
+                self.logger.error("ExportTemplate %s doesn't apply to %s", export_template, content_type)
+                raise Exception("ExportTemplate ContentType mismatch")
+            self.logger.info("Exporting based on ExportTemplate", extra={"object": export_template})
+            try:
+                output = export_template.render(queryset)
+            except Exception as e:
+                self.logger.error("Error when rendering ExportTemplate: %s", e)
+                raise
+            if export_template.file_extension:
+                filename += f".{export_template.file_extension}"
+            self.create_file(filename, output)
+
+        elif "export" in query_params and hasattr(model, "to_yaml"):
+            # Device-type YAML export
+            self.logger.info("Exporting to YAML")
+            yaml_data = [obj.to_yaml() for obj in queryset]
+            self.create_file(filename + ".yaml", "---\n".join(yaml_data))
+
+        else:
+            # Generic CSV export
+            self.logger.info("Exporting to CSV")
+            serializer_class = get_serializer_for_model(model)
+            self.logger.debug("Found serializer class: `%s`", serializer_class.__name__)
+            # The force_csv=True attribute is a hack, but much easier than trying to construct a valid HttpRequest
+            # object from scratch that passes all implicit and explicit assumptions in Django and DRF.
+            serializer = serializer_class(queryset, many=True, context={"request": None}, force_csv=True)
+            output = NautobotCSVRenderer().render(serializer.data)
+            self.create_file(filename + ".csv", output)
+
+
+jobs = [ExportObjectList, GitRepositorySync, GitRepositoryDryRun]
 register_jobs(*jobs)
