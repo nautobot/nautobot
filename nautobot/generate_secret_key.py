328b17fe47d50a5ffe2d764b848d443471a6d3da
Karsten Rohrbach <karsten.rohrbach@deutschebahn.com>
2023-11-16 10:49:53 +0100

    Use secrets modules for key generation

--- nautobot/generate_secret_key.py
+++ nautobot/generate_secret_key.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # This script will generate a random 50-character string suitable for use as a SECRET_KEY.
-from random import SystemRandom
+# shamelessly pulled from https://docs.python.org/3/library/secrets.html#recipes-and-best-practices
+import string
+import secrets
 
-choice = SystemRandom().choice
-
-charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(-_=+)"
-print("".join(choice(charset) for _ in range(50)))
+length = 50
+alphabet = string.ascii_letters + string.digits + "!@#$%^&*(-_=+)"  # string.punctuation would be an option
+while True:
+    password = ''.join(secrets.choice(alphabet) for i in range(length))
+    if (any(c.islower() for c in password)
+            and any(c.isupper() for c in password)
+            and sum(c.isdigit() for c in password) >= 3):
+        break
+print(password)
