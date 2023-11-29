f674cf5bee5e0430c972a2030d4144ffd0c65191
Glenn Matthews <glenn.matthews@networktocode.com>
2023-11-29 16:49:27 -0500

    Restore test code

--- nautobot/core/tests/test_graphql.py
+++ nautobot/core/tests/test_graphql.py
@@ -654,9 +654,7 @@ class GraphQLAPIPermissionTest(TestCase):
         self.assertIsInstance(response.data["data"]["locations"], list)
         location_names = [item["name"] for item in response.data["data"]["locations"]]
         location_list = list(Location.objects.values_list("name", flat=True))
-        # The GraphQL query response is in tree traversal order,
-        # while with django-tree-queries 0.16.0, `values_list()` returns in sorted order.
-        self.assertEqual(sorted(location_names), sorted(location_list))
+        self.assertEqual(location_names, location_list)
 
 
 class GraphQLQueryTest(TestCase):
