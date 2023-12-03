a0c28530350fecb400dafb9e31418570c8242eda
Matt Vitale <matt.vitale@networktocode.com>
2023-12-03 09:11:56 -0500

    Change to use cookies. Fixes JS toggling

--- nautobot/project-static/js/homepage_layout.js
+++ nautobot/project-static/js/homepage_layout.js
@@ -9,25 +9,17 @@ $(document).ready(function() {
         collapsibleDiv.toggleClass('collapsed');
 
         // Update the state in localStorage
-        var isCollapsed = collapsibleDiv.hasClass('collapsed');
+        var isCollapsed = collapsibleDiv.hasClass('in');
         localStorage.setItem(elementId, isCollapsed ? 'collapsed' : 'expanded');
+        // Set Cookie value based on isCollapsed
+        if (isCollapsed) {
+            document.cookie = elementId + "=True; path=/";
+        }
+        else {
+            console.log("isNotCollapsed");
+            document.cookie = elementId + "=False; path=/";
+        }
     }
-
-    // Function to set initial state for all collapsible elements
-    function setInitialStates() {
-        $('.collapsible-div').each(function() {
-            var elementId = this.id;
-            var isCollapsed = localStorage.getItem(elementId) === 'collapsed';
-
-            // Set initial state based on localStorage
-            if (isCollapsed) {
-                $(this).removeClass('in');
-            }
-        });
-    }
-
-    // Set initial states on page load
-    setInitialStates();
 
     // Add event listener to each collapsible div
     $('.collapse-icon').on('click', function() {
