8a1606de99a885c1dfbd2ae1a6c005ac0a18c972
Matt Vitale <matt.vitale@networktocode.com>
2023-12-03 09:40:55 -0500

    Draggable homepage panels

--- nautobot/project-static/js/homepage_layout.js
+++ nautobot/project-static/js/homepage_layout.js
@@ -27,3 +27,38 @@ $(document).ready(function() {
         toggleAndSaveState(elementId);
     });
 });
+
+$(document).ready(function() {
+    // Initialize draggable and sortable
+    $("#draggable-homepage-panels").sortable({
+        update: function(event, ui) {
+            savePanelOrder();
+        }
+    });
+
+    // Load saved panel order on page load
+    loadSavedPanelOrder();
+
+    // Function to save the order of panels in localStorage
+    function savePanelOrder() {
+        // WORKS
+        var panelOrder = $("#draggable-homepage-panels").sortable("toArray");
+        localStorage.setItem("homepage-panels-order", JSON.stringify(panelOrder));
+    }
+
+    // Function to load the saved order of panels from localStorage
+    function loadSavedPanelOrder() {
+        var savedOrder = localStorage.getItem("homepage-panels-order");
+        console.log(savedOrder);
+
+        if (savedOrder) {
+            savedOrder = JSON.parse(savedOrder);
+            console.log(savedOrder);
+            // $("#draggable-homepage-panels").html("");
+            
+            for (var i = 0; i < savedOrder.length; i++) {
+                $("#" + savedOrder[i]).appendTo("#draggable-homepage-panels");
+            }
+        }
+    }
+});
