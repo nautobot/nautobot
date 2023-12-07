ebb3b3f70c0b71b0d4c61468a0cc96e280cc949d
Hanlin Miao <hanlin980505@gmail.com>
2023-12-07 14:15:51 -0500

    fix schema checks

--- nautobot/core/settings.py
+++ nautobot/core/settings.py
@@ -258,6 +258,8 @@ SPECTACULAR_SETTINGS = {
         "CableTypeChoices": "nautobot.dcim.choices.CableTypeChoices",
         "ConsolePortTypeChoices": "nautobot.dcim.choices.ConsolePortTypeChoices",
         "CustomFieldTypeChoices": "nautobot.extras.choices.CustomFieldTypeChoices",
+        "HTTPMethodClassChoices": "nautobot.extras.choices.HTTPMethodClassChoices",
+        "WebhookHttpMethodChoices": "nautobot.extras.choices.WebhookHttpMethodChoices",
         "InterfaceTypeChoices": "nautobot.dcim.choices.InterfaceTypeChoices",
         "IPAddressTypeChoices": "nautobot.ipam.choices.IPAddressTypeChoices",
         "PortTypeChoices": "nautobot.dcim.choices.PortTypeChoices",
