8a8b84c0d4f6761bcd6eeb6785ccb6bdbd4fdabe
Hanlin Miao <hanlin980505@gmail.com>
2023-12-08 01:25:36 -0500

    delete filter test for ca_file_path

--- nautobot/extras/tests/test_filters.py
+++ nautobot/extras/tests/test_filters.py
@@ -538,7 +538,6 @@ class ExternalIntegrationTestCase(FilterTestCases.FilterTestCase):
         ["secrets_group", "secrets_group__id"],
         ["secrets_group", "secrets_group__name"],
         ["http_method"],
-        ["ca_file_path"],
     )
 
     @classmethod
