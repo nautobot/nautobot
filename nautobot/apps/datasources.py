0f59409f5dcabb04b5242e3b6f9253742ef6fdff
Ken Celenza <ken@celenza.org>
2023-12-15 11:18:00 -0500

    Created convenience methods to access Git with and without secrets. (#4973)
    * Created convenience methods to access Git with and without secrets.
    
    * Updates per feedback
    
    * fix import and update function name
    
    * Update nautobot/extras/datasources/git.py
    
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>
    
    ---------
    
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>
    Co-authored-by: Hanlin Miao <46973263+HanlinMiao@users.noreply.github.com>

--- nautobot/apps/datasources.py
+++ nautobot/apps/datasources.py
@@ -1,6 +1,7 @@
 """Utilities for an app to register datasource contents (for Git, etc.)."""
 
+from nautobot.extras.datasources import get_repo_access_url
 from nautobot.extras.registry import DatasourceContent
 
 
-__all__ = ("DatasourceContent",)
+__all__ = ("DatasourceContent", "get_repo_access_url")
