755146dfcc7b5d30ef00f5b3927f310b7586ca8b
erjac77 <erjac77@gmail.com>
2023-12-26 19:38:14 +0000

    refactor(devcontainer): remove pylint and pre-commit leftover

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,8 +3,3 @@
 set -ex
 
 git config --global --add safe.directory /source
-
-# if [ ! -f ".git/hooks/pre-commit" ]; then
-#     cd .git/hooks/
-#     ln -s ../../scripts/git-hooks/pre-commit
-# fi
