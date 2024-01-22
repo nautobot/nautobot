290df4b6677c98e0538aa2facd3800957361d6cf
Glenn Matthews <glenn.matthews@networktocode.com>
2024-01-22 10:30:04 -0500

    Alternate safer approach to parallel graphql test

--- nautobot/core/tests/test_graphql.py
+++ nautobot/core/tests/test_graphql.py
@@ -90,12 +90,15 @@ from nautobot.virtualization.models import Cluster, VirtualMachine, VMInterface
 User = get_user_model()
 
 
-# TODO: the below *shouldn't* be needed, but without it,
-# these tests consistently fail with schema construction errors when run with --parallel flag
-SCHEMA = graphene_settings.SCHEMA  # not a no-op; this causes the schema to be built
+class GraphQLTestCaseBase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # TODO: the below *shouldn't* be needed, but without it, when run with --parallel test flag,
+        # these tests consistently fail with schema construction errors
+        cls.SCHEMA = graphene_settings.SCHEMA  # not a no-op; this causes the schema to be built
 
 
-class GraphQLTestCase(TestCase):
+class GraphQLTestCase(GraphQLTestCaseBase):
     def setUp(self):
         self.user = create_test_user("graphql_testuser")
         GraphQLQuery.objects.create(name="GQL 1", query="{ query: locations {name} }")
@@ -162,13 +165,13 @@ class GraphQLTestCase(TestCase):
     @override_settings(EXEMPT_VIEW_PERMISSIONS=["*"])
     def test_graphql_url_field(self):
         """Test the url field for all graphql types."""
-        schema = graphene_settings.SCHEMA.introspect()
+        schema = self.SCHEMA.introspect()
         graphql_fields = schema["__schema"]["types"][0]["fields"]
         for graphql_field in graphql_fields:
             if graphql_field["type"]["kind"] == "LIST" or graphql_field["name"] == "content_type":
                 continue
             with self.subTest(f"Testing graphql url field for {graphql_field['name']}"):
-                graphene_object_type_definition = graphene_settings.SCHEMA.get_type(graphql_field["type"]["name"])
+                graphene_object_type_definition = self.SCHEMA.get_type(graphql_field["type"]["name"])
 
                 # simple check for url field in type definition
                 self.assertIn(
@@ -194,14 +197,14 @@ class GraphQLTestCase(TestCase):
                     )
 
 
-class GraphQLUtilsTestCase(TestCase):
+class GraphQLUtilsTestCase(GraphQLTestCaseBase):
     def test_str_to_var_name(self):
         self.assertEqual(str_to_var_name("IP Addresses"), "ip_addresses")
         self.assertEqual(str_to_var_name("My New VAR"), "my_new_var")
         self.assertEqual(str_to_var_name("My-VAR"), "my_var")
 
 
-class GraphQLGenerateSchemaTypeTestCase(TestCase):
+class GraphQLGenerateSchemaTypeTestCase(GraphQLTestCaseBase):
     def test_model_w_filterset(self):
         schema = generate_schema_type(app_name="dcim", model=Device)
         self.assertEqual(schema.__bases__[0], OptimizedNautobotObjectType)
@@ -215,7 +218,7 @@ class GraphQLGenerateSchemaTypeTestCase(TestCase):
         self.assertIsNone(schema._meta.filterset_class)
 
 
-class GraphQLExtendSchemaType(TestCase):
+class GraphQLExtendSchemaType(GraphQLTestCaseBase):
     def setUp(self):
         self.datas = (
             {"field_name": "my_text", "field_type": CustomFieldTypeChoices.TYPE_TEXT},
@@ -306,7 +309,7 @@ class GraphQLExtendSchemaType(TestCase):
         self.assertIsInstance(getattr(schema, "resolve_mode"), types.FunctionType)
 
 
-class GraphQLExtendSchemaRelationship(TestCase):
+class GraphQLExtendSchemaRelationship(GraphQLTestCaseBase):
     def setUp(self):
         location_ct = ContentType.objects.get_for_model(Location)
         rack_ct = ContentType.objects.get_for_model(Rack)
@@ -432,7 +435,7 @@ class GraphQLExtendSchemaRelationship(TestCase):
             self.assertNotIn(field_name, schema._meta.fields.keys())
 
 
-class GraphQLSearchParameters(TestCase):
+class GraphQLSearchParameters(GraphQLTestCaseBase):
     def setUp(self):
         self.schema = generate_schema_type(app_name="dcim", model=Location)
 
@@ -449,12 +452,14 @@ class GraphQLSearchParameters(TestCase):
                 self.assertNotIn(field, params.keys())
 
 
-class GraphQLAPIPermissionTest(TestCase):
+class GraphQLAPIPermissionTest(GraphQLTestCaseBase):
     client_class = NautobotTestClient
 
     @classmethod
     def setUpTestData(cls):
         """Initialize the Database with some datas and multiple users associated with different permissions."""
+        super().setUpTestData()
+
         cls.groups = (
             Group.objects.create(name="Group 1"),
             Group.objects.create(name="Group 2"),
@@ -688,7 +693,7 @@ class GraphQLAPIPermissionTest(TestCase):
         self.assertEqual(location_names, location_list)
 
 
-class GraphQLQueryTest(TestCase):
+class GraphQLQueryTest(GraphQLTestCaseBase):
     """Execute various GraphQL queries and verify their correct responses."""
 
     @classmethod
@@ -1081,10 +1086,9 @@ class GraphQLQueryTest(TestCase):
         cls.rm2ms_assoc_3.validated_save()
 
         cls.backend = get_default_backend()
-        cls.schema = graphene_settings.SCHEMA
 
     def execute_query(self, query, variables=None):
-        document = self.backend.document_from_string(self.schema, query)
+        document = self.backend.document_from_string(self.SCHEMA, query)
         if variables:
             return document.execute(context_value=self.request, variable_values=variables)
         else:
