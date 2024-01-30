402479e798e111bcbf8f93ca3f5b42a5e0910197
Hanlin Miao <hanlin980505@gmail.com>
2024-01-30 12:15:56 -0500

    Fixed AssertionError when querying Date type custom fields in GraphQL.

--- nautobot/core/graphql/types.py
+++ nautobot/core/graphql/types.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.contenttypes.models import ContentType
 import graphene
 import graphene_django_optimizer as gql_optimizer
@@ -22,6 +24,23 @@ class ContentTypeType(OptimizedNautobotObjectType):
 
     class Meta:
         model = ContentType
+
+
+class Date(graphene.Date):
+    """
+    Overriding the default serialize method from https://github.com/graphql-python/graphene/blob/master/graphene/types/datetime.py
+    to handle the case where the date object is passed as a str object.
+    """
+
+    @staticmethod
+    def serialize(date):
+        if isinstance(date, datetime.datetime):
+            date = date.date()
+        if not isinstance(date, datetime.date) and isinstance(date, str):
+            date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+        if not isinstance(date, datetime.date):
+            raise AssertionError('Received not compatible date "{}"'.format(repr(date)))
+        return date.isoformat()
 
 
 class JSON(graphene.Scalar):
