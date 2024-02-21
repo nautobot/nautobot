fa515bb85aa0e2c1b033a4b3cf1a8061080bb7fb
Hanlin Miao <hanlin980505@gmail.com>
2024-02-21 10:53:25 -0500

    ruff

--- nautobot/extras/forms/forms.py
+++ nautobot/extras/forms/forms.py
@@ -6,7 +6,6 @@ from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
-from django.db import models
 from django.db.models.fields import TextField
 from django.forms import inlineformset_factory, ModelMultipleChoiceField
 from django.urls.base import reverse
