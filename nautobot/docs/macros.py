8aafce76699027d9ab74ce7ed7004a5e2592e541
Glenn Matthews <glenn.matthews@networktocode.com>
2024-02-29 17:57:11 -0500

    Fix RTD build?

--- nautobot/docs/macros.py
+++ nautobot/docs/macros.py
@@ -6,10 +6,8 @@ import yaml
 
 
 def define_env(env):
-    # Are we seeing ourselves as in `./docs/` or in `./nautobot/docs/`?
-    if os.path.basename(os.path.dirname(os.path.dirname(__file__))) == "nautobot":
-        settings_file = os.path.join(os.path.dirname(__file__), "..", "core", "settings.yaml")
-    else:
-        settings_file = os.path.join(os.path.dirname(__file__), "..", "nautobot", "core", "settings.yaml")
+    settings_file = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "nautobot", "core", "settings.yaml"
+    )
     with open(settings_file, "rt") as fh:
         env.variables["settings_data"] = yaml.safe_load(fh)
