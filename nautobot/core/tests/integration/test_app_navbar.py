b34dc682c08adfa55314fc2e78cbb6174aebb9c3
Hanlin Miao <hanlin980505@gmail.com>
2024-03-18 15:54:46 -0400

    ruff fix

--- nautobot/core/tests/integration/test_app_navbar.py
+++ nautobot/core/tests/integration/test_app_navbar.py
@@ -18,9 +18,11 @@ class AppNavBarTestCase(SeleniumTestCase):
         "Circuits": {
             "Circuits": {
                 "Circuits": {
+                    "permission": "circuits.view_circuit",
                     "buttons": ["Add"],
                 },
                 "Circuit Types": {
+                    "permission": "circuits.view_circuittype",
                     "buttons": ["Add"],
                 },
             },
@@ -32,6 +34,7 @@ class AppNavBarTestCase(SeleniumTestCase):
             },
             "Providers": {
                 "Providers": {
+                    "permission": "circuits.view_provider",
                     "buttons": ["Add"],
                 },
             },
@@ -76,7 +79,7 @@ class AppNavBarTestCase(SeleniumTestCase):
 
         group = tab.find_by_xpath(f"{tab_xpath}/following-sibling::ul//li[normalize-space()='Example Group 1']")
 
-        item_xpath = f"{tab_xpath}/following-sibling::ul//li[.//a[normalize-space()='Example Models']]"
+        item_xpath = f"{tab_xpath}/following-sibling::ul//li[.//a[normalize-space()='Example Model']]"
         group.find_by_xpath(item_xpath)
 
     def test_app_navbar_modify_circuits(self):
--- nautobot/extras/tests/test_customfields.py
+++ nautobot/extras/tests/test_customfields.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.db.models import ProtectedError
