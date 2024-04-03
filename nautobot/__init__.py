ff8d8b030fa207f088a8899a2f909d92cc581bef
Glenn Matthews <glenn.matthews@networktocode.com>
2024-04-03 15:07:21 -0400

    Re-add sentinel against repeated initialization

--- nautobot/__init__.py
+++ nautobot/__init__.py
@@ -1,4 +1,5 @@
 from importlib import metadata
+import logging
 import os
 
 import django
@@ -6,10 +7,20 @@ import django
 # Primary package version
 __version__ = metadata.version(__name__)
 
+# Sentinel to make sure we only initialize once.
+__initialized = False
+
+logger = logging.getLogger(__name__)
+
 
 def setup(config_path=None):
     """Similar to `django.setup()`, this configures Django with the appropriate Nautobot settings data."""
     from nautobot.core.cli import get_config_path, load_settings
+
+    global __initialized
+
+    if __initialized:
+        return
 
     if config_path is None:
         config_path = get_config_path()
@@ -19,3 +30,6 @@ def setup(config_path=None):
 
     load_settings(config_path)
     django.setup()
+
+    logger.info("Nautobot initialized!")
+    __initialized = True
