e14304398c02fccd105a4354f920b433ed23607f
Glenn Matthews <glenn.matthews@networktocode.com>
2024-04-17 15:34:53 -0400

    Fix synchronous job execution (patched_apply)

--- nautobot/extras/jobs.py
+++ nautobot/extras/jobs.py
@@ -283,77 +283,6 @@ class BaseJob:
         if status == JobResultStatusChoices.STATUS_SUCCESS:
             self.logger.info("Job completed", extra={"grouping": "post_run"})
 
-    def apply(
-        self,
-        args=None,
-        kwargs=None,
-        link=None,
-        link_error=None,
-        task_id=None,
-        retries=None,
-        throw=None,
-        logfile=None,
-        loglevel=None,
-        headers=None,
-        **options,
-    ):
-        """Fix celery's apply method to propagate options to the task result"""
-        # trace imports Task, so need to import inline.
-        from celery.app.trace import build_tracer
-
-        app = self._get_app()
-        args = args or ()
-        kwargs = kwargs or {}
-        task_id = task_id or uuid()
-        retries = retries or 0
-        if throw is None:
-            throw = app.conf.task_eager_propagates
-
-        # Make sure we get the task instance, not class.
-        task = app._tasks[self.name]
-
-        request = {
-            "id": task_id,
-            "retries": retries,
-            "is_eager": True,
-            "logfile": logfile,
-            "loglevel": loglevel or 0,
-            "hostname": gethostname(),
-            "callbacks": maybe_list(link),
-            "errbacks": maybe_list(link_error),
-            "headers": headers,
-            "ignore_result": options.get("ignore_result", False),
-            "delivery_info": {
-                "is_eager": True,
-                "exchange": options.get("exchange"),
-                "routing_key": options.get("routing_key"),
-                "priority": options.get("priority"),
-            },
-            "properties": options,  # one line fix to overloaded method
-        }
-        if "stamped_headers" in options:
-            request["stamped_headers"] = maybe_list(options["stamped_headers"])
-            request["stamps"] = {header: maybe_list(options.get(header, [])) for header in request["stamped_headers"]}
-
-        tb = None
-        tracer = build_tracer(
-            task.name,
-            task,
-            eager=True,
-            propagate=throw,
-            app=self._get_app(),
-        )
-        ret = tracer(task_id, args, kwargs, request)
-        retval = ret.retval
-        if isinstance(retval, ExceptionInfo):
-            retval, tb = retval.exception, retval.traceback
-            if isinstance(retval, ExceptionWithTraceback):
-                retval = retval.exc
-        if isinstance(retval, Retry) and retval.sig is not None:
-            return retval.sig.apply(retries=retries + 1)
-        state = states.SUCCESS if ret.info is None else ret.info.state
-        return EagerResult(task_id, retval, state, traceback=tb)
-
     @final
     @classproperty
     def file_path(cls) -> str:  # pylint: disable=no-self-argument
@@ -1236,3 +1165,74 @@ def enqueue_job_hooks(object_change):
     for job_hook in job_hooks:
         job_model = job_hook.job
         JobResult.enqueue_job(job_model, object_change.user, object_change=object_change.pk)
+
+def patched_apply(
+    self,
+    args=None,
+    kwargs=None,
+    link=None,
+    link_error=None,
+    task_id=None,
+    retries=None,
+    throw=None,
+    logfile=None,
+    loglevel=None,
+    headers=None,
+    **options,
+):
+    """Fix celery's Task.apply() method to propagate options to the task result just like apply_async does."""
+    # trace imports Task, so need to import inline.
+    from celery.app.trace import build_tracer
+
+    app = self._get_app()
+    args = args or ()
+    kwargs = kwargs or {}
+    task_id = task_id or uuid()
+    retries = retries or 0
+    if throw is None:
+        throw = app.conf.task_eager_propagates
+
+    # Make sure we get the task instance, not class.
+    task = app._tasks[self.name]
+
+    request = {
+        "id": task_id,
+        "retries": retries,
+        "is_eager": True,
+        "logfile": logfile,
+        "loglevel": loglevel or 0,
+        "hostname": gethostname(),
+        "callbacks": maybe_list(link),
+        "errbacks": maybe_list(link_error),
+        "headers": headers,
+        "ignore_result": options.get("ignore_result", False),
+        "delivery_info": {
+            "is_eager": True,
+            "exchange": options.get("exchange"),
+            "routing_key": options.get("routing_key"),
+            "priority": options.get("priority"),
+        },
+        "properties": options,  # <------- this is the one line fix to the overloaded method
+    }
+    if "stamped_headers" in options:
+        request["stamped_headers"] = maybe_list(options["stamped_headers"])
+        request["stamps"] = {header: maybe_list(options.get(header, [])) for header in request["stamped_headers"]}
+
+    tb = None
+    tracer = build_tracer(
+        task.name,
+        task,
+        eager=True,
+        propagate=throw,
+        app=self._get_app(),
+    )
+    ret = tracer(task_id, args, kwargs, request)
+    retval = ret.retval
+    if isinstance(retval, ExceptionInfo):
+        retval, tb = retval.exception, retval.traceback
+        if isinstance(retval, ExceptionWithTraceback):
+            retval = retval.exc
+    if isinstance(retval, Retry) and retval.sig is not None:
+        return retval.sig.apply(retries=retries + 1)
+    state = states.SUCCESS if ret.info is None else ret.info.state
+    return EagerResult(task_id, retval, state, traceback=tb)
