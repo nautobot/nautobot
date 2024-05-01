408db2e195137f1d318ef7eb2766b163e1854db0
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2024-05-01 17:23:32 -0500

    Revert "remove filter and rest api changes - to be added in another branch/pr"
    This reverts commit 80e361b5440e49da48e5a5b61851b1a46458f0b4.

--- nautobot/dcim/api/views.py
+++ nautobot/dcim/api/views.py
@@ -47,6 +47,9 @@ from nautobot.dcim.models import (
     Location,
     LocationType,
     Manufacturer,
+    Module,
+    ModuleBay,
+    ModuleType,
     Platform,
     PowerFeed,
     PowerOutlet,
@@ -837,3 +840,28 @@ class ControllerManagedDeviceGroupViewSet(NautobotModelViewSet):
 
     serializer_class = serializers.ControllerManagedDeviceGroupSerializer
     filterset_class = filters.ControllerManagedDeviceGroupFilterSet
+
+
+#
+# Modules
+#
+
+
+class ModuleViewSet(NautobotModelViewSet):
+    queryset = Module.objects.select_related(
+        "parent_module_bay", "module_type", "tenant", "role", "location"
+    ).prefetch_related("module_bays", "tags")
+    serializer_class = serializers.ModuleSerializer
+    filterset_class = filters.ModuleFilterSet
+
+
+class ModuleBayViewSet(NautobotModelViewSet):
+    queryset = ModuleBay.objects.select_related("device", "parent_module").prefetch_related("installed_module", "tags")
+    serializer_class = serializers.ModuleBaySerializer
+    # filterset_class = filters.ModuleBayFilterSet
+
+
+class ModuleTypeViewSet(NautobotModelViewSet):
+    queryset = ModuleType.objects.select_related("device_family", "manufacturer").prefetch_related("tags", "modules")
+    serializer_class = serializers.ModuleTypeSerializer
+    # filterset_class = filters.ModuleTypeFilterSet
