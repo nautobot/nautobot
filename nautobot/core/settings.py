474663033511879470c13c833ac8637dc41fe791
Glenn Matthews <glenn.matthews@networktocode.com>
2024-05-20 11:34:46 -0400

    More dependency updates

--- nautobot/core/settings.py
+++ nautobot/core/settings.py
@@ -533,7 +533,6 @@ INSTALLED_APPS = [
     # "health_check.contrib.migrations",
     # "health_check.contrib.redis",
     "django_extensions",
-    "constance.backends.database",
     "django_ajax_tables",
     "silk",
 ]
