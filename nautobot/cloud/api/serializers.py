1e97f9b288d4d8c668fef73ea8c12586f088b221
Timizuo <ebideritimizuo@gmail.com>
2024-05-29 07:15:12 +0100

    fix failing unittest

--- nautobot/cloud/api/serializers.py
+++ nautobot/cloud/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
 
 from nautobot.cloud.models import CloudAccount, CloudType
 from nautobot.core.api import NautobotModelSerializer
@@ -17,7 +18,7 @@ class CloudAccountSerializer(NautobotModelSerializer):
 
 class CloudTypeSerializer(NautobotModelSerializer):
     content_types = ContentTypeField(
-        queryset=ContentType.objects.all(),
+        queryset=ContentType.objects.filter(Q(app_label="cloud", model="cloudaccount")),
         many=True,
     )
 
