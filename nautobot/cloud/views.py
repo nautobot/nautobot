2928fec1e0c06ce9882a2adbffae2f86856c018c
Hanlin Miao <hanlin980505@gmail.com>
2024-05-29 14:00:14 -0400

    fix unittests

--- nautobot/cloud/views.py
+++ nautobot/cloud/views.py
@@ -1,7 +1,7 @@
-from nautobot.cloud.api.serializers import CloudAccountSerializer
-from nautobot.cloud.filters import CloudAccountFilterSet
-from nautobot.cloud.models import CloudAccount
-from nautobot.cloud.tables import CloudAccountTable
+from nautobot.cloud.api.serializers import CloudAccountSerializer, CloudTypeSerializer
+from nautobot.cloud.filters import CloudAccountFilterSet, CloudTypeFilterSet
+from nautobot.cloud.models import CloudAccount, CloudType
+from nautobot.cloud.tables import CloudAccountTable, CloudTypeTable
 from nautobot.core.views.viewsets import NautobotUIViewSet
 
 
@@ -10,3 +10,10 @@ class CloudAccountUIViewSet(NautobotUIViewSet):
     filterset_class = CloudAccountFilterSet
     serializer_class = CloudAccountSerializer
     table_class = CloudAccountTable
+
+
+class CloudTypeUIViewSet(NautobotUIViewSet):
+    queryset = CloudType.objects.all()
+    filterset_class = CloudTypeFilterSet
+    serializer_class = CloudTypeSerializer
+    table_class = CloudTypeTable
