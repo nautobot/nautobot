367b6f708307ac2ba79db666514b7fcf82524c7d
Glenn Matthews <glenn.matthews@networktocode.com>
2024-06-27 16:57:33 -0400

    Fix tooltip too

--- nautobot/project-static/js/connection_toggles.js
+++ nautobot/project-static/js/connection_toggles.js
@@ -1,5 +1,6 @@
 function toggleConnection(elem) {
     var url = nautobot_api_path + "dcim/cables/" + elem.attr('data') + "/";
+    elem.tooltip("destroy");
     if (elem.hasClass('connected')) {
         $.ajax({
             url: url,
@@ -16,6 +17,7 @@ function toggleConnection(elem) {
                 elem.removeClass('connected btn-warning').addClass('btn-success');
                 elem.attr('title', 'Mark installed');
                 elem.children('i').removeClass('mdi mdi-lan-disconnect').addClass('mdi mdi-lan-connect')
+                elem.tooltip("show");
             }
         });
     } else {
@@ -34,6 +36,7 @@ function toggleConnection(elem) {
                 elem.removeClass('btn-success').addClass('connected btn-warning');
                 elem.attr('title', 'Mark planned');
                 elem.children('i').removeClass('mdi mdi-lan-connect').addClass('mdi mdi-lan-disconnect')
+                elem.tooltip("show");
             }
         });
     }
