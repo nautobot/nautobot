b2cb0d002ca070ebf7826974f04f57bab1e3d6c7
Stephen Kiely <stephenkiely87@gmail.com>
2024-07-08 11:08:33 -0500

    Add "verbose_name" to the Add button.

--- nautobot/core/templatetags/buttons.py
+++ nautobot/core/templatetags/buttons.py
@@ -90,22 +90,24 @@ def delete_button(instance, use_pk=False, key="slug"):
 
 
 @register.inclusion_tag("buttons/add.html")
-def add_button(url, list_element=False):
+def add_button(url, verbose_name=None, list_element=False):
     """Display an Add Button/List Element on the page.
 
     This allows an Add Button to either be displayed on a page or within a Button Group.
     Args:
         url (str): URL for the object's create page.
+        verbose_name (str, optional): Append the verbose_name to the button text.
         list_element (bool, optional): Render as a <li> element instead of a button. Defaults to False.
     """
     try:
         url = reverse(url)
     except NoReverseMatch:
-        return {"add_url": None, "list_element": list_element}
+        return {"add_url": None, "list_element": list_element, "verbose_name": verbose_name}
 
     return {
         "add_url": url,
         "list_element": list_element,
+        "verbose_name": verbose_name,
     }
 
 
@@ -162,6 +164,13 @@ def job_export_url():
 
 @register.inclusion_tag("buttons/export.html", takes_context=True)
 def export_button(context, content_type=None, list_element=False):
+    """Display an Export Button/List Element on the page.
+
+    Args:
+        context (dict): current Django Template context
+        content_type (content_type, optional): Django Content Type for the model. Defaults to None.
+        list_element (bool, optional): Render as a <li> element instead of a button. Defaults to False.
+    """
     if content_type is not None:
         user = context["request"].user
         export_templates = models.ExportTemplate.objects.restrict(user, "view").filter(content_type=content_type)
