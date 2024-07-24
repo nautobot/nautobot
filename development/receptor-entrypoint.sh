e67a663c9a5498e20fe5e14293cf3b67998655b5
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2024-07-24 17:34:43 -0500

    prototype remote worker using ansible receptor to tunnel database and redis traffic

---
+++ development/receptor-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -o /usr/bin/receptor.tgz -L https://github.com/ansible/receptor/releases/download/v1.4.8/receptor_1.4.8_linux_arm64.tar.gz
+tar -xzf /usr/bin/receptor.tgz -C /usr/bin
+chmod +x /usr/bin/receptor
+/usr/bin/receptor --config /etc/receptor/receptor.conf > /etc/receptor/stdout 2> /etc/receptor/stderr &
+# TODO: we need to wait until the mesh is up and running, but just sleeping for 10 seconds now to get it working
+sleep 10
+exec $@
