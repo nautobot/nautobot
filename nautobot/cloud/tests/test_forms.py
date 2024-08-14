10f88f8e209056502fa0c98ff2d3bea8f46ee14f
Timizuo <ebideritimizuo@gmail.com>
2024-08-14 07:44:17 +0200

    PR feedback resolve

--- nautobot/cloud/tests/test_forms.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from nautobot.cloud.forms import CloudAccountForm
-
-
-class CloudAccountTest(TestCase):
-    def test_secrets_group_is_not_required(self):
-        """Assert Secrets Group form field is not required: Fix for https://github.com/nautobot/nautobot/issues/6096"""
-        self.assertFalse(CloudAccountForm().fields["secrets_group"].required)
