c53920dae17aa827b70d7424a9b3087d5ea9f8a2
Stephen Kiely <stephenkiely87@gmail.com>
2024-09-10 17:26:00 -0500

    Update nautobot/wireless/models.py
    Co-authored-by: Gary Snider <75227981+gsnider2195@users.noreply.github.com>

--- nautobot/wireless/models.py
+++ nautobot/wireless/models.py
@@ -123,7 +123,7 @@ class WirelessNetwork(PrimaryModel):
     """
 
     name = models.CharField(max_length=CHARFIELD_MAX_LENGTH, unique=True)
-    description = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=CHARFIELD_MAX_LENGTH, blank=True)
     ssid = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
     mode = models.CharField(
         max_length=CHARFIELD_MAX_LENGTH,
