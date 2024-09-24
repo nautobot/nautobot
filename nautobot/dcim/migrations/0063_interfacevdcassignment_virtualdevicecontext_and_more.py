70e166bda1d721b9f8db5ef2fb265de30783800d
HanlinMiao <hanlin980505@gmail.com>
2024-09-24 16:54:51 -0400

    migration file

--- nautobot/dcim/migrations/0063_interfacevdcassignment_virtualdevicecontext_and_more.py
+++ nautobot/dcim/migrations/0063_interfacevdcassignment_virtualdevicecontext_and_more.py
@@ -118,7 +118,7 @@ class Migration(migrations.Migration):
                 (
                     "role",
                     nautobot.extras.models.roles.RoleField(
-                        blank=True, null=True, on_delete=django.db.models.deletion.PROTECT, to="extras.role"
+                        blank=True, null=True, related_name="virtual_device_contexts", on_delete=django.db.models.deletion.PROTECT, to="extras.role"
                     ),
                 ),
                 (
