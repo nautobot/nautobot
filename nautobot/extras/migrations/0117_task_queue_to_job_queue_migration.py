89e556ba313aaa7f938bbc53e5974f895a29c721
HanlinMiao <hanlin980505@gmail.com>
2024-09-25 15:55:35 -0400

    missing job.save()

--- nautobot/extras/migrations/0117_task_queue_to_job_queue_migration.py
+++ nautobot/extras/migrations/0117_task_queue_to_job_queue_migration.py
@@ -16,9 +16,11 @@ def migrate_task_queues_to_job_queues(apps, schema):
     JobQueue.objects.get_or_create(name="default", defaults={"queue_type": JobQueueTypeChoices.TYPE_CELERY})
     for job in Job.objects.all():
         task_queues = job.task_queues or [settings.CELERY_TASK_DEFAULT_QUEUE]
-        job.default_job_queue = JobQueue.objects.get_or_create(
+        default_job_queue, _ = JobQueue.objects.get_or_create(
             name=task_queues[0], defaults={"queue_type": JobQueueTypeChoices.TYPE_CELERY}
         )
+        job.default_job_queue = default_job_queue
+        job.save()
         # remove duplicates?
         task_queues = list(set(task_queues))
 
