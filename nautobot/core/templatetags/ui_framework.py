9b3097d35f1879817b9eafbe7defb873bb47c89b
Glenn Matthews <glenn.matthews@networktocode.com>
2024-09-25 09:43:48 -0400

    Self-review feedback

--- nautobot/core/templatetags/ui_framework.py
+++ nautobot/core/templatetags/ui_framework.py
@@ -5,15 +5,16 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def object_detail_tabs(context):
-    return context["object_detail_content"].render_tabs(context)
-
-
-@register.simple_tag(takes_context=True)
-def object_detail_tabs_content(context):
-    return context["object_detail_content"].render_content(context)
+def render_tabs_labels(context, tabs):
+    """Render the tab labels for each Tab in the given `tabs` with the given `context`."""
+    if tabs is not None:
+        return format_html_join("\n", "{}", ([tab.render_label_wrapper(context)] for tab in tabs))
+    return ""
 
 
 @register.simple_tag(takes_context=True)
 def render_components(context, components):
-    return format_html_join("\n", "{}", ([component.render(context)] for component in components))
+    """Render each component in the given `components` with the given `context`."""
+    if components is not None:
+        return format_html_join("\n", "{}", ([component.render(context)] for component in components))
+    return ""
