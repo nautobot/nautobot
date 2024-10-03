581044452eefb5c3bff97fe101af4d7fd1fa11b0
Stephen Kiely <stephenkiely87@gmail.com>
2024-10-02 21:23:42 -0500

    Finish Wireless Views
    This adds the formsets to the create/edit views as well as adding the tables to the table view.

--- nautobot/wireless/tables.py
+++ nautobot/wireless/tables.py
@@ -7,7 +7,7 @@ from nautobot.core.tables import (
     TagColumn,
     ToggleColumn,
 )
-
+from nautobot.core.templatetags.helpers import render_boolean
 from .models import AccessPointGroup, RadioProfile, SupportedDataRate, WirelessNetwork
 
 
@@ -50,7 +50,6 @@ class AccessPointGroupTable(BaseTable):
 class RadioProfileTable(BaseTable):
     pk = ToggleColumn()
     name = tables.Column(linkify=True)
-    description = tables.Column()
     tags = TagColumn(url_name="wireless:radioprofile_list")
     actions = ButtonsColumn(RadioProfile)
 
@@ -59,22 +58,37 @@ class RadioProfileTable(BaseTable):
         fields = (
             "pk",
             "name",
-            "description",
+            "frequency",
+            "channel_width",
+            "allowed_channel_list",
+            "tx_power_min",
+            "tx_power_max",
+            "rx_power_min",
+            "regulatory_domain",
             "tags",
             "actions",
         )
         default_columns = (
             "pk",
             "name",
-            "description",
+            "frequency",
+            "channel_width",
+            "allowed_channel_list",
             "actions",
         )
+
+    def render_channel_width(self, value):
+        return ", ".join(f"{v}MHz" for v in value)
+
+    def render_allowed_channel_list(self, value):
+        return ", ".join(f"{v}" for v in value)
 
 
 class SupportedDataRateTable(BaseTable):
     pk = ToggleColumn()
     rate = tables.Column(linkify=True)
     standard = tables.Column()
+    mcs_index = tables.Column()
     tags = TagColumn(url_name="wireless:supporteddatarate_list")
     actions = ButtonsColumn(SupportedDataRate)
 
@@ -84,6 +98,7 @@ class SupportedDataRateTable(BaseTable):
             "pk",
             "rate",
             "standard",
+            "mcs_index",
             "tags",
             "actions",
         )
@@ -91,6 +106,7 @@ class SupportedDataRateTable(BaseTable):
             "pk",
             "rate",
             "standard",
+            "mcs_index",
             "actions",
         )
 
@@ -107,6 +123,12 @@ class WirelessNetworkTable(BaseTable):
         fields = (
             "pk",
             "name",
+            "ssid",
+            "mode",
+            "authentication",
+            "enabled",
+            "hidden",
+            "secret",
             "description",
             "tags",
             "actions",
@@ -114,6 +136,106 @@ class WirelessNetworkTable(BaseTable):
         default_columns = (
             "pk",
             "name",
+            "ssid",
+            "mode",
+            "authentication",
+            "enabled",
+            "hidden",
             "description",
             "actions",
         )
+
+    def render_enabled(self, value):
+        return render_boolean(value)
+
+    def render_hidden(self, value):
+        return render_boolean(value)
+
+
+class AccessPointGroupWirelessNetworkAssignmentTable(BaseTable):
+    access_point_group = tables.Column(linkify=True)
+    wireless_network = tables.Column(linkify=True)
+    vlan = tables.Column(linkify=True)
+    actions = ButtonsColumn(AccessPointGroup)
+    ssid = tables.Column(accessor="wireless_network.ssid")
+    mode = tables.Column(accessor="wireless_network.mode")
+    authentication = tables.Column(accessor="wireless_network.authentication")
+    enabled = tables.Column(accessor="wireless_network.enabled")
+    hidden = tables.Column(accessor="wireless_network.hidden")
+    secrets_group = tables.Column(accessor="wireless_network.secrets_group", linkify=True)
+    controller = tables.Column(accessor="access_point_group.controller", linkify=True)
+
+    class Meta(BaseTable.Meta):
+        model = AccessPointGroup
+        fields = (
+            "wireless_network",
+            "access_point_group",
+            "vlan",
+            "ssid",
+            "mode",
+            "authentication",
+            "enabled",
+            "hidden",
+            "secrets_group",
+            "controller",
+            "actions",
+        )
+        default_columns = (
+            "wireless_network",
+            "access_point_group",
+            "vlan",
+            "ssid",
+            "mode",
+            "authentication",
+            "enabled",
+            "hidden",
+            "secrets_group",
+            "controller",
+            "actions",
+        )
+
+
+class AccessPointGroupRadioProfileAssignmentTable(BaseTable):
+    access_point_group = tables.Column(linkify=True)
+    radio_profile = tables.Column(linkify=True)
+    actions = ButtonsColumn(AccessPointGroup)
+    frequency = tables.Column(accessor="radio_profile.frequency")
+    channel_width = tables.Column(accessor="radio_profile.channel_width")
+    allowed_channel_list = tables.Column(accessor="radio_profile.allowed_channel_list")
+    tx_power_min = tables.Column(accessor="radio_profile.tx_power_min")
+    tx_power_max = tables.Column(accessor="radio_profile.tx_power_max")
+    rx_power_min = tables.Column(accessor="radio_profile.rx_power_min")
+    regulatory_domain = tables.Column(accessor="radio_profile.regulatory_domain")
+
+    class Meta(BaseTable.Meta):
+        model = AccessPointGroup
+        fields = (
+            "radio_profile",
+            "access_point_group",
+            "frequency",
+            "channel_width",
+            "allowed_channel_list",
+            "tx_power_min",
+            "tx_power_max",
+            "rx_power_min",
+            "regulatory_domain",
+            "actions",
+        )
+        default_columns = (
+            "radio_profile",
+            "access_point_group",
+            "frequency",
+            "channel_width",
+            "allowed_channel_list",
+            "tx_power_min",
+            "tx_power_max",
+            "rx_power_min",
+            "regulatory_domain",
+            "actions",
+        )
+
+    def render_channel_width(self, value):
+        return ", ".join(f"{v}MHz" for v in value)
+
+    def render_allowed_channel_list(self, value):
+        return ", ".join(f"{v}" for v in value)
--- nautobot/wireless/views.py
+++ nautobot/wireless/views.py
@@ -1,13 +1,21 @@
-from nautobot.core.views.paginator import EnhancedPaginator, get_paginate_count
+from django.core.exceptions import ValidationError
+from django_tables2 import RequestConfig
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from nautobot.core.views.paginator import get_paginate_count
 from nautobot.core.views.viewsets import NautobotUIViewSet
+from nautobot.dcim.tables import DeviceTable
 from nautobot.wireless.api.serializers import (
     AccessPointGroupSerializer,
+    AccessPointGroupDeviceAssignmentSerializer,
     RadioProfileSerializer,
     SupportedDataRateSerializer,
     WirelessNetworkSerializer,
 )
 from nautobot.wireless.filters import (
     AccessPointGroupFilterSet,
+    AccessPointGroupDeviceAssignmentFilterSet,
     RadioProfileFilterSet,
     SupportedDataRateFilterSet,
     WirelessNetworkFilterSet,
@@ -16,6 +24,7 @@ from nautobot.wireless.forms import (
     AccessPointGroupBulkEditForm,
     AccessPointGroupFilterForm,
     AccessPointGroupForm,
+    AccessPointGroupWirelessNetworkFormSet,
     RadioProfileBulkEditForm,
     RadioProfileFilterForm,
     RadioProfileForm,
@@ -25,10 +34,12 @@ from nautobot.wireless.forms import (
     WirelessNetworkBulkEditForm,
     WirelessNetworkFilterForm,
     WirelessNetworkForm,
+    WirelessNetworkAccessPointGroupFormSet,
 )
-from nautobot.wireless.models import AccessPointGroup, RadioProfile, SupportedDataRate, WirelessNetwork
+from nautobot.wireless.models import AccessPointGroup, RadioProfile, SupportedDataRate, WirelessNetwork, AccessPointGroupDeviceAssignment
 from nautobot.wireless.tables import (
     AccessPointGroupTable,
+    AccessPointGroupWirelessNetworkAssignmentTable,
     RadioProfileTable,
     SupportedDataRateTable,
     WirelessNetworkTable,
@@ -44,6 +55,74 @@ class AccessPointGroupUIViewSet(NautobotUIViewSet):
     form_class = AccessPointGroupForm
     bulk_update_form_class = AccessPointGroupBulkEditForm
 
+    def get_extra_context(self, request, instance=None):
+        context = super().get_extra_context(request, instance)
+        if self.action == "retrieve":
+            # Devices
+            devices = instance.devices.restrict(request.user, "view")
+            context["device_count"] = devices.count()
+
+            # Wireless Networks
+            wireless_networks = instance.wireless_network_assignments.restrict(request.user, "view")
+            wireless_networks_table = AccessPointGroupWirelessNetworkAssignmentTable(wireless_networks)
+            wireless_networks_table.columns.hide("access_point_group")
+            wireless_networks_table.columns.hide("controller")
+            RequestConfig(request, paginate={"per_page": get_paginate_count(request)}).configure(wireless_networks_table)
+            context["wireless_networks_table"] = wireless_networks_table
+
+            # Radio Profiles
+            radio_profiles = instance.radio_profiles.restrict(request.user, "view")
+            radio_profiles_table = RadioProfileTable(radio_profiles)
+            RequestConfig(request, paginate={"per_page": get_paginate_count(request)}).configure(radio_profiles_table)
+            context["radio_profiles_table"] = radio_profiles_table
+
+        if self.action in ["create", "update"]:
+            context["wireless_networks"] = AccessPointGroupWirelessNetworkFormSet(
+                instance=instance,
+                data=request.POST if request.method == "POST" else None,
+            )
+
+        return context
+
+    def form_save(self, form, **kwargs):
+        obj = super().form_save(form, **kwargs)
+
+        ctx = self.get_extra_context(self.request, obj)
+        wireless_networks = ctx.get("wireless_networks")
+        if wireless_networks.is_valid():
+            wireless_networks.save()
+        else:
+            raise ValidationError(wireless_networks.errors)
+
+        return obj
+
+    def extra_post_save_action(self, obj, form):
+        if form.cleaned_data.get("add_radio_profiles", None):
+            obj.radio_profiles.add(*form.cleaned_data["add_radio_profiles"])
+        if form.cleaned_data.get("remove_radio_profiles", None):
+            obj.radio_profiles.remove(*form.cleaned_data["remove_radio_profiles"])
+        if form.cleaned_data.get("add_devices", None):
+            obj.devices.add(*form.cleaned_data["add_devices"])
+        if form.cleaned_data.get("remove_devices", None):
+            obj.devices.remove(*form.cleaned_data["remove_devices"])
+
+
+    @action(detail=True, url_path="devices")
+    def devices(self, request, *args, **kwargs):
+        instance = self.get_object()
+        devices = (
+            instance.devices.restrict(request.user, "view")
+        )
+        device_table = DeviceTable(devices)
+        RequestConfig(request, paginate={"per_page": get_paginate_count(request)}).configure(device_table)
+
+        return Response(
+            {
+                "device_table": device_table,
+                "active_tab": "devices",
+                "device_count": devices.count(),
+            }
+        )
 
 class RadioProfileUIViewSet(NautobotUIViewSet):
     queryset = RadioProfile.objects.all()
@@ -53,6 +132,27 @@ class RadioProfileUIViewSet(NautobotUIViewSet):
     table_class = RadioProfileTable
     form_class = RadioProfileForm
     bulk_update_form_class = RadioProfileBulkEditForm
+
+    def get_extra_context(self, request, instance=None):
+        context = super().get_extra_context(request, instance)
+        if self.action == "retrieve":
+            # Supported Data Rates
+            supported_data_rates = instance.supported_data_rates.restrict(request.user, "view")
+            supported_data_rates_table = SupportedDataRateTable(supported_data_rates)
+            RequestConfig(request, paginate={"per_page": get_paginate_count(request)}).configure(supported_data_rates_table)
+            context["supported_data_rates_table"] = supported_data_rates_table
+
+        return context
+
+    def extra_post_save_action(self, obj, form):
+        if form.cleaned_data.get("add_supported_data_rates", None):
+            obj.supported_data_rates.add(*form.cleaned_data["add_supported_data_rates"])
+        if form.cleaned_data.get("remove_supported_data_rates", None):
+            obj.supported_data_rates.remove(*form.cleaned_data["remove_supported_data_rates"])
+        if form.cleaned_data.get("add_access_point_groups", None):
+            obj.access_point_groups.add(*form.cleaned_data["add_access_point_groups"])
+        if form.cleaned_data.get("remove_access_point_groups", None):
+            obj.access_point_groups.remove(*form.cleaned_data["remove_access_point_groups"])
 
 
 class SupportedDataRateUIViewSet(NautobotUIViewSet):
@@ -73,3 +173,49 @@ class WirelessNetworkUIViewSet(NautobotUIViewSet):
     table_class = WirelessNetworkTable
     form_class = WirelessNetworkForm
     bulk_update_form_class = WirelessNetworkBulkEditForm
+
+    def get_extra_context(self, request, instance=None):
+        context = super().get_extra_context(request, instance)
+        if self.action == "retrieve":
+            # Access Point Groups
+            access_point_groups = instance.access_point_group_assignments.restrict(request.user, "view")
+            access_point_groups_table = AccessPointGroupWirelessNetworkAssignmentTable(access_point_groups)
+            access_point_groups_table.columns.hide("wireless_network")
+            access_point_groups_table.columns.hide("ssid")
+            access_point_groups_table.columns.hide("mode")
+            access_point_groups_table.columns.hide("enabled")
+            access_point_groups_table.columns.hide("authentication")
+            access_point_groups_table.columns.hide("hidden")
+            access_point_groups_table.columns.hide("secrets_group")
+            RequestConfig(request, paginate={"per_page": get_paginate_count(request)}).configure(access_point_groups_table)
+            context["access_point_groups_table"] = access_point_groups_table
+
+        if self.action in ["create", "update"]:
+            context["access_point_groups"] = WirelessNetworkAccessPointGroupFormSet(
+                instance=instance,
+                data=request.POST if request.method == "POST" else None,
+            )
+
+        return context
+
+    def form_save(self, form, **kwargs):
+        obj = super().form_save(form, **kwargs)
+
+        ctx = self.get_extra_context(self.request, obj)
+        access_point_groups = ctx.get("access_point_groups")
+        if access_point_groups.is_valid():
+            access_point_groups.save()
+        else:
+            raise ValidationError(access_point_groups.errors)
+
+        return obj
+
+
+class AccessPointGroupDeviceAssignmentViewSet(NautobotUIViewSet):
+    queryset = AccessPointGroupDeviceAssignment.objects.all()
+    serializer_class = AccessPointGroupDeviceAssignmentSerializer
+    filterset_class = AccessPointGroupDeviceAssignmentFilterSet
+    filterset_form_class = None
+    table_class = None
+    form_class = None
+    bulk_update_form_class = None
