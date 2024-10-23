044490de8c4e88a09be11cdd0c31ccc7c19140bf
Hanlin Miao <46973263+HanlinMiao@users.noreply.github.com>
2024-10-23 12:57:18 -0400

    Added missing column software_version to the Device Table (#6416)
    * Added missing column software_version to the Device Table
    
    * added verbose_name to software_version column
    
    * revert docstring changes

--- nautobot/dcim/tables/devices.py
+++ nautobot/dcim/tables/devices.py
@@ -173,6 +173,7 @@ class DeviceTable(StatusTableMixin, RoleTableMixin, BaseTable):
         template_code="""{% if record.device_redundancy_group %}<span class="badge badge-default">{{ record.device_redundancy_group_priority|default:'None' }}</span>{% else %}—{% endif %}"""
     )
     controller_managed_device_group = tables.Column(linkify=True)
+    software_version = tables.Column(linkify=True, verbose_name="Software Version")
     secrets_group = tables.Column(linkify=True)
     tags = TagColumn(url_name="dcim:device_list")
 
@@ -201,6 +202,7 @@ class DeviceTable(StatusTableMixin, RoleTableMixin, BaseTable):
             "vc_priority",
             "device_redundancy_group",
             "device_redundancy_group_priority",
+            "software_version",
             "controller_managed_device_group",
             "secrets_group",
             "tags",
