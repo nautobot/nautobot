3b4fd64d0c139fff853c312a55a8afc8ba4ff705
Stephen Kiely <stephenkiely87@gmail.com>
2024-10-30 10:44:23 -0500

    Ruff

--- nautobot/dcim/api/serializers.py
+++ nautobot/dcim/api/serializers.py
@@ -988,7 +988,9 @@ class DeviceTypeToSoftwareImageFileSerializer(ValidatedModelSerializer):
 
 
 class ControllerSerializer(TaggedModelSerializerMixin, NautobotModelSerializer):
-    capabilities = serializers.ListField(child=ChoiceField(choices=ControllerCapabilitiesChoices, required=False), allow_empty=True)
+    capabilities = serializers.ListField(
+        child=ChoiceField(choices=ControllerCapabilitiesChoices, required=False), allow_empty=True
+    )
 
     class Meta:
         model = Controller
@@ -996,7 +998,9 @@ class ControllerSerializer(TaggedModelSerializerMixin, NautobotModelSerializer):
 
 
 class ControllerManagedDeviceGroupSerializer(TaggedModelSerializerMixin, NautobotModelSerializer):
-    capabilities = serializers.ListField(child=ChoiceField(choices=ControllerCapabilitiesChoices, required=False), allow_empty=True)
+    capabilities = serializers.ListField(
+        child=ChoiceField(choices=ControllerCapabilitiesChoices, required=False), allow_empty=True
+    )
 
     class Meta:
         model = ControllerManagedDeviceGroup
