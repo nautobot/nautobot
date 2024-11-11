db25f5809aacd6374d1e32df707d54474c90e186
Timizuo <94907097+timizuoebideri1@users.noreply.github.com>
2024-11-11 18:16:18 +0100

    Update nautobot/core/views/generic.py
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>

--- nautobot/core/views/generic.py
+++ nautobot/core/views/generic.py
@@ -1041,7 +1041,7 @@ class BulkEditView(GetReturnURLMixin, ObjectPermissionRequiredMixin, EditAndDele
                 try:
                     with deferred_change_logging_for_bulk_operation():
                         updated_objects = []
-                        queryset = queryset if edit_all else self.queryset.filter(pk__in=form.cleaned_data["pk"])
+                        queryset = queryset if edit_all else queryset.filter(pk__in=form.cleaned_data["pk"])
                         for obj in queryset:
                             obj = self.alter_obj(obj, request, [], kwargs)
 
