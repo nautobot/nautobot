4ba6982beed159321a46f1fd84bd4546d90311b5
Glenn Matthews <glenn.matthews@networktocode.com>
2024-11-14 17:19:17 -0500

    TemplateExtension enhancements for UI Component Framework (#6507)
    ---------
    
    Co-authored-by: Hanlin Miao <46973263+HanlinMiao@users.noreply.github.com>

--- nautobot/extras/plugins/__init__.py
+++ nautobot/extras/plugins/__init__.py
@@ -286,21 +286,32 @@ class PluginConfig(NautobotAppConfig):
 
 class TemplateExtension:
     """
-    This class is used to register plugin content to be injected into core Nautobot templates. It contains methods
-    that are overridden by plugin authors to return template content.
+    This class is used to register App content to be injected into core Nautobot templates.
 
-    The `model` attribute on the class defines the which model detail page this class renders content for. It
-    should be set as a string in the form `<app_label>.<model_name>`. `render()` provides the following context data:
+    It contains methods and attributes that may be overridden by App authors to return template content.
 
-    * object - The object being viewed
-    * request - The current request
-    * settings - Global Nautobot settings
-    * config - Plugin-specific configuration parameters
+    The `model` attribute on the class defines the which model detail/list pages this class renders content for.
+    It should be set as a string in the form `<app_label>.<model_name>`.
     """
 
-    model = None
+    model: str = None
+    """The model (as a string in the form `<app_label>.<model>`) that this TemplateExtension subclass applies to."""
+    object_detail_tabs = None
+    """List of Tab instances to add to the specified model's detail view."""
+    object_detail_panels = None
+    """List of Panel instances to add to the specified model's detail view."""
 
     def __init__(self, context):
+        """
+        Called automatically to instantiate a TemplateExtension with render context before calling `left_page()`, etc.
+
+        The provided context typically includes the following keys:
+
+        * object - The object being viewed
+        * request - The current request
+        * settings - Global Nautobot settings
+        * config - App-specific configuration parameters
+        """
         self.context = context
 
     def render(self, template_name, extra_context=None):
@@ -317,22 +328,37 @@ class TemplateExtension:
 
     def left_page(self):
         """
-        Content that will be rendered on the left of the detail page view. Content should be returned as an
-        HTML string. Note that content does not need to be marked as safe because this is automatically handled.
+        (Deprecated) Provide content that will be rendered on the left of the detail page view.
+
+        In Nautobot v2.4.0 and later, Apps can (should) instead register `Panel` instances in `object_detail_panels`,
+        instead of registering a `TemplateExtension` with a `.left_page()` method.
+
+        Content should be returned as an HTML string.
+        Note that content does not need to be marked as safe because this is automatically handled.
         """
         raise NotImplementedError
 
     def right_page(self):
         """
-        Content that will be rendered on the right of the detail page view. Content should be returned as an
-        HTML string. Note that content does not need to be marked as safe because this is automatically handled.
+        (Deprecated) Provide content that will be rendered on the right of the detail page view.
+
+        In Nautobot v2.4.0 and later, Apps can (should) instead register `Panel` instances in `object_detail_panels`,
+        instead of registering a `TemplateExtension` with a `.right_page()` method.
+
+        Content should be returned as an HTML string.
+        Note that content does not need to be marked as safe because this is automatically handled.
         """
         raise NotImplementedError
 
     def full_width_page(self):
         """
-        Content that will be rendered within the full width of the detail page view. Content should be returned as an
-        HTML string. Note that content does not need to be marked as safe because this is automatically handled.
+        (Deprecated) Provide content that will be rendered within the full width of the detail page view.
+
+        In Nautobot v2.4.0 and later, Apps can (should) instead register `Panel` instances in `object_detail_panels`,
+        instead of registering a `TemplateExtension` with a `.full_width_page()` method.
+
+        Content should be returned as an HTML string.
+        Note that content does not need to be marked as safe because this is automatically handled.
         """
         raise NotImplementedError
 
@@ -354,7 +380,10 @@ class TemplateExtension:
 
     def detail_tabs(self):
         """
-        Tabs that will be rendered and added to the existing list of tabs on the detail page view.
+        (Deprecated) Provide a dict of tabs and associated views that will be added to the detail page view.
+
+        In Nautobot v2.4.0 and later, Apps can (should) instead implement the `object_detail_tabs` attribute instead.
+
         Tabs will be ordered by their position in the list.
 
         Content should be returned as a list of dicts in the following format:
