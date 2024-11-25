3adbf0ed7912cd09813116ca991fdae687add752
Timizuo <ebideritimizuo@gmail.com>
2024-11-25 16:09:23 +0100

    fix unittest

--- nautobot/dcim/tests/test_views.py
+++ nautobot/dcim/tests/test_views.py
@@ -4663,9 +4663,7 @@ class SoftwareVersionTestCase(ViewTestCases.PrimaryObjectViewTestCase):
             status=device_status,
             software_version=software_version,
         )
-        device_type_to_software_image_file = DeviceTypeToSoftwareImageFile.objects.create(
-            device_type=device_type, software_image_file=software_image_file
-        )
+        DeviceTypeToSoftwareImageFile.objects.create(device_type=device_type, software_image_file=software_image_file)
 
         self.add_permissions("dcim.delete_softwareversion")
         pk_list = [software_version.pk]
@@ -4674,9 +4672,15 @@ class SoftwareVersionTestCase(ViewTestCases.PrimaryObjectViewTestCase):
             "confirm": True,
             "_confirm": True,  # Form button
         }
-        response = self.client.post(self._get_url("bulk_delete"), data, follow=True)
         # Assert protected error message included in the response body
-        self.assertBodyContains(response, f"<span>{device_type_to_software_image_file}</span>", html=True)
+        response = self.client.post(self._get_url("bulk_delete"), data)
+        job_result = JobResult.objects.filter(name="Bulk Delete Objects").first()
+        self.assertRedirects(
+            response,
+            reverse("extras:jobresult", args=[job_result.pk]),
+            status_code=302,
+            target_status_code=200,
+        )
 
 
 class ControllerTestCase(ViewTestCases.PrimaryObjectViewTestCase):
