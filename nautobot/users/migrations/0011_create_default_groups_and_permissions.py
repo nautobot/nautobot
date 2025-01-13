0002c22dc485ffc0f570492eb925374890476081
HanlinMiao <hanlin980505@gmail.com>
2025-01-13 11:56:57 -0500

    ruff

--- nautobot/users/migrations/0011_create_default_groups_and_permissions.py
+++ nautobot/users/migrations/0011_create_default_groups_and_permissions.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import Group
 from django.contrib.contenttypes.models import ContentType
 from django.db import migrations
 
