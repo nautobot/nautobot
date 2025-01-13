54d518f6ae57e1fbae5a17daeb2d2479c4606748
Federico Tabbò <federico.tabbo@networktocode.com>
2025-01-13 15:43:04 +0000

    Add custom migration to create default roles and permissions

---
+++ nautobot/users/migrations/0011_create_default_groups_and_permissions.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.db import migrations
+
+from nautobot.users.models import ObjectPermission
+
+
+def add_default_groups(apps, schema_editor):
+    read_only, _ = Group.objects.get_or_create(name="Read Only")
+    admin, _ = Group.objects.get_or_create(name="Admins")
+
+    ro_obj_permission, created = ObjectPermission.objects.get_or_create(
+        name="Default: Global Read Only",
+        actions=["view"],
+    )
+
+    if not created:
+        # Only exit if it didn't exist before
+        return
+
+    rw_obj_permission, created = ObjectPermission.objects.get_or_create(
+        name="Default: Global Read/Write",
+        actions=["read", "view", "modify", "delete"],
+    )
+
+    if not created:
+        return
+
+    ro_obj_permission.groups.add(read_only)
+    rw_obj_permission.groups.add(admin)
+
+    # Get all ContentTypes and add them to Permissions
+    for x in ContentType.objects.all():
+        ro_obj_permission.object_types.add(x)
+        rw_obj_permission.object_types.add(x)
+
+
+def remove_default_groups(apps, schema_editor):
+    read_only, _ = Group.objects.get_or_create(name="Default: Global Read Only")
+    admin, _ = Group.objects.get_or_create(name="Default: Global Read/Write")
+
+    read_only.delete()
+    admin.delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("users", "0010_user_default_saved_views"),
+    ]
+
+    operations = [migrations.RunPython(add_default_groups, remove_default_groups)]
