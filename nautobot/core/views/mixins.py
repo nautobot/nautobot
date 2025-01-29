9fc8d0ccaef675a382d02d2f9870b0e21dcdefdf
Hanlin Miao <46973263+HanlinMiao@users.noreply.github.com>
2025-01-29 13:48:21 -0500

    Fixed Bulk Edit Objects job when adding/removing a single tag to multiple objects. (#6824)
    * Implemented fix for BulkEditObjects when adding a single tag
    
    * changelog
    
    * unit test
    
    * added unit test with single value for add_tags and remove_tags form fields
    
    * fixed normalize_querydict() method
    
    * enforce type of the parameters that we passed into normalize_querydict
    
    * pass form.clean_data to send_bulk_edit_objects_to_job
    
    * address PR feedback

--- nautobot/core/views/mixins.py
+++ nautobot/core/views/mixins.py
@@ -992,10 +992,9 @@ class BulkEditAndBulkDeleteModelMixin:
         )
         return redirect("extras:jobresult", pk=job_result.pk)
 
-    def send_bulk_edit_objects_to_job(self, request, form, model):
+    def send_bulk_edit_objects_to_job(self, request, form_data, model):
         """Prepare and enqueue a bulk edit job."""
         job_model = Job.objects.get_for_class_path(BulkEditObjects.class_path)
-        form_data = normalize_querydict(request.POST, form)
         if filterset_class := lookup.get_filterset_for_model(model):
             filter_query_params = normalize_querydict(request.GET, filterset=filterset_class())
         else:
@@ -1288,7 +1287,7 @@ class ObjectBulkUpdateViewMixin(NautobotViewSetMixin, BulkUpdateModelMixin, Bulk
             form = form_class(queryset.model, request.POST, edit_all=edit_all)
             restrict_form_fields(form, request.user)
             if form.is_valid():
-                return self.send_bulk_edit_objects_to_job(self.request, form, queryset.model)
+                return self.send_bulk_edit_objects_to_job(self.request, form.cleaned_data, queryset.model)
             else:
                 return self.form_invalid(form)
         table = None
