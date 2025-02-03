30aada300a3b55c9dcf8becb5b6c2782ee044299
mateusz-lachowski-codilime <mateusz.lachowski@codilime.com>
2025-02-03 16:41:43 +0100

    6113 Cleanup code

--- nautobot/project-static/js/dropdown.js
+++ nautobot/project-static/js/dropdown.js
@@ -15,17 +15,14 @@ $(document).on('show.bs.dropdown', '.table-responsive .dropdown', function() {
         display: 'table', // required, because we're outside any container
     });
 
-    const hideMenu = function () {
+    // move dropdown back and hide
+    $dropdown.one('hidden.bs.dropdown', function () {
         $menu.appendTo($dropdown);
         $menu.removeAttr('style');
-    }
-
-    // move dropdown back and hide
-    $dropdown.one('hidden.bs.dropdown', hideMenu);
+    });
 
     // hide dropdown when scrolling vertically to avoid recalculating position
     $('.table-responsive').one('scroll', function() {
-        hideMenu();
         $toggle.trigger("click");
     });
 });
