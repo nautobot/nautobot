e45395cacdd5e01ce73d68a30bf2fd99801149e8
HanlinMiao <hanlin980505@gmail.com>
2025-02-03 12:35:55 -0500

    save current changes

--- nautobot/core/management/commands/generate_performance_test_endpoints.py
+++ nautobot/core/management/commands/generate_performance_test_endpoints.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.urls import get_resolver
 from django.utils.http import urlencode
@@ -22,6 +23,9 @@ class Command(BaseCommand):
         self.app_name_to_urls["endpoints"] = {}
         # Fetch and store the urls by app names in the dictionary
         self.fetch_urls(url_patterns)
+        for view_name, url_patterns in self.app_name_to_urls["endpoints"].items():
+            self.app_name_to_urls["endpoints"][view_name] = sorted(list(set(url_patterns)))
+
         with open("./endpoints.yml", "w") as outfile:
             yaml.dump(self.app_name_to_urls, outfile, sort_keys=True)
 
@@ -92,12 +96,81 @@ class Command(BaseCommand):
                     self.app_name_to_urls["endpoints"][view_name].append(url_pattern + f"?{query_params}")
             else:
                 # TODO handle the case where there is no instances of the model is found
-                self.stdout.write(f"No instances of {model_class} found")
+                # self.stdout.write(f"No instances of {model_class} found")
+                pass
         else:
             # A generic endpoint like `core:home`
             if view_name not in self.app_name_to_urls["endpoints"]:
                 self.app_name_to_urls["endpoints"][view_name] = []
             self.app_name_to_urls["endpoints"][view_name].append(url_pattern)
+
+    def construct_view_name_and_url_pattern(self, pattern):
+        """
+        Args:
+            pattern (django.urls.resolvers.URLPattern): A URL pattern object.
+
+        Returns:
+            url_pattern (str): The URL pattern of the view.
+            view_name (str): The URL name of the view.
+            is_api_endpoint (bool): True if the endpoint is an API endpoint, False otherwise.
+        """
+        lookup_str_list = pattern.lookup_str.split(".")
+
+        # Determine if the endpoint belongs to a plugin
+        is_plugin = lookup_str_list[0] != "nautobot"
+        is_api_endpoint = False
+        # Determine if the endpoint is an API endpoint
+        if "api" in lookup_str_list:
+            is_api_endpoint = True
+
+        if not is_plugin:
+            # One of the nautobot apps: circuits, dcim, and etc.
+            app_name = lookup_str_list[1]
+        else:
+            # One of the plugin apps: example_app, and etc.
+            app_name = lookup_str_list[0]
+
+        model = pattern.default_args.get("model", None)
+        if model:
+            app_name = model._meta.app_label
+
+        # Handle the special case first for Installed apps related view is nested under the extras app.
+        # ['nautobot', 'extras', 'plugins', 'views', 'InstalledAppsView']
+        if app_name == "extras" and "plugins" in lookup_str_list:
+            # We need special case handling to determine if the endpoint is an api endpoint as well for this view
+            view_class_name = lookup_str_list[-1]
+            if "API" in view_class_name:
+                is_api_endpoint = True
+            apps_or_plugins = "plugins" if "plugins" in pattern.name else "apps"
+
+            if is_api_endpoint:
+                url_pattern = f"/api/{apps_or_plugins}/{pattern.pattern}"  # /api/apps/installed-apps
+                view_name = f"{apps_or_plugins}-api:{pattern.name}"  # apps-api:apps-list
+            else:
+                url_pattern = f"/{apps_or_plugins}/{pattern.pattern}"  # /apps/installed-apps
+                view_name = f"{apps_or_plugins}:{pattern.name}"  # apps:apps_list
+
+            return url_pattern, view_name, is_api_endpoint
+
+        if is_api_endpoint:
+            if not is_plugin:
+                # One of the nautobot apps: circuits, dcim, and etc.
+                url_pattern = f"/api/{app_name}/{pattern.pattern}"  # /api/dcim/devices/
+                app_name = f"{app_name}-api"  # dcim-api
+                view_name = f"{app_name}:{pattern.name}"  # dcim-api:device-list
+            else:
+                url_pattern = f"/api/plugins/{app_name}/{pattern.pattern}"  # /api/plugins/example-app/models/
+                app_name = f"{app_name}-api"  # example_app-api
+                view_name = f"plugins-api:{app_name}:{pattern.name}"  # plugins-api:example_app-api:examplemodel-list
+        else:
+            if not is_plugin:
+                url_pattern = f"/{app_name}/{pattern.pattern}"  # /dcim/devices/
+                view_name = f"{app_name}:{pattern.name}"  # dcim:device_list
+            else:
+                url_pattern = f"/plugins/{app_name}/{pattern.pattern}"  # /plugins/example-app/models/
+                view_name = f"plugins:{app_name}:{pattern.name}"  # plugins:example_app:examplemodel_list
+
+        return url_pattern, view_name, is_api_endpoint
 
     def fetch_urls(self, url_patterns):
         """
@@ -126,26 +199,9 @@ class Command(BaseCommand):
                 # If it's a nested URL pattern, recursively list its URLs
                 self.fetch_urls(pattern.url_patterns)
             else:
-                # TODO need to include Nautobot App endpoints as well at some point
-                if pattern.lookup_str.startswith("nautobot."):
-                    # Only fetch urls from relevant apps
-                    app_name = pattern.lookup_str.split(".")[1]
-                    model = pattern.default_args.get("model", None)
-                    if model:
-                        app_name = model._meta.app_label
-
-                    # Determine if the endpoint is an API endpoint
-                    lookup_str_list = pattern.lookup_str.split(".")
-                    is_api_endpoint = False
-                    if len(lookup_str_list) > 2 and lookup_str_list[2] == "api":
-                        url_pattern = f"/api/{app_name}/{pattern.pattern}"  # /api/dcim/devices/
-                        app_name = f"{app_name}-api"  # dcim-api
-                        is_api_endpoint = True
-                    else:
-                        url_pattern = f"/{app_name}/{pattern.pattern}"  # /dcim/devices/
-
-                    view_name = f"{app_name}:{pattern.name}"  # dcim:device_list or dcim-api:device-list
-
+                # Only fetch urls from relevant apps
+                if pattern.lookup_str.startswith(("nautobot.", *settings.PLUGINS)):
+                    url_pattern, view_name, is_api_endpoint = self.construct_view_name_and_url_pattern(pattern)
                     # We do not need to test the ?format=<json,csv,api> endpoints and non-GET endpoints
                     if "(?P<format>[a-z0-9]+)" not in url_pattern and self.is_get_endpoint(view_name):
                         # Replace "^" and "$" from the url pattern
