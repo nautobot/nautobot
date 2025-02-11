794bd7d54d8bb6f42274c2dc8fbae437be4bbaa9
HanlinMiao <hanlin980505@gmail.com>
2025-02-11 15:53:07 -0500

    ruff

--- nautobot/extras/views.py
+++ nautobot/extras/views.py
@@ -29,7 +29,6 @@ from nautobot.core.events import publish_event
 from nautobot.core.exceptions import FilterSetFieldNotFound
 from nautobot.core.forms import restrict_form_fields
 from nautobot.core.models.querysets import count_related
-from nautobot.core.models.tree_queries import TreeQuerySet
 from nautobot.core.models.utils import pretty_print_query, serialize_object_v2
 from nautobot.core.tables import ButtonsColumn
 from nautobot.core.ui import object_detail
