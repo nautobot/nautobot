21036b5ec35fb14d2235677ae0c7045a27ff2598
Sridevi-NTC <sridevi.cheekatla@networktocode.com>
2025-02-20 03:59:56 +0000

    #5006 Prevent removal of ContentType from LocationType if in use by related models

--- nautobot/dcim/signals.py
+++ nautobot/dcim/signals.py
@@ -7,6 +7,7 @@ from django.db.models.signals import m2m_changed, post_save, pre_delete
 from django.dispatch import receiver
 
 from nautobot.core.signals import disable_for_loaddata
+from nautobot.extras.utils import FeatureQuery
 
 from .models import (
     Cable,
@@ -16,6 +17,7 @@ from .models import (
     DeviceRedundancyGroup,
     Interface,
     InterfaceVDCAssignment,
+    LocationType,
     PathEndpoint,
     PowerPanel,
     Rack,
@@ -355,3 +357,35 @@ def handle_controller_managed_device_group_controller_change(instance, raw=False
             group.controller = instance.controller
             group.save()
             logger.debug("Updated controller from parent %s for child %s", instance, group)
+
+
+@receiver(m2m_changed, sender=LocationType.content_types.through)
+def content_type_changed(instance, action, **kwargs):
+    """
+    Prevents removal of a ContentType from LocationType if it's in use by any models
+    associated with the locations.
+    """
+
+    if action != "pre_remove":
+        return
+
+    related_models = FeatureQuery("locations").list_subclasses()
+    removed_content_types = ContentType.objects.filter(pk__in=kwargs.get("pk_set", []))
+
+    for content_type in removed_content_types:
+        model_class = content_type.model_class()
+
+        if (
+            model_class in related_models
+            and model_class.objects.filter(
+                location__location_type=instance, location__location_type__content_types=content_type
+            ).exists()
+        ):
+            raise ValidationError(
+                {
+                    "content_types": (
+                        f"Cannot remove the content type {content_type} as it is currently in use by {model_class.__name__.lower()}. "
+                        "Please remove the association before proceeding."
+                    )
+                }
+            )
