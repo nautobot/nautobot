5490ae562d46e1b28053827a13c1fa9c048d2d9d
Gary McCann <gary.mccann@networktocode.com>
2025-03-11 17:35:45 +0000

    Fix branch leak - removed dcim/filters/__init__.py changes

--- nautobot/dcim/filters/__init__.py
+++ nautobot/dcim/filters/__init__.py
@@ -1791,11 +1791,6 @@ class SoftwareImageFileFilterSet(NautobotFilterSet, StatusModelFilterSetMixin):
     default_image = django_filters.BooleanFilter(
         label="Is default image for associated software version",
     )
-    external_integration = NaturalKeyOrPKMultipleChoiceFilter(
-        queryset=ExternalIntegration.objects.all(),
-        to_field_name="name",
-        label="External integration (name or ID)",
-    )
 
     class Meta:
         model = SoftwareImageFile
