d6c1e940f2f2fcfe0fea91ac3f32b7d6831433fe
Norbert Mieczkowski <norbert.mieczkowski@codilime.com>
2025-03-25 20:38:11 +0100

    feature: implement sidenav flyouts

--- nautobot/project-static/js/nav_menu.js
+++ nautobot/project-static/js/nav_menu.js
@@ -10,4 +10,30 @@ document.addEventListener('DOMContentLoaded', function() {
         const sidenav = document.getElementById(controls);
         sidenav.classList.toggle('sidenav-collapsed', expanded)
     });
+
+    [...document.querySelectorAll('.sidenav-list-item')].forEach(sidenavListItem => {
+        sidenavListItem.addEventListener('click', (event) => {
+            const controls = sidenavListItem.getAttribute('aria-controls');
+            const expanded = sidenavListItem.getAttribute('aria-expanded') === 'true';
+
+            sidenavListItem.setAttribute('aria-expanded', String(!expanded));
+
+            const onClickDocument = (documentClickEvent) => {
+                const { target: documentClickTarget } = documentClickEvent;
+                const sidenavFlyout = document.getElementById(controls);
+
+                const isClickOutside = !sidenavListItem.contains(documentClickTarget)
+                    && !sidenavFlyout.contains(documentClickTarget);
+
+                if (isClickOutside) {
+                    sidenavListItem.setAttribute('aria-expanded', 'false');
+                    document.removeEventListener('click', onClickDocument);
+                }
+            };
+
+            expanded
+                ? document.removeEventListener('click', onClickDocument)
+                : document.addEventListener('click', onClickDocument);
+        });
+    });
 });
