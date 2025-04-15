d4561950dea8b84edd68c08396be75b5738d01ca
Glenn Matthews <glenn.matthews@networktocode.com>
2025-04-15 10:54:50 -0400

    Fix missing Data Compliance tabs

--- nautobot/data_validation/apps.py
+++ nautobot/data_validation/apps.py
@@ -1,5 +1,5 @@
 from nautobot.core.apps import NautobotConfig
-from nautobot.extras.plugins import register_custom_validators
+from nautobot.extras.plugins import register_custom_validators, register_template_extensions
 
 
 class DataValidationEngineConfig(NautobotConfig):
@@ -14,3 +14,7 @@ class DataValidationEngineConfig(NautobotConfig):
         from nautobot.data_validation.custom_validators import custom_validators
 
         register_custom_validators(custom_validators)
+
+        from nautobot.data_validation.template_content import template_extensions
+
+        register_template_extensions(template_extensions)
