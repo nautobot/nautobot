aa11c8e95f1b400a07a808453e48f86550f38d5a
Austin De Coup-Crank <adecoupcrank@nvidia.com>
2025-05-29 09:44:08 -0500

    linting

--- nautobot/dcim/forms.py
+++ nautobot/dcim/forms.py
@@ -2517,9 +2517,9 @@ class DeviceAddToClustersForm(BootstrapMixin, forms.Form):
             self.fields["clusters"].help_text = help_text
 
         if self.fields["clusters"].queryset.exists():
-            available_clusters = Cluster.objects.exclude(
-                pk__in=device.clusters.values_list("pk", flat=True)
-            ).order_by("name")
+            available_clusters = Cluster.objects.exclude(pk__in=device.clusters.values_list("pk", flat=True)).order_by(
+                "name"
+            )
             if not available_clusters.exists():
                 disable_fields("This device already belongs to all available clusters.")
         else:
@@ -2527,6 +2527,7 @@ class DeviceAddToClustersForm(BootstrapMixin, forms.Form):
 
         # Only show clusters that the device isn't already a member of
         self.fields["clusters"].widget.add_query_param("devices__n", device.id)
+
 
 class DeviceRemoveFromClustersForm(ConfirmationForm):
     pk = forms.ModelMultipleChoiceField(queryset=Cluster.objects.all(), widget=forms.MultipleHiddenInput())
