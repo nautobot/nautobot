aefa7210e43ffbfa083751b5060f5b1674f0b14a
Pavan-183 <pavan.pulicherla@networktocode.com>
2025-06-09 13:31:34 +0000

    Updated as per the comments

--- nautobot/dcim/views.py
+++ nautobot/dcim/views.py
@@ -252,7 +252,7 @@ class LocationTypeUIViewSet(NautobotUIViewSet):
 
 
 class LocationUIViewSet(NautobotUIViewSet):
-    queryset = Location.objects.without_tree_fields().all()
+    queryset = Location.objects.without_tree_fields().select_related("location_type", "parent", "tenant").all()
     filterset_class = filters.LocationFilterSet
     filterset_form_class = forms.LocationFilterForm
     table_class = tables.LocationTable
