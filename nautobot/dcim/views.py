d06ceb7627d1ac785316ff57a30bbd156ef86971
Pavan-183 <pavan.pulicherla@networktocode.com>
2025-06-26 17:23:17 +0000

    Replaced perform_create/update with form_save for VirtualChassis form handling

--- nautobot/dcim/views.py
+++ nautobot/dcim/views.py
@@ -3907,7 +3907,11 @@ class VirtualChassisUIViewSet(NautobotUIViewSet):
                 extra=0,
             )
             members_queryset = instance.members.select_related("rack").order_by("vc_position")
-            formset = VCMemberFormSet(queryset=members_queryset)
+
+            if request.method == "POST":
+                formset = VCMemberFormSet(request.POST, queryset=members_queryset)
+            else:
+                formset = VCMemberFormSet(queryset=members_queryset)
 
             context.update(
                 {
@@ -3923,65 +3927,23 @@ class VirtualChassisUIViewSet(NautobotUIViewSet):
 
         return context
 
-    def perform_create(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = form_class(
-            data=request.POST,
-            files=request.FILES,
-            initial=normalize_querydict(request.GET, form_class=form_class),
-        )
-        restrict_form_fields(form, request.user)
-        if not form.is_valid():
-            return self.form_invalid(form)
+    def form_save(self, form, **kwargs):
+        obj = super().form_save(form, **kwargs)
 
-        obj = form.save(commit=False)
-        restricted_qs = self.queryset.model.objects.restrict(request.user, "add")
-        try:
-            with transaction.atomic():
-                obj.save()
-                if not restricted_qs.filter(pk=obj.pk).exists():
-                    raise PermissionDenied()
-                self.object_created = True
-                self.object = obj
-        except PermissionDenied:
-            form.add_error(None, "You do not have permission to add this object.")
-            return self.form_invalid(form)
+        if self.action == "update":
+            context = self.get_extra_context(self.request, obj)
+            formset = context.get("formset")
 
-        return self.form_valid(form)
+            if formset and formset.is_valid():
+                with transaction.atomic():
+                    members = formset.save(commit=False)
+                    Device.objects.filter(pk__in=[m.pk for m in members]).update(vc_position=None)
+                    for member in members:
+                        member.save()
+            elif formset:
+                raise ValidationError(formset.errors)
 
-    def perform_update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        form_class = self.get_form_class()
-        form = form_class(data=request.POST, files=request.FILES, instance=instance)
-        restrict_form_fields(form, request.user)
-
-        VCMemberFormSet = modelformset_factory(
-            model=Device,
-            form=forms.DeviceVCMembershipForm,
-            formset=forms.BaseVCMemberFormSet,
-            extra=0,
-        )
-        members_queryset = instance.members.select_related("rack").order_by("vc_position")
-        formset = VCMemberFormSet(request.POST, queryset=members_queryset)
-
-        if not form.is_valid() or not formset.is_valid():
-            return self.form_invalid(form)
-
-        with transaction.atomic():
-            # Save the VirtualChassis
-            form.save()
-
-            # Nullify the vc_position of each member first to allow reordering without raising an IntegrityError on
-            # duplicate positions. Then save each member instance.
-            members = formset.save(commit=False)
-            devices = Device.objects.filter(pk__in=[m.pk for m in members])
-            for device in devices:
-                device.vc_position = None
-                device.save()
-            for member in members:
-                member.save()
-
-        return self.form_valid(form)
+        return obj
 
     def get_required_permission(self):
         if getattr(self, "action", None) == "add_member":
