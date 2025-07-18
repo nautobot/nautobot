406b95f985f88203766f8f09ca0972512e0394ee
Gabriel Lacerda <gabriel.lacerda@expereo.com>
2025-07-18 10:58:32 +0200

    Ordering the choices alphabetically and improved key description

--- nautobot/extras/choices.py
+++ nautobot/extras/choices.py
@@ -484,30 +484,31 @@ class SecretsGroupAccessTypeChoices(ChoiceSet):
 
 
 class SecretsGroupSecretTypeChoices(ChoiceSet):
+    TYPE_AUTHKEY = "authentication-key"  # noqa: S105  # hardcoded-password-string -- false positive
+    TYPE_AUTHPROTOCOL = "authentication-protocol"
     TYPE_KEY = "key"
+    TYPE_NOTES = "notes"
     TYPE_PASSWORD = "password"  # noqa: S105  # hardcoded-password-string -- false positive
+    TYPE_PRIVALGORITHM = "private-algorithm"
+    TYPE_PRIVKEY = "private-key"  # noqa: S105  # hardcoded-password-string -- false positive
     TYPE_SECRET = "secret"  # noqa: S105  # hardcoded-password-string -- false positive
     TYPE_TOKEN = "token"  # noqa: S105  # hardcoded-password-string -- false positive
-    TYPE_PRIVKEY = "privkey"  # noqa: S105  # hardcoded-password-string -- false positive
-    TYPE_AUTHKEY = "authkey"  # noqa: S105  # hardcoded-password-string -- false positive
-    TYPE_USERNAME = "username"
     TYPE_URL = "url"
-    TYPE_NOTES = "notes"
-    TYPE_AUTHPROTOCOL = "authprotocol"
-    TYPE_PRIVALGORITHM = "privalgorithm"
+    TYPE_USERNAME = "username"
 
     CHOICES = (
-        (TYPE_KEY, "Key"),
-        (TYPE_PASSWORD, "Password"),
-        (TYPE_SECRET, "Secret"),
-        (TYPE_TOKEN, "Token"),
-        (TYPE_USERNAME, "Username"),
-        (TYPE_URL, "URL"),
-        (TYPE_NOTES, "Notes"),
         (TYPE_AUTHKEY, "Authentication Key"),
         (TYPE_AUTHPROTOCOL, "Authentication Protocol"),
-        (TYPE_PRIVKEY, "Private Key"),
+        (TYPE_KEY, "Key"),
+        (TYPE_NOTES, "Notes"),
+        (TYPE_PASSWORD, "Password"),
         (TYPE_PRIVALGORITHM, "Private Algorithm"),
+        (TYPE_PRIVKEY, "Private Key"),
+        (TYPE_SECRET, "Secret"),
+        (TYPE_TOKEN, "Token"),
+        (TYPE_URL, "URL"),
+        (TYPE_USERNAME, "Username"),
+
     )
 
 #
