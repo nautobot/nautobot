fb6d1502df96a1284e58882abdd0207252f74605
Norbert Mieczkowski <norbert.mieczkowski@codilime.com>
2025-10-07 16:54:29 +0200

    feature: extend echarts color palette

--- nautobot/core/ui/constants.py
+++ nautobot/core/ui/constants.py
@@ -1,15 +1,47 @@
 UI_COLORS = {
-    "dodgerblue": {
+    "blue": {
         "light": "#339dff",
         "dark": "#339dff",
+    },
+    "gray": {
+        "light": "#505d68",
+        "dark": "#b5b5b5",
+    },
+    "green": {
+        "light": "#1ca92a",
+        "dark": "#2ecc40",
     },
     "indigo": {
         "light": "#4e00b1",
         "dark": "#4e00b1",
     },
-    "orange-0": {
+    "lightblue": {
+        "light": "#cce5ff",
+        "dark": "#0e1C2b",
+    },
+    "lightgray": {
+        "light": "#dedede",
+        "dark": "#2f2f2f",
+    },
+    "lightgreen": {
+        "light": "#d2eed4",
+        "dark": "#1a3d1d",
+    },
+    "lightorange": {
+        "light": "#f9e4cd",
+        "dark": "#3d2a16",
+    },
+    "lightred": {
+        "light": "#f9d2d2",
+        "dark": "#3d1616",
+    },
+    "orange": {
         "light": "#e07807",
         "dark": "#ff9933",
+    },
+    "red": {
+        "light": "#e01f1f",
+        "dark": "#ff4c4c",
     },
     "slateblue": {
         "light": "#7b61ff",
