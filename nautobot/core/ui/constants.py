8fc55f079f0f2b29a6ed2d6b02847c2fb059811d
Norbert Mieczkowski <norbert.mieczkowski@codilime.com>
2025-10-13 10:56:18 +0200

    chore: update echarts color palette

--- nautobot/core/ui/constants.py
+++ nautobot/core/ui/constants.py
@@ -1,54 +1,86 @@
 UI_COLORS = {
     "blue": {
-        "light": "#339dff",
+        "light": "#007dff",
         "dark": "#339dff",
     },
-    "gray": {
-        "light": "#505d68",
-        "dark": "#b5b5b5",
+    "blue-darker": {
+        "light": "#0e4781",
+        "dark": "#255581",
+    },
+    "blue-lighter": {
+        "light": "#8cc5ff",
+        "dark": "#a3d3ff",
     },
     "green": {
         "light": "#1ca92a",
         "dark": "#2ecc40",
     },
+    "green-darker": {
+        "light": "#1b5a21",
+        "dark": "#236a2b",
+    },
+    "green-lighter": {
+        "light": "#99d89f",
+        "dark": "#a1e8a9",
+    },
     "indigo": {
         "light": "#4e00b1",
         "dark": "#4e00b1",
     },
-    "lightblue": {
-        "light": "#cce5ff",
-        "dark": "#0e1C2b",
+    "indigo-darker": {
+        "light": "#310e5e",
+        "dark": "#310e5e",
     },
-    "lightgray": {
-        "light": "#dedede",
-        "dark": "#2f2f2f",
-    },
-    "lightgreen": {
-        "light": "#d2eed4",
-        "dark": "#1a3d1d",
-    },
-    "lightorange": {
-        "light": "#f9e4cd",
-        "dark": "#3d2a16",
-    },
-    "lightred": {
-        "light": "#f9d2d2",
-        "dark": "#3d1616",
+    "indigo-lighter": {
+        "light": "#af8cdc",
+        "dark": "#af8cdc",
     },
     "orange": {
         "light": "#e07807",
         "dark": "#ff9933",
     },
+    "orange-darker": {
+        "light": "#734411",
+        "dark": "#815325",
+    },
+    "orange-lighter": {
+        "light": "#f1c28f",
+        "dark": "#ffd1a3",
+    },
+    "purple": {
+        "light": "#7b61ff",
+        "dark": "#a394ff",
+    },
+    "purple-darker": {
+        "light": "#463a81",
+        "dark": "#585181",
+    },
+    "purple-lighter": {
+        "light": "#c4b8ff",
+        "dark": "#d6cfff",
+    },
     "red": {
         "light": "#e01f1f",
         "dark": "#ff4c4c",
     },
-    "slateblue": {
-        "light": "#7b61ff",
-        "dark": "#a394ff",
+    "red-darker": {
+        "light": "#731c1c",
+        "dark": "#813131",
+    },
+    "red-lighter": {
+        "light": "#f19a9a",
+        "dark": "#ffaeae",
     },
     "turquoise": {
         "light": "#00bfa5",
         "dark": "#26d7c5",
+    },
+    "turquoise-darker": {
+        "light": "#0e6459",
+        "dark": "#1f6f67",
+    },
+    "turquoise-lighter": {
+        "light": "#8ce2d7",
+        "dark": "#9dede5",
     },
 }
