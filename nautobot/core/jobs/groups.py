e52632f1b08caf0cd56910fb4e4253d90aaa40dc
Glenn Matthews <glenn.matthews@networktocode.com>
2025-10-23 09:28:47 -0400

    Add JobButton and JobButtonReceiver to refresh a DG members, change UI to no longer do so automatically.

--- nautobot/core/jobs/groups.py
+++ nautobot/core/jobs/groups.py
@@ -1,5 +1,5 @@
 from nautobot.extras.choices import DynamicGroupTypeChoices
-from nautobot.extras.jobs import Job, ObjectVar
+from nautobot.extras.jobs import Job, JobButtonReceiver, ObjectVar
 from nautobot.extras.models import DynamicGroup
 
 name = "System Jobs"
@@ -31,8 +31,39 @@ class RefreshDynamicGroupCaches(Job):
         if single_group is not None:
             groups = groups.filter(pk=single_group.pk)
 
+        if not groups.exists():
+            self.logger.info("No relevant dynamic groups were specified, nothing to do.")
+            return
+
+        self.logger.info("Re-calculating and re-caching group members. This may take some time.")
         for group in groups:
             group.update_cached_members()
             self.logger.info("Cache refreshed successfully, now with %d members", group.count, extra={"object": group})
 
         self.logger.info("Cache(s) refreshed")
+
+
+class RefreshDynamicGroupCacheJobButtonReceiver(JobButtonReceiver):
+    """
+    System Job Button Receiver to re-calculate and re-cache the members of a given Dynamic Group.
+    """
+
+    class Meta:
+        name = "Refresh Dynamic Group Cache (Job Button Receiver)"
+        description = "Re-calculate and re-cache the membership list of a given Dynamic Group."
+
+    def receive_job_button(self, obj):
+        if not isinstance(obj, DynamicGroup):
+            self.fail("This job button should only be used with Dynamic Group records.")
+        elif obj.group_type == DynamicGroupTypeChoices.TYPE_STATIC:
+            self.fail(
+                "The members of this Dynamic Group are statically defined and do not need to be recalculated.",
+                extra={"object": obj},
+            )
+        else:
+            self.logger.info(
+                "Re-calculating and re-caching group members. This may take some time.", extra={"object": obj}
+            )
+            obj.update_cached_members()
+            self.logger.success("Cache refreshed successfully, now with %d members", obj.count, extra={"object": obj})
+            return obj.count
