b552e5f491d5cefbf5a5a6a177cc8034c021ad8c
Joe Wesch <joe.wesch@networktocode.com>
2025-11-07 09:30:59 -0700

    Fixed get_link edge cases when there isn't an object

--- nautobot/core/ui/object_detail.py
+++ nautobot/core/ui/object_detail.py
@@ -246,6 +246,8 @@ class Button(Component):
         """
         if self.link_name and self.link_includes_pk:
             obj = get_obj_from_context(context, self.context_object_key)
+            if not obj:
+                return None
             return reverse(self.link_name, kwargs={"pk": obj.pk})
         elif self.link_name:
             return reverse(self.link_name)
