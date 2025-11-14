f682ffa4ce3dd9fb00ab8e2dcbd2c18418d8b131
Leo Fleskes <leo.fleskes@deutschebahn.com>
2025-11-14 11:53:17 +0100

    proper ordering dependant on the after middleware

--- nautobot/core/settings_funcs.py
+++ nautobot/core/settings_funcs.py
@@ -123,9 +123,19 @@ def setup_structlog_logging(
         return
 
     django_apps.append("django_structlog")
-    # Insert the middleware in second to last place so it comes ahead of
-    # django_prometheus.middleware.PrometheusAfterMiddleware, which consumes the request
-    django_middleware.insert(-1, "django_structlog.middlewares.RequestMiddleware")
+
+    # Insert the middleware ahead of django_prometheus.middleware.PrometheusAfterMiddleware, which consumes the request.
+    # If that middleware is not present, append it at the end.
+    try:
+        index_of_prometheus_after_middleware = django_middleware.index("django_structlog.middlewares.RequestMiddleware")
+    except ValueError:
+        index_of_prometheus_after_middleware = None
+    if index_of_prometheus_after_middleware:
+        django_middleware.insert(
+            index_of_prometheus_after_middleware, "django_structlog.middlewares.RequestMiddleware"
+        )
+    else:
+        django_middleware.append("django_structlog.middlewares.RequestMiddleware")
 
     processors = (
         # Add the log level to the event dict under the level key.
