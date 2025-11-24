2421ac6f9e35bfc3f949da121b5372fa032fcda3
Glenn Matthews <glenn.matthews@networktocode.com>
2025-11-24 08:26:25 -0500

    Fix a couple of cases of URL name construction that weren't valid for Apps (#8236)

--- nautobot/core/views/generic.py
+++ nautobot/core/views/generic.py
@@ -227,6 +227,7 @@ class ObjectListView(UIComponentsMixin, ObjectPermissionRequiredMixin, View):
         hide_hierarchy_ui = False
         clear_view = request.GET.get("clear_view", False)
         resolved_path = resolve(request.path)
+        # Note that `resolved_path.app_name` does work even for nested paths like `plugins:example_app:...`
         list_url = f"{resolved_path.app_name}:{resolved_path.url_name}"
 
         skip_user_and_global_default_saved_view = False
