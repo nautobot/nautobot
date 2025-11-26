4fb058792c5f48d731817edf5fd7112637eb0fb0
Sridevi-NTC <sridevi.cheekatla@networktocode.com>
2025-11-26 14:45:58 +0000

    Fixed test cases

--- nautobot/dcim/tests/test_views.py
+++ nautobot/dcim/tests/test_views.py
@@ -783,7 +783,7 @@ class RackTestCase(ViewTestCases.PrimaryObjectViewTestCase):
         # Validate Rack Power Utilization for Combined powerfeeds is displaying correctly on the Rack View
         total_utilization_html = """
         <div title="Used: 3789&#13;Count: 7680" class="progress text-center">
-            <div class="progress-bar progress-bar-success"
+            <div class="progress-bar bg-success"
                 role="progressbar" aria-valuenow="49" aria-valuemin="0" aria-valuemax="100" style="width: 49%">
                 49%
             </div>
