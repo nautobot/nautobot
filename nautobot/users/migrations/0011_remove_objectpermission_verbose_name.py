5b2c2ea407edda812a840f6de25e923f889bd9b3
Christina Lang <xtina.lang.25@gmail.com>
2026-01-07 12:40:51 -0500

    Format migration file with ruff
    - Apply ruff formatting to 0011_remove_objectpermission_verbose_name.py
    - Change single quotes to double quotes for consistency
    - Remove unnecessary blank lines

--- nautobot/users/migrations/0011_remove_objectpermission_verbose_name.py
+++ nautobot/users/migrations/0011_remove_objectpermission_verbose_name.py
@@ -4,15 +4,13 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('users', '0010_user_default_saved_views'),
+        ("users", "0010_user_default_saved_views"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='objectpermission',
-            options={'ordering': ['name']},
+            name="objectpermission",
+            options={"ordering": ["name"]},
         ),
     ]
-
