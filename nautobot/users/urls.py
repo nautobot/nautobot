00857ec040247901ca3a869a8a623a9e8bd340b2
sirisha-bode <sirisha.bode@networktocode.com>
2026-02-17 11:43:21 +0000

    TokenUIViewSet

--- nautobot/users/urls.py
+++ nautobot/users/urls.py
@@ -8,6 +8,7 @@ from . import views
 app_name = "user"
 
 router = NautobotUIViewSetRouter()
+router.register("api-tokens", views.TokenUIViewSet, basename="token")
 
 urlpatterns = [
     path("profile/", views.ProfileView.as_view(), name="profile"),
@@ -15,14 +16,6 @@ urlpatterns = [
     path("navbar-favorites/", views.UserNavbarFavoritesAddView.as_view(), name="navbar_favorites_add"),
     path("navbar-favorites/delete/", views.UserNavbarFavoritesDeleteView.as_view(), name="navbar_favorites_delete"),
     path("password/", views.ChangePasswordView.as_view(), name="change_password"),
-    path("api-tokens/", views.TokenListView.as_view(), name="token_list"),
-    path("api-tokens/add/", views.TokenEditView.as_view(), name="token_add"),
-    path("api-tokens/<uuid:pk>/edit/", views.TokenEditView.as_view(), name="token_edit"),
-    path(
-        "api-tokens/<uuid:pk>/delete/",
-        views.TokenDeleteView.as_view(),
-        name="token_delete",
-    ),
     path("advanced-settings/", views.AdvancedProfileSettingsEditView.as_view(), name="advanced_settings_edit"),
 ]
 
