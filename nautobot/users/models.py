ea439288a9d9bccf59bfbda693eb07292b4106e1
Sridevi-NTC <sridevi.cheekatla@networktocode.com>
2026-03-02 14:25:41 +0000

    Removed redundant code

--- nautobot/users/models.py
+++ nautobot/users/models.py
@@ -45,9 +45,6 @@ class UserManager(BaseManager, UserManager_):
     def get_queryset(self):
         return UserQuerySet(self.model, using=self._db)
 
-    def restrict(self, user, action="view"):
-        return self.get_queryset().restrict(user, action)
-
 
 class User(BaseModel, AbstractUser):
     """
