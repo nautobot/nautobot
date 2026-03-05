bbea18efe409f025ddf063e78f2fa25ab0d3eb4d
Jere Julian <jere.julian@networktocode.com>
2026-03-05 13:47:46 -0500

    Add OTel instrumentation to GraphQL schema init

--- nautobot/core/graphql/schema_init.py
+++ nautobot/core/graphql/schema_init.py
@@ -1,12 +1,17 @@
 import graphene
+from opentelemetry import trace
 
 from .schema import generate_query_mixin
 
-DynamicGraphQL = generate_query_mixin()
+_tracer = trace.get_tracer("nautobot.graphql")
+
+with _tracer.start_as_current_span("graphql.schema.generate"):
+    DynamicGraphQL = generate_query_mixin()
 
 
 class Query(graphene.ObjectType, DynamicGraphQL):
     """Contains the entire GraphQL Schema definition for Nautobot."""
 
 
-schema = graphene.Schema(query=Query, auto_camelcase=False)
+with _tracer.start_as_current_span("graphql.schema.build"):
+    schema = graphene.Schema(query=Query, auto_camelcase=False)
