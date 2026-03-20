1a223add73711b8c418a2f488f51eeeab0a0dd41
Daria Kakolewska <117984547+dsoftait@users.noreply.github.com>
2026-03-20 15:54:38 +0100

    Fix Run/Schedule button in Job detail view (#8724)
    ---------
    
    Co-authored-by: Daria Kakolewska <daria.kakolewska@networktocode.com>

--- nautobot/extras/views.py
+++ nautobot/extras/views.py
@@ -1648,7 +1648,7 @@ class JobView(generic.ObjectView):
                 label="Run/Schedule",
                 icon="mdi-play",
                 color=ButtonActionColorChoices.SUBMIT,
-                required_permissions=["extras.job_run"],
+                required_permissions=["extras.run_job"],
             ),
         ],
     )
