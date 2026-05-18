be5919b36df3f29ca5898ed38e31df36d8f21d2c
Daria Kakolewska <daria.kakolewska@networktocode.com>
2026-05-18 12:03:52 +0200

    Fix revoke job button

--- nautobot/extras/views.py
+++ nautobot/extras/views.py
@@ -3536,6 +3536,7 @@ class JobResultUIViewSet(
                     )
                     else None
                 ),
+                template_path="extras/inc/jobresult_revokejobbutton.html",
             ),
         ),
         extra_tabs=[
