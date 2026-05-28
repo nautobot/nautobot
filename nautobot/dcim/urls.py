a3ba58666da2a5c6c919ea69b1ba9d7508d7c337
Sridevi-NTC <sridevi.cheekatla@networktocode.com>
2026-05-28 11:38:49 +0000

    Refactored PowerPortTemplate

--- nautobot/dcim/urls.py
+++ nautobot/dcim/urls.py
@@ -47,6 +47,7 @@ router.register("modules", views.ModuleUIViewSet)
 router.register("platforms", views.PlatformUIViewSet)
 router.register("power-feeds", views.PowerFeedUIViewSet)
 router.register("power-panels", views.PowerPanelUIViewSet)
+router.register("power-port-templates", views.PowerPortTemplateUIViewSet)
 router.register("racks", views.RackUIViewSet)
 router.register("rack-groups", views.RackGroupUIViewSet)
 router.register("rack-reservations", views.RackReservationUIViewSet)
@@ -179,37 +180,6 @@ urlpatterns = [
         "console-server-port-templates/<uuid:pk>/delete/",
         views.ConsoleServerPortTemplateDeleteView.as_view(),
         name="consoleserverporttemplate_delete",
-    ),
-    # Power port templates
-    path(
-        "power-port-templates/add/",
-        views.PowerPortTemplateCreateView.as_view(),
-        name="powerporttemplate_add",
-    ),
-    path(
-        "power-port-templates/edit/",
-        views.PowerPortTemplateBulkEditView.as_view(),
-        name="powerporttemplate_bulk_edit",
-    ),
-    path(
-        "power-port-templates/rename/",
-        views.PowerPortTemplateBulkRenameView.as_view(),
-        name="powerporttemplate_bulk_rename",
-    ),
-    path(
-        "power-port-templates/delete/",
-        views.PowerPortTemplateBulkDeleteView.as_view(),
-        name="powerporttemplate_bulk_delete",
-    ),
-    path(
-        "power-port-templates/<uuid:pk>/edit/",
-        views.PowerPortTemplateEditView.as_view(),
-        name="powerporttemplate_edit",
-    ),
-    path(
-        "power-port-templates/<uuid:pk>/delete/",
-        views.PowerPortTemplateDeleteView.as_view(),
-        name="powerporttemplate_delete",
     ),
     # Power outlet templates
     path(
