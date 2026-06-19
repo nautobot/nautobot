4d425213d4b798302421f7b6ba30872ad3c313de
Daria Kakolewska <daria.kakolewska@networktocode.com>
2026-06-19 10:36:17 +0200

    changelog and one more test

--- nautobot/ipam/tests/test_graphql.py
+++ nautobot/ipam/tests/test_graphql.py
@@ -32,3 +32,55 @@ class TestPrefix(APITestCase):
             self.assertIsInstance(prefix["prefix"], str)
             self.assertIsInstance(prefix["prefix_length"], int)
             self.assertIn(prefix["ip_version"], [4, 6])
+
+
+class TestIPAddressRange(APITestCase):
+    def setUp(self):
+        super().setUp()
+        self.api_url = reverse("graphql-api")
+        self.add_permissions("ipam.view_ipaddressrange")
+
+    def test_ip_address_range_fields(self):
+        """Test start_address, end_address, and ip_version are available for an IPAddressRange via GraphQL."""
+        get_ranges_query = """
+        query {
+            ip_address_ranges {
+                start_address
+                end_address
+                ip_version
+            }
+        }
+        """
+        payload = {"query": get_ranges_query}
+        response = self.client.post(self.api_url, payload, format="json", **self.header)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        ranges = response.data["data"]["ip_address_ranges"]
+        self.assertIsInstance(ranges, list)
+        self.assertGreater(len(ranges), 0)
+
+        for ip_range in ranges:
+            self.assertIsInstance(ip_range["start_address"], str)
+            self.assertIsInstance(ip_range["end_address"], str)
+            self.assertIn(ip_range["ip_version"], [4, 6])
+
+    def test_ip_address_range_dynamic_groups(self):
+        """Test dynamic_groups resolver is available for an IPAddressRange via GraphQL."""
+        get_ranges_query = """
+        query {
+            ip_address_ranges {
+                start_address
+                dynamic_groups {
+                    id
+                    name
+                }
+            }
+        }
+        """
+        payload = {"query": get_ranges_query}
+        response = self.client.post(self.api_url, payload, format="json", **self.header)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        ranges = response.data["data"]["ip_address_ranges"]
+        self.assertIsInstance(ranges, list)
+
+        for ip_range in ranges:
+            self.assertIsInstance(ip_range["dynamic_groups"], list)
