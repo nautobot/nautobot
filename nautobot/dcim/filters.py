f5e59a182f854bff613a38998dd46826bb49eff6
sirisha-bode <sirisha.bode@networktocode.com>
2026-07-08 21:12:55 +0530

    InventoryItemUIViewSet & UI Component (#9033)
    * InventoryItem-UI-ViewSet
    
    
    ---------
    
    Co-authored-by: Sridevi05 <sridevi.cheekatla@networktocode.com>

--- nautobot/dcim/filters.py
+++ nautobot/dcim/filters.py
@@ -107,6 +107,7 @@ from nautobot.dcim.models import (
 from nautobot.extras.filters import (
     LocalContextModelFilterSetMixin,
     NautobotFilterSet,
+    RelationshipModelFilterSetMixin,
     RoleModelFilterSetMixin,
     StatusModelFilterSetMixin,
 )
@@ -1324,7 +1325,7 @@ class DeviceBayFilterSet(DeviceComponentModelFilterSetMixin, BaseFilterSet):
         fields = ["id", "name", "description", "label", "tags"]
 
 
-class InventoryItemFilterSet(DeviceComponentModelFilterSetMixin, BaseFilterSet):
+class InventoryItemFilterSet(DeviceComponentModelFilterSetMixin, RelationshipModelFilterSetMixin, BaseFilterSet):
     q = SearchFilter(
         filter_predicates={
             "name": "icontains",
