8a11d5532e020825cbd9c965588d6a4d43073238
Daria Kakolewska <117984547+dsoftait@users.noreply.github.com>
2026-07-16 18:17:25 +0200

    Fix IP Address bulk create (#9246)
    ---------
    
    Co-authored-by: Daria Kakolewska <daria.kakolewska@networktocode.com>
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>

--- nautobot/ipam/models.py
+++ nautobot/ipam/models.py
@@ -1730,6 +1730,11 @@ class IPAddress(NamespaceParentedModelMixin, PrimaryModel):
             self.parent = closest_parent
             self._namespace = None
 
+        if self.host and self.parent_id:
+            duplicate = IPAddress.objects.filter(parent_id=self.parent_id, host=self.host).exclude(pk=self.pk)
+            if duplicate.exists():
+                raise ValidationError({"__all__": f"IP address {self.address} already exists in {self.parent}."})
+
         # Force dns_name to lowercase
         if not self.dns_name.islower():
             self.dns_name = self.dns_name.lower()
