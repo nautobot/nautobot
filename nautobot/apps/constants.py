8c42d8c67f5b62b7a3884d9da6b10d448e68e7b2
Noah Robin <noahr@nvidia.com>
2026-07-27 09:28:46 -0400

    Added BOOLEAN_CHOICES to nautobot.apps.constants

--- nautobot/apps/constants.py
+++ nautobot/apps/constants.py
@@ -1,9 +1,10 @@
 """Nautobot Constants."""
 
 from nautobot.core.constants import CHARFIELD_MAX_LENGTH
-from nautobot.core.forms.constants import BOOLEAN_WITH_BLANK_CHOICES
+from nautobot.core.forms.constants import BOOLEAN_CHOICES, BOOLEAN_WITH_BLANK_CHOICES
 
 __all__ = (
+    "BOOLEAN_CHOICES",
     "BOOLEAN_WITH_BLANK_CHOICES",
     "CHARFIELD_MAX_LENGTH",
 )
