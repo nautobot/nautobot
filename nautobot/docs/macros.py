ebcabf737cb4202fb742095cc0ed89ab94a2127e
Glenn Matthews <glenn.matthews@networktocode.com>
2026-08-31 13:09:40 -0400

    Remove docs symlink and other changes needed to build successfully in docker sbx

--- nautobot/docs/macros.py
+++ nautobot/docs/macros.py
@@ -7,7 +7,7 @@ import yaml
 
 def define_env(env):
     """Load nautobot/core/*.yaml source-of-truth files into the Jinja2 rendering environment."""
-    core_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "nautobot", "core")
+    core_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "core")
 
     with open(os.path.join(core_dir, "settings.yaml"), "rt") as fh:
         env.variables["settings_schema"] = yaml.safe_load(fh)
